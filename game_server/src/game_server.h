43138c25a187d6a872946bca780930f88f78e88d
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-05-26 15:22:17 +0800

    add gameserver

---
+++ game_server/src/game_server.h
@@ -0,0 +1,8 @@
+#ifndef GAME_SERVER_SRC_GAME_SERVER_GAME_SERVER_H_
+#define GAME_SERVER_SRC_GAME_SERVER_GAME_SERVER_H_
+
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/protorpc/RpcServer.h"
+#include "muduo/net/TcpServer.h"
+
+#endif // !GAME_SERVER_SRC_GAME_SERVER_GAME_SERVER_H_
