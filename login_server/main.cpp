3490de91d139096750abd92a790b0a32e67c52ab
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-05-26 14:59:20 +0800

    add master server

--- login_server/main.cpp
+++ login_server/main.cpp
@@ -1,19 +1,22 @@
 #include "login_server.h"
 
-#include "src/gateway/service.h"
 #include "src/database/rpcclient/database_rpcclient.h"
+#include "src/gateway/service.h"
+#include "src/master/rpcclient/master_rpcclient.h"
 
 int main(int argc, char* argv[])
 {
     int nThreads = argc > 1 ? atoi(argv[1]) : 1;
     EventLoop loop;
-    InetAddress listenAddr("127.0.0.1", 2001);
+    InetAddress listen_addr("127.0.0.1", 2001);
     InetAddress database_addr("127.0.0.1", 2003);
+    InetAddress master_addr("127.0.0.1", 2004);
 
     database.Connect(&loop, database_addr);
+    master.Connect(&loop, master_addr);
 
     gw2l::LoginServiceImpl impl;
-    RpcServer server(&loop, listenAddr);
+    RpcServer server(&loop, listen_addr);
     server.setThreadNum(nThreads);
     server.registerService(&impl);
     server.start();
---
+++ login_server/src/master/rpcclient/master_rpcclient.h
@@ -0,0 +1,49 @@
+#ifndef LOGIN_MASTER_RPCCLIENT_MASTER_RPC_CLIENT_H_
+#define LOGIN_MASTER_RPCCLIENT_MASTER_RPC_CLIENT_H_
+
+#ifdef __linux__
+#include <unistd.h>
+#endif//__linux__
+
+#include "l2ms.pb.h"
+
+#include "muduo/base/Logging.h"
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/protorpc/RpcServer.h"
+
+#include "src/server_rpc_client/login_client.h"
+
+using namespace muduo;
+using namespace muduo::net;
+
+namespace login
+{
+    class MasterRpcClient
+    {
+    public:
+        using RpcStub = common::RpcClient <l2ms::LoginService_Stub>;
+        using RpcClientPtr = std::shared_ptr<RpcStub>;
+
+        void Connect(EventLoop* loop,
+            const InetAddress& login_server_addr)
+        {
+            master_client_ = std::make_shared<RpcStub>(loop, login_server_addr);
+            master_client_->connect();
+        }
+
+        static MasterRpcClient& GetSingleton()
+        {
+            static MasterRpcClient singleton;
+            return singleton;
+        }
+
+    private:
+        RpcClientPtr master_client_;
+    };
+
+}// namespace login
+
+#define  master login::MasterRpcClient::GetSingleton() 
+
+#endif // LOGIN_MASTER_RPCCLIENT_MASTER_RPC_CLIENT_H_
+
