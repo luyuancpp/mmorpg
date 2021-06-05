f2aa20365c4e6133b4f5fb25e8ba0e0cac8999e8
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-06-05 11:38:20 +0800

    message table

---
+++ database_server/src/mysql_database/mysql_database.cpp
@@ -0,0 +1,11 @@
+#include "mysql_database.h"
+
+namespace database
+{
+void MysqlDatabase::Init()
+{
+
+}
+
+}//namespace database
+
---
+++ database_server/src/mysql_database/mysql_database.h
@@ -0,0 +1,14 @@
+#ifndef DATA_BASE_SRC_MYSQL_DATA_BASE_MYSQL_DATABASE_H_
+#define DATA_BASE_SRC_MYSQL_DATA_BASE_MYSQL_DATABASE_H_
+
+namespace database
+{
+class MysqlDatabase
+{
+private:
+
+};
+
+}//namespace database
+
+#endif // !DATA_BASE_SRC_MYSQL_DATA_BASE_MYSQL_DATABASE_H_
