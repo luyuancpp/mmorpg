ceab1b8541d4032f05bcb55ec92b647b13a41c77
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-06-05 12:16:49 +0800

    create table

--- database_server/src/mysql_database/mysql_database.cpp
+++ database_server/src/mysql_database/mysql_database.cpp
@@ -1,10 +1,14 @@
 #include "mysql_database.h"
 
+#include "mysql_database_table.pb.h"
+
+#include "src/pb2db/pb2dbsql.h"
+
 namespace database
 {
 void MysqlDatabase::Init()
 {
-
+     common::Pb2DbTables::GetSingleton().AddTable(account_database::GetDescriptor()->full_name());
 }
 
 }//namespace database
