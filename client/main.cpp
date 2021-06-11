bfa9dcea8468bbb9c20ccaacb8016aae922d7b08
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-06-11 17:11:59 +0800

    login logic

---
+++ client/main.cpp
@@ -0,0 +1,59 @@
+#include "src/client.h"
+
+int main(int argc, char* argv[])
+{
+    LOG_INFO << "pid = " << getpid();
+    if (argc > 1)
+    {
+        int nClients = 1;
+
+        if (argc > 2)
+        {
+            nClients = atoi(argv[2]);
+        }
+
+        int nThreads = 1;
+
+        if (argc > 3)
+        {
+            nThreads = atoi(argv[3]);
+        }
+
+        CountDownLatch allConnected(nClients);
+        CountDownLatch allFinished(nClients);
+
+        EventLoop loop;
+        EventLoopThreadPool pool(&loop, "playerbench-client");
+        pool.setThreadNum(nThreads);
+        pool.start();
+        InetAddress serverAddr("127.0.0.1", 2000);
+
+        std::vector<std::unique_ptr<PlayerClient>> clients;
+        for (int i = 0; i < nClients; ++i)
+        {
+            clients.emplace_back(new PlayerClient(pool.getNextLoop(), serverAddr, &allConnected, &allFinished));
+            clients.back()->connect();
+        }
+        allConnected.wait();
+        Timestamp start(Timestamp::now());
+        LOG_INFO << "all connected";
+        for (int i = 0; i < nClients; ++i)
+        {
+            clients[i]->SendRequest();
+        }
+        allFinished.wait();
+        Timestamp end(Timestamp::now());
+        LOG_INFO << "all finished";
+        double seconds = timeDifference(end, start);
+        printf("%f seconds\n", seconds);
+        printf("%.1f calls per second\n", nClients * seconds);
+
+        return 0;
+    }
+    else
+    {
+        printf("Usage: %s host_ip numClients [numThreads]\n", argv[0]);
+    }
+    return 0;
+}
+
--- client/src/client.h
+++ client/src/client.h
@@ -1,4 +1,7 @@
-﻿#include <stdio.h>
+#ifndef CLIENT_SRC_CLIENT_H_
+#define CLIENT_SRC_CLIENT_H_
+
+#include <stdio.h>
 
 #include "muduo/base/CountDownLatch.h"
 #include "muduo/base/Logging.h"
@@ -97,61 +100,4 @@ private:
     CountDownLatch* all_connected_;
     CountDownLatch* all_finished_;
 };
-
-int main(int argc, char* argv[])
-{
-    LOG_INFO << "pid = " << getpid();
-    if (argc > 1)
-    {
-        int nClients = 1;
-
-        if (argc > 2)
-        {
-            nClients = atoi(argv[2]);
-        }
-
-        int nThreads = 1;
-
-        if (argc > 3)
-        {
-            nThreads = atoi(argv[3]);
-        }
-
-        CountDownLatch allConnected(nClients);
-        CountDownLatch allFinished(nClients);
-
-        EventLoop loop;
-        EventLoopThreadPool pool(&loop, "playerbench-client");
-        pool.setThreadNum(nThreads);
-        pool.start();
-        InetAddress serverAddr("127.0.0.1", 2000);
-
-        std::vector<std::unique_ptr<PlayerClient>> clients;
-        for (int i = 0; i < nClients; ++i)
-        {
-            clients.emplace_back(new PlayerClient(pool.getNextLoop(), serverAddr, &allConnected, &allFinished));
-            clients.back()->connect();
-        }
-        allConnected.wait();
-        Timestamp start(Timestamp::now());
-        LOG_INFO << "all connected";
-        for (int i = 0; i < nClients; ++i)
-        {
-            clients[i]->SendRequest();
-        }
-        allFinished.wait();
-        Timestamp end(Timestamp::now());
-        LOG_INFO << "all finished";
-        double seconds = timeDifference(end, start);
-        printf("%f seconds\n", seconds);
-        printf("%.1f calls per second\n", nClients *  seconds);
-
-        return 0;
-    }
-    else
-    {
-        printf("Usage: %s host_ip numClients [numThreads]\n", argv[0]);
-    }
-    return 0;
-}
-
+#endif//CLIENT_SRC_CLIENT_H_
