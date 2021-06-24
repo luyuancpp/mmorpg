477e77b2c00015178bbf91f0c49a174c6a27031c
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-06-24 15:20:58 +0800

    waiting enter game

--- unit_test/login_state/login_state_test.cpp
+++ unit_test/login_state/login_state_test.cpp
@@ -8,17 +8,17 @@ using namespace common;
 TEST(LoginStateMachineTest, LoginRepetition)
 {
     LoginStateMachine lsm;
-    EXPECT_EQ(RET_LOGIN_LOGIN_NOT_COMPLETE, lsm.CreatePlayer());
-    EXPECT_EQ(RET_LOGIN_LOGIN_NOT_COMPLETE, lsm.EnterGame());
-    EXPECT_EQ(RET_OK, lsm.Lgoin());
-    EXPECT_EQ(RET_LOGIN_REPETITION_LOGIN, lsm.Lgoin());
-    EXPECT_EQ(RET_LOGIN_LOGIN_NO_PLAYER, lsm.EnterGame());
+    EXPECT_EQ(RET_LOGIN_DONOT_LOGIN, lsm.CreatePlayer());
+    EXPECT_EQ(RET_LOGIN_DONOT_LOGIN, lsm.EnterGame());
+    EXPECT_EQ(RET_OK, lsm.Login());
+    EXPECT_EQ(RET_LOGIN_LOGIN_ING, lsm.Login());
+    EXPECT_EQ(RET_LOGIN_LOGIN_ING, lsm.EnterGame());
 }
 
 TEST(LoginStateMachineTest, LoginNoPlayer)
 {
     LoginStateMachine lsm;
-    EXPECT_EQ(RET_OK, lsm.Lgoin());
+    EXPECT_EQ(RET_OK, lsm.Login());
     lsm.NoPlayer();
     EXPECT_EQ(RET_LOGIN_LOGIN_NO_PLAYER, lsm.EnterGame());
     EXPECT_EQ(RET_OK, lsm.CreatePlayer());
@@ -30,7 +30,7 @@ TEST(LoginStateMachineTest, LoginNoPlayer)
 TEST(LoginStateMachineTest, LoginNoPlayerCreateFull)
 {
     LoginStateMachine lsm;
-    EXPECT_EQ(RET_OK, lsm.Lgoin());
+    EXPECT_EQ(RET_OK, lsm.Login());
     lsm.NoPlayer();
     EXPECT_EQ(RET_LOGIN_LOGIN_NO_PLAYER, lsm.EnterGame());
     EXPECT_EQ(RET_OK, lsm.CreatePlayer());
@@ -51,7 +51,7 @@ TEST(LoginStateMachineTest, LoginNoPlayerCreateFull)
 TEST(LoginStateMachineTest, RenterGame)
 {
     LoginStateMachine lsm;
-    EXPECT_EQ(RET_OK, lsm.Lgoin());
+    EXPECT_EQ(RET_OK, lsm.Login());
     lsm.WaitingEnterGame();
     EXPECT_EQ(RET_OK, lsm.EnterGame());
     EXPECT_EQ(RET_LOGIN_BEING_ENTER_GAME, lsm.EnterGame());
@@ -59,6 +59,39 @@ TEST(LoginStateMachineTest, RenterGame)
     EXPECT_EQ(RET_LOGIN_PLAYEING, lsm.EnterGame());
 }
 
+
+TEST(LoginStateMachineTest, LoginNotReturn)
+{
+    LoginStateMachine lsm;
+    EXPECT_EQ(RET_OK, lsm.Login());
+    EXPECT_EQ(RET_LOGIN_LOGIN_ING, lsm.CreatePlayer());
+    EXPECT_EQ(RET_LOGIN_LOGIN_ING, lsm.EnterGame());
+}
+
+TEST(LoginStateMachineTest, CreatePlayerNotReturn)
+{
+    LoginStateMachine lsm;
+    EXPECT_EQ(RET_OK, lsm.Login());
+    lsm.WaitingEnterGame();
+    EXPECT_EQ(RET_LOGIN_WAITING_ENTER_GAME, lsm.Login());
+    EXPECT_EQ(RET_OK, lsm.CreatePlayer());
+    EXPECT_EQ(RET_LOGIN_BEING_CREATE_PLAYER, lsm.Login());
+    EXPECT_EQ(RET_LOGIN_BEING_CREATE_PLAYER, lsm.CreatePlayer());
+    EXPECT_EQ(RET_LOGIN_BEING_CREATE_PLAYER, lsm.EnterGame());
+}
+
+TEST(LoginStateMachineTest, EnterGameNotReturn)
+{
+    LoginStateMachine lsm;
+    EXPECT_EQ(RET_OK, lsm.Login());
+    lsm.WaitingEnterGame();
+    EXPECT_EQ(RET_OK, lsm.CreatePlayer());
+    lsm.WaitingEnterGame();
+    EXPECT_EQ(RET_OK, lsm.EnterGame());
+    EXPECT_EQ(RET_LOGIN_BEING_ENTER_GAME, lsm.Login());
+    EXPECT_EQ(RET_LOGIN_BEING_ENTER_GAME, lsm.CreatePlayer());
+    EXPECT_EQ(RET_LOGIN_BEING_ENTER_GAME, lsm.EnterGame());
+}
 
 int main(int argc, char** argv)
 {
