7ba10c10c555cf2b8849a2c302203eb010ba8cad
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-07-14 15:43:39 +0800

    add team logic

---
+++ common/src/game_logic/teams/team.h
@@ -0,0 +1,93 @@
+#ifndef COMMON_SRC_TEAM_TEAM_H_
+#define COMMON_SRC_TEAM_TEAM_H_
+
+#include <deque>
+#include <list>
+#include <unordered_map>
+#include <unordered_set>
+#include <vector>
+
+#include "team.pb.h"
+
+#include "src/common_type/common_type.h"
+#include "src/event/event.h"
+#include "src/return_code/notice_struct.h"
+
+namespace common
+{
+    static const std::size_t kMaxApplicantSize{ 20 };
+    static const std::size_t kMaxMemberSize{ 5 };
+    typedef std::unordered_map<GameGuid, TeamMember> Members;
+
+    //function order get, set is, test action
+    struct CreateTeamParam
+    {
+        GameGuid leader_id_{ 0 };
+        const Members members;
+        std::string  name_;
+        EventManagerPtr emp_;
+    };
+
+    class Team
+    {
+    public:
+        using ApplyMembers = std::unordered_map<GameGuid, TeamMember>;
+        
+        Team(GameGuid team_id, EventManagerPtr& emp, const CreateTeamParam& param);
+
+        GameGuid team_id()const { return team_id_; }
+        GameGuid leader_id()const { return leader_id_; }
+        const TeamMember& team_member(GameGuid playerid)const;
+        std::size_t max_member_size()const { return kMaxMemberSize; }
+        std::size_t member_size()const { return members_.size(); }
+        bool empty()const { return members_.empty(); }
+        std::size_t applicant_size()const
+        {
+            assert(applicant_ids_.size() == applicants_.size());  return applicants_.size();
+        }
+        GameGuid first_applicant_id()const;
+        const Members& members()const { return members_; }
+
+        bool HasApplicant(GameGuid applicant_id) const { return applicants_.find(applicant_id) != applicants_.end(); }
+        bool HasApply()const { return !applicants_.empty(); }
+        bool IsFull()const { return members_.size() >= max_member_size(); }
+        bool IsLeader(GameGuid playerid)const { return leader_id_ != kEmptyGameGuid && leader_id_ == playerid; }
+        bool InTeam(GameGuid player_guid)const { return members_.find(player_guid) != members_.end(); }
+
+        ReturnValue CheckLimt(const TeamMember& m);
+        bool TestApplicantValueEqual()const;
+
+        ReturnValue JoinTeam(const TeamMember& m);
+        ReturnValue TryToJoinTeam(const TeamMember& m);
+        ReturnValue LeaveTeam(GameGuid playerid);
+        ReturnValue KickMember(GameGuid current_leader, GameGuid  nKickplayerid);
+        ReturnValue AppointLeader(GameGuid current_leader, GameGuid  new_leader_player_id);
+        ReturnValue ApplyForTeam(const TeamMember& m);
+        ReturnValue AgreeApplicant(GameGuid applicant_id);
+        ReturnValue RemoveApplicant(GameGuid applicant_id);
+        ReturnValue DisMiss();
+        void ClearApplyList();
+    protected:
+        template<typename E>
+        void OnJoinTeam(const TeamMember& m)
+        {
+            RemoveApplicant(m.player_id());
+            members_.emplace(m.player_id(), m);
+            sequence_players_id_.push_back(m.player_id());
+            assert(members_.size() == sequence_players_id_.size());
+            emp_->emit<E>(team_id_, m.player_id());
+        }
+        void OnAppointLeader(GameGuid  new_leader_player_id);
+        void RemoveApplicantId(GameGuid  player_id);
+
+        GameGuid team_id_{ kEmptyGameGuid };
+        GameGuid leader_id_{ kEmptyGameGuid };
+        Members members_;
+        ApplyMembers applicants_;
+        PlayerIdsV applicant_ids_;
+        PlayerIdsV sequence_players_id_;
+        EventManagerPtr emp_;
+    };
+}//namespace common
+
+#endif // COMMON_SRC_TEAM_TEAM_H_
---
+++ common/src/game_logic/teams/teams.h
@@ -0,0 +1,88 @@
+#ifndef SRC_TEAM_TEAM_LIST_H_
+#define SRC_TEAM_TEAM_LIST_H_
+
+#include <unordered_map>
+
+#include "src/snow_flake/snow_flake.h"
+#include "src/teams/team_event.h"
+
+#include "team.h"
+
+namespace common
+{
+    class TeamList : public Receiver<TeamList>
+    {
+    public:
+        using TeamPtr = std::shared_ptr<Team>;
+        using TeamMap = std::unordered_map<GameGuid, TeamPtr>;
+        using PlayerIdTeamIdMap = std::unordered_map<GameGuid, GameGuid>;
+        using TeamIdList = std::unordered_set<GameGuid>;
+        static const std::size_t kMaxTeamSize = 10000;
+
+        TeamList();
+
+        std::size_t team_size()const { return teams_.size(); }
+        std::size_t member_size(GameGuid team_id);
+        std::size_t applicant_size_by_player_id(GameGuid player_id)const;
+        std::size_t applicant_size_by_team_id(GameGuid team_id)const;
+        std::size_t players_size()const { return player_team_map_.size(); }
+        const TeamMember& team_member(GameGuid player_id)const;
+        GameGuid GetTeamId(GameGuid player_id)const;
+        GameGuid last_team_id() const { return last_team_id_; }
+        GameGuid leader_id_by_teamid(GameGuid team_id)const;
+        GameGuid leader_id_by_player_id(GameGuid player_id)const;
+        GameGuid first_applicant_id(GameGuid team_id)const;
+       
+        bool IsTeamsMax()const;
+        bool IsTeamFull(GameGuid team_id);
+        bool PlayerInTeam(GameGuid team_id, GameGuid player_id);
+        bool PlayerInTeam(GameGuid player_id)const { return player_team_map_.find(player_id) != player_team_map_.end(); }
+        bool FindTeamId(GameGuid team_id) const{ return teams_.find(team_id) != teams_.end(); }
+        bool IsLeader(GameGuid team_id, GameGuid player_id);
+        bool IsLeader(GameGuid player_id);
+        bool FindTeamId(GameGuid player_id);
+        bool HasApplicant(GameGuid team_id, GameGuid player_id)const;
+
+        bool TestApplicantValueEqual(GameGuid team_id)const;
+        
+        void receive(const TeamEventStructJoinTeam& es);
+        void receive(const TeamEventStructCreateTeamJoinTeam& es);        
+        void receive(const TeamEventStructLeaderDismissTeam& es);
+        void receive(const TeamEventStructLeaveTeam& es);
+        void receive(const TeamEventStructDismissTeamOnTeamMemberEmpty& es);
+
+        ReturnValue CreateTeam(const CreateTeamParam& param);
+        ReturnValue JoinTeam(GameGuid team_id, TeamMember& mem);
+        ReturnValue LeaveTeam(GameGuid player_id);
+        ReturnValue KickMember(GameGuid team_id, GameGuid current_leader_id, GameGuid  kick_player_id);
+        ReturnValue DissMissTeam(GameGuid team_id, GameGuid current_leader_id);
+        ReturnValue DissMissTeamNoLeader(GameGuid team_id);
+        ReturnValue AppointLeader(GameGuid team_id, GameGuid current_leader_id, GameGuid  nNewLeaderPlayerId);
+        ReturnValue ApplyForTeam(GameGuid team_id, const TeamMember& m);
+        ReturnValue RemoveApplicant(GameGuid team_id, GameGuid apply_player_id);
+        ReturnValue AgreeApplicant(GameGuid team_id, GameGuid apply_player_id);
+        void ClearApplyList(GameGuid team_id);
+        
+    protected:
+
+        ReturnValue JoinTeam(const Members& member_list, GameGuid  team_id);
+        ReturnValue CheckMemberInTeam(const Members& member_list);
+        void OnPlayerLeaveTeam(GameGuid player_id);
+        void EraseTeam(GameGuid team_id);
+        void OnJoinTeam(GameGuid player_id, GameGuid team_id)
+        {
+            player_team_map_.emplace(player_id, team_id);
+        }
+
+        TeamMap teams_;
+        PlayerIdTeamIdMap player_team_map_;
+        SnowFlake snow_flake_;
+        GameGuid last_team_id_{ 0 };
+        EventManagerPtr emp_;
+    };
+}//namespace common
+
+
+
+#endif // SRC_TEAM_TEAM_LIST_H_
+
