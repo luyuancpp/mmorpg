48524adcbbf7206fd32f804849287fd7e7acdf4d
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-07-16 19:03:24 +0800

    optimize

--- common/src/server_common/rpc_client_closure.h
+++ common/src/server_common/rpc_client_closure.h
@@ -12,13 +12,10 @@ namespace common
             : s_resp_(new ServerResponse()),
               client_connection_(cc)
         {
-            uint64_t p = (uint64_t)(client_connection_.get());
-            boost::any connection_context = p;
-            client_connection_->setContext(connection_context);
         }
 
         uint64_t connection_hash_id() {
-            return boost::any_cast<uint64_t>(client_connection_->getContext());
+            return uint64_t(client_connection_.get());
         }
 
         ~ClientClosure() {};
