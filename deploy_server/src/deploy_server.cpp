5adf29977ecaf49ff2c4302d1fac8d195bb549f3
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-07-17 18:20:59 +0800

    Revert "connection common server"
    This reverts commit 62e26f969e8ffdd600b0e492174fdcb37e8a857d.
    
    # Conflicts:
    #	deploy_server/src/deploy_server.cpp

--- deploy_server/src/deploy_server.cpp
+++ deploy_server/src/deploy_server.cpp
@@ -1,35 +1,25 @@
 ﻿#include "deploy_server.h"
 
-#include <google/protobuf/descriptor.h>
-#include <google/protobuf/service.h>
-
-#include "muduo/base/Logging.h"
-#include "muduo/net/Callbacks.h"
-
 #include "src/game_config/game_config.h"
-#include "src/server_common/rpc_channel.h"
+#include "src/server_type_id/server_type_id.h"
 
 #include "deploy_database_table.pb.h"
 
-using namespace muduo;
-using namespace muduo::net;
-using namespace std::placeholders;
-
-static const std::string kIp("127.0.0.1");
+static const uint32_t kRedisPort = 6379;
+static const uint32_t kGroup = 500;
+static const uint32_t kBeginPort = 2000;
+static const uint32_t kGroupServerSize = common::SERVER_ID_GROUP_SIZE - common::SERVER_REDIS + 1;
+static const uint32_t kTotalSize = kGroup * kGroupServerSize;
+static const std::string kIp = "127.0.0.1";
 
 namespace deploy_server
 {
     DeployServer::DeployServer(muduo::net::EventLoop* loop,
         const muduo::net::InetAddress& listen_addr)
-        :server_(loop, listen_addr, "DeployServer"),
+        :server_(loop, listen_addr),
         database_(std::make_shared<common::MysqlDatabase>())
     {
         database_->Connect(common::DeployConfig::GetSingleton().connetion_param());
-
-        server_.setConnectionCallback(
-            std::bind(&DeployServer::onConnection, this, _1));
-        //   server_.setMessageCallback(
-        //       std::bind(&RpcServer::onMessage, this, _1, _2, _3));
     }
 
     void DeployServer::Start()
@@ -42,8 +32,7 @@ namespace deploy_server
 
     void DeployServer::RegisterService(google::protobuf::Service* service)
     {
-        const google::protobuf::ServiceDescriptor* desc = service->GetDescriptor();
-        services_[desc->full_name()] = service;
+        server_.registerService(service);
     }
 
     void DeployServer::InitServerInof()
@@ -70,24 +59,4 @@ namespace deploy_server
         }
     }
 
-
-    void DeployServer::onConnection(const muduo::net::TcpConnectionPtr& conn)
-    {
-        LOG_INFO << "RpcServer - " << conn->peerAddress().toIpPort() << " -> "
-            << conn->localAddress().toIpPort() << " is "
-            << (conn->connected() ? "UP" : "DOWN");
-        if (conn->connected())
-        {
-            RpcChannelPtr channel(new RpcChannel(conn));
-            channel->setServices(&services_);
-            conn->setMessageCallback(
-                std::bind(&RpcChannel::onMessage, get_pointer(channel), _1, _2, _3));
-            conn->setContext(channel);
-        }
-        else
-        {
-            conn->setContext(RpcChannelPtr());
-            // FIXME:
-        }
-    }
 }//namespace deploy_server
--- deploy_server/src/deploy_server.h
+++ deploy_server/src/deploy_server.h
@@ -2,52 +2,30 @@
 #define DEPLOY_SERVER_SRCDEPLOY_SERVER_H_
 
 #include "muduo/net/InetAddress.h"
-#include "muduo/net/TcpServer.h"
+#include "src/server_common/rpc_server.h"
+
 #include "src/mysql_database/mysql_database.h"
 #include "src/redis_client/redis_client.h"
-#include "src/server_type_id/server_type_id.h"
-
-namespace google {
-    namespace protobuf {
-
-        class Service;
-
-    }  // namespace protobuf
-}  // namespace google
 
 namespace deploy_server
 {
     class DeployServer
     {
     public:
-
         using MysqlClientPtr = std::shared_ptr<common::MysqlDatabase>;
         using RedisClientPtr = std::shared_ptr<common::RedisClient>;
-
-        static const uint32_t kRedisPort = 6379;
-        static const uint32_t kGroup = 500;
-        static const uint32_t kBeginPort = 2000;
-        static const uint32_t kGroupServerSize = common::SERVER_ID_GROUP_SIZE - common::SERVER_REDIS + 1;
-        static const uint32_t kTotalSize = kGroup * kGroupServerSize;
-        static const uint32_t kLogicBeginPort = kTotalSize + kBeginPort + 1;
-        
-
         DeployServer(muduo::net::EventLoop* loop,
             const muduo::net::InetAddress& listen_addr);
 
         MysqlClientPtr& player_mysql_client() { return database_; }
 
         void Start();
-        
+
         void RegisterService(::google::protobuf::Service*);
-
     private:
-        void onConnection(const muduo::net::TcpConnectionPtr& conn);
-
         void InitServerInof();
 
-        muduo::net::TcpServer server_;
-        std::map<std::string, ::google::protobuf::Service*> services_;
+        muduo::net::RpcServer server_;
         MysqlClientPtr database_;
     };
 }//namespace deploy_server
