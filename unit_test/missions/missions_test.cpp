cca5b75443e9172cc417ab4b9d37b32783c86690
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-07-27 10:38:58 +0800

    missions

---
+++ unit_test/missions/missions_test.cpp
@@ -0,0 +1,30 @@
+﻿#include <gtest/gtest.h>
+
+#include "src/game_config/generator/json_cpp/mission_json.h"
+#include "src/game_config/generator/json_cpp/condition_json.h"
+
+using namespace common;
+
+TEST(MissionsTest, Decorate)
+{
+
+}
+
+TEST(MissionsTest, RadomCondtion)
+{
+
+}
+
+TEST(MissionsTest, CondtionList)
+{
+
+}
+
+int main(int argc, char** argv)
+{
+    ConditionJson::GetSingleton().Load("config/json/condition.json");
+    testing::InitGoogleTest(&argc, argv);
+
+    return RUN_ALL_TESTS();
+}
+
