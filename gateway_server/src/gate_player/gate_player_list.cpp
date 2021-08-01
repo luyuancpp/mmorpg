9c9b52d225ef339618e64719c285dd8894096975
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-08-01 19:24:00 +0800

    conection master

---
+++ gateway_server/src/gate_player/gate_player_list.cpp
@@ -0,0 +1,6 @@
+#include "gate_player_list.h"
+
+namespace gateway
+{
+GateClientList* g_gate_clients_ = nullptr;
+}
---
+++ gateway_server/src/gate_player/gate_player_list.h
@@ -0,0 +1,19 @@
+#ifndef GATEWAY_SERVER_SRC_GATEWAY_PLAYER_GATEWAY_PLAYER_LIST_H_
+#define GATEWAY_SERVER_SRC_GATEWAY_PLAYER_GATEWAY_PLAYER_LIST_H_
+
+#include "src/common_type/common_type.h"
+
+namespace gateway
+{
+    struct GateClient
+    {
+        common::GameGuid player_id_{ 0 };
+        uint32_t game_server_id_{ 0 };
+    };
+
+    using GateClientList = std::unordered_map<uint64_t, GateClient>;
+
+    extern GateClientList* g_gate_clients_;
+}
+
+#endif//GATEWAY_SERVER_SRC_GATEWAY_PLAYER_GATEWAY_PLAYER_LIST_H_
