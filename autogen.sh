1b80e20eb248a09c991e28b24bf4befb63baecf1
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-08-03 20:12:47 +0800

    autogen.sh

---
+++ autogen.sh
@@ -0,0 +1,32 @@
+#!/bin/sh
+
+python vcxproj2cmake.py
+
+cd protopb && cmake . && make -j4
+if [$? -ne 0]; then 
+    echo "faild"
+    exit 
+fi
+
+cd ..
+
+cd third_party && cmake . && make -j4
+if [$? -ne 0]; then 
+    echo "faild"
+    exit 
+fi
+
+cd ..
+
+cd common && cmake . && make -j4
+if [$? -ne 0]; then 
+    echo "faild"
+    exit 
+fi
+
+cd ..
+
+cd deploy_server && cmake . && make -j4
+if [$? -ne 0]; then 
+    echo "faild"
+    exit 
