7ab62bffce20f1eea796617fa10aabe7ed75bd4f
luyuan <luyuan_cpp@163.com>
2021-08-06 12:40:45 +0800

    client error

--- client/src/module/login/login.cpp
+++ client/src/module/login/login.cpp
@@ -1,10 +1,13 @@
 #include "login.h"
 
+#include <atomic>
+
 void LoginModule::ReadyGo()
 {
     LoginRequest request;
-    static uint32_t i = 100;
-    request.set_account(std::string("luhailong") + std::to_string(++i));
+    static std::atomic_uint32_t i = 100;
+    auto real_id = i.fetch_add(1);
+    request.set_account(std::string("luhailong") + std::to_string(real_id));
     request.set_password("lhl.2021");
     codec_.send(conn_, request);
 }
