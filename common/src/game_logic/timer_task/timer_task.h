e584a5d65e29cb612629c4e62376b9f6446fa96f
luyuan <luyuan_cpp@163.com>
2021-08-07 20:21:27 +0800

    master 2 server

--- common/src/game_logic/timer_task/timer_task.h
+++ common/src/game_logic/timer_task/timer_task.h
@@ -12,7 +12,7 @@ namespace common
     const uint32_t kSecondsPerHours = 60 * 60;
     const uint32_t kSecondsPerMinute = 60;
     const uint32_t kMillisecondsPerSecond = 1000;
-    const double k3Millisecond = 0.003;
+    const double k5Millisecond = 0.005;
 
 	using muduo::net::TimerId;
 	using muduo::Timestamp;
