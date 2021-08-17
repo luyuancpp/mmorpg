a8974824772d5700076301c9c50d60ee576507f3
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-08-17 19:56:44 +0800

    snow flake

--- common/src/snow_flake/snow_flake.h
+++ common/src/snow_flake/snow_flake.h
@@ -16,8 +16,8 @@ namespace common
     {
     public:
         //https://en.cppreference.com/w/cpp/chrono/duration second time byte
-        static const uint32_t kServerIdByte = 13;
-        static const uint32_t kTimeByte = 35;
+        static const uint32_t kServerIdByte = 16;
+        static const uint32_t kTimeByte = 32;
         static const uint32_t kIncrementedByte = 16;
         static const uint32_t kMaxIncremented = UINT16_MAX;
 
