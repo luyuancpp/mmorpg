fb88ba42ab842fb6d51bbc09d432c24b6bd92eb7
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-08-21 20:01:35 +0800

    game server load balance

---
+++ unit_test/game_server_load_balance/game_server_load_balance_test.cpp
@@ -0,0 +1,47 @@
+#include <gtest/gtest.h>
+
+TEST(GameServer, MainTain)
+{
+}
+
+TEST(GameServer, Crash)
+{
+}
+
+TEST(GameServer, Route)
+{
+}
+
+TEST(GameServer, WeightRoundRobin)
+{
+}
+
+TEST(GameServer, Enter)
+{
+}
+
+TEST(GameServer, Leave)
+{
+}
+
+TEST(GameServer, Login)
+{
+}
+
+TEST(GameServer, Logout)
+{
+}
+
+TEST(GameServer, Disconnect)
+{
+}
+
+TEST(GameServer, ReDisconnect)
+{
+}
+
+int32_t main(int argc, char** argv)
+{
+    testing::InitGoogleTest(&argc, argv);
+    return RUN_ALL_TESTS();
+}
