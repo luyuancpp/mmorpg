5ff4b85e2ea25869f5a54440f94b0802ee8496c3
luyuan_cpp@163.com <luyuan_cpp@163.com>
2021-09-04 01:27:18 +0800

    build proto

--- bin/config/buillall.py
+++ bin/config/buillall.py
@@ -3,8 +3,16 @@
 import os
 from os import system
 
+if not os.path.exists("cpp"):
+	os.makedirs("cpp")
+if not os.path.exists("protocpp"):
+	os.makedirs("protocpp")
+if not os.path.exists("json"):
+	os.makedirs("json")
+
 system("xlstojson.py")
 system("xlstopb.py")
 system("cd proto && buildproto.py")
 system("xlstocpp.py")
 system("md5tool.py md5copy ./cpp/ ../../common/src/game_config/")
+system("md5tool.py md5copy ./protocpp/ ../../protopb/pbc/")
