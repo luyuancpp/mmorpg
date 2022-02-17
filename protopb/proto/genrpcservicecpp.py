c10712387505e3a023017c4ea0b5942fa142d49f
luhailong <lhlcpp6@163.com>
2022-02-17 22:55:17 +0800

    cpp service

--- protopb/proto/genrpcservicecpp.py
+++ protopb/proto/genrpcservicecpp.py
@@ -1,13 +1,17 @@
 import os
 
 rpcarry = []
+servicenames = []
 pkg = ''
 cpkg = 'package'
 yourcodebegin = '///<<< BEGIN WRITING YOUR CODE'
 yourcodeend = '///<<< END WRITING YOUR CODE'
+rpcbegin = '///<<<rpc begin'
+rpcend = '///<<<rpc end'
 service = ''
 tabstr = '    '
-headmaxpart = 3
+cppmaxpart = 4
+controller = '(::google::protobuf::RpcController* controller'
 
 def parsefile(filename):
     global rpcarry
@@ -21,15 +25,19 @@ def parsefile(filename):
             if fileline.find('rpc') >= 0:
                 rpcarry.append(fileline)
             elif fileline.find(cpkg) >= 0:
-                pkg = fileline.replace(cpkg, '').replace(';', '').strip('').strip('\n')
+                pkg = fileline.replace(cpkg, '').replace(';', '').replace(' ', '').strip('\n')
             elif fileline.find('service') >= 0:
                 service = fileline.replace('service', '').replace('{', '').replace(' ', '').strip('\n')
 
-def genheadrpcstr():
-    servicestr = ""
+def genheadrpcfun():
+    servicestr = ''
+    global servicenames
+    global controller
+    servicenames = []
     for service in rpcarry:
         s = service.strip(' ').split(' ')
-        line = tabstr + 'void ' + s[1] + '(::google::protobuf::RpcController* controller,\n'
+        line = tabstr + 'void ' + s[1] + controller + '\n'
+        servicenames.append(s[1])
         line += tabstr + tabstr + 'const ' + pkg + '::' + s[2].replace('(', '').replace(')', '') + '* request,\n'
         rsp = s[4].replace('(', '').replace(')',  '').replace(';',  '').strip('\n');
         if rsp == 'google.protobuf.Empty' :
@@ -40,19 +48,34 @@ def genheadrpcstr():
         servicestr += line
     return servicestr
 
+def gencpprpcfunbegin(rpcindex):
+    global service
+    servicestr = ''
+    s = rpcarry[rpcindex]
+    s = s.strip(' ').split(' ')
+    servicestr = 'void ' + service + 'Impl::' + s[1] + controller + '\n'
+    servicestr +=  tabstr + 'const ' + pkg + '::' + s[2].replace('(', '').replace(')', '') + '* request,\n'
+    rsp = s[4].replace('(', '').replace(')',  '').replace(';',  '').strip('\n');
+    if rsp == 'google.protobuf.Empty' :
+        servicestr +=  tabstr + '::google::protobuf::Empty* response,\n'
+    else :
+        servicestr +=  tabstr + pkg + '::' + rsp + '* response,\n'
+    servicestr +=  tabstr + '::google::protobuf::Closure* done)\n{\n'
+    return servicestr
+
 def yourcode():
     return yourcodebegin + '\n' + yourcodeend + '\n'
 def headfilestart():
     return ''
-def headnamespace():
+def namespacebegin():
     return 'namespace ' + pkg + '{\n'
 def headclass():
-    return 'class ' + service + 'Impl : public ' + service + '{\npublic:\n' +  genheadrpcstr()    
+    return 'class ' + service + 'Impl : public ' + service + '{\npublic:\n' +  genheadrpcfun()    
 
-def genhfile(filename, writedir):
-    headfun = [headfilestart, headnamespace, headclass]
+def genheadfile(filename, writedir):
+    headfun = [headfilestart, namespacebegin, headclass]
     hfilename = writedir + '/' + filename.replace('.proto', '.h')
-    newhfilename = writedir + '/' + filename.replace('.proto', '.h.h')
+    newheadfilename = writedir + '/' + filename.replace('.proto', '.h.new')
     headdefine = writedir.replace('/', '_').replace('.', '').upper().strip('_')
     newstr = '#ifndef ' + headdefine + '_H_\n'
     newstr += '#define ' + headdefine + '_H_\n'
@@ -62,10 +85,10 @@ def genhfile(filename, writedir):
             part = 0
             owncode = 1 
             generated = 0
-            line = 0 
+            skipheadline = 0 
             for fileline in file:
-                if line < 3 :
-                    line += 1
+                if skipheadline < 3 :
+                    skipheadline += 1
                     continue
                 if fileline.find(yourcodebegin) >= 0:
                     generated = 0
@@ -89,21 +112,86 @@ def genhfile(filename, writedir):
             newstr += headfun[i]()
             newstr += yourcode()
 
-    newstr += "};\n}// namespace " + pkg + '\n'
+    newstr += '};\n}// namespace ' + pkg + '\n'
     newstr += '#endif//' + headdefine + '_H_\n'
-    with open(newhfilename, 'w', encoding='utf-8')as file:
+    with open(newheadfilename, 'w', encoding='utf-8')as file:
         file.write(newstr)
 
 def gencppfile(filename, writedir):
-    with open(filename,'r', encoding='utf-8') as file:
-        for fileline in file:
-            if fileline.find('rpc') < 0:
-                continue
-            rpcarry.append(fileline)
+    global cppmaxpart
+    hfilename = writedir + '/' + filename.replace('.proto', '.h')
+    cppfilename = writedir + '/' + filename.replace('.proto', '.cpp')
+    newcppfilename = writedir + '/' + filename.replace('.proto', '.cpp.new')
+    newstr = '#include "' + hfilename + '.h"\n'
+    global servicenames
+    try:
+        with open(cppfilename,'r', encoding='utf-8') as file:
+            part = 0
+            owncode = 1 
+            skipheadline = 0 
+            serviceidx = 0
+            curservicename = ''
+            nextrpcline = 0
+            for fileline in file:
+                if skipheadline < 1 :
+                    skipheadline += 1
+                    continue
+                if fileline.find(yourcodebegin) >= 0 and (part < 2 or part > 2):
+                    generated = 0
+                    owncode = 1
+                    newstr += fileline
+                    continue
+                elif fileline.find(yourcodeend) >= 0 and (part < 2 or part > 2):
+                    owncode = 0
+                    newstr += fileline + '\n'
+                    part += 1
+                    if part == 1 :
+                        newstr += namespacebegin()
+                    continue     
+                elif part == 2:
+                    if fileline.find(rpcbegin) >= 0:
+                        newstr += fileline
+                        continue
+                    elif serviceidx < len(rpcarry) and fileline.find(servicenames[serviceidx] + controller) >= 0 :
+                        curservicename = servicenames[serviceidx]
+                        owncode = 0
+                        newstr += gencpprpcfunbegin(serviceidx)
+                        continue
+                    elif fileline.find(yourcodebegin) >= 0 :
+                        newstr += yourcodebegin + ' ' + curservicename + '\n'
+                        owncode = 1
+                        continue
+                    elif fileline.find(yourcodeend) >= 0 :
+                        newstr += yourcodeend + ' ' + curservicename + '\n}\n\n'
+                        owncode = 0
+                        nextrpcline = 0
+                        serviceidx += 1  
+                        continue
+                    elif fileline.find(rpcend) >= 0:
+                        owncode = 0
+                        print(rpcarry)
+                        while serviceidx < len(rpcarry) :
+                            newstr += gencpprpcfunbegin(serviceidx)
+                            newstr += yourcodebegin + ' ' + curservicename + '\n'
+                            newstr += yourcodeend + ' ' + curservicename + '\n}\n\n'
+                            serviceidx += 1 
+                        newstr += fileline
+                        part += 1 
+                        continue
+                if owncode == 1:
+                    newstr += fileline
+                    continue                
+                if part > cppmaxpart :
+                    break
+    except FileNotFoundError:
+        newstr += genheadrpcfun()
+    newstr += '}// namespace ' + pkg + '\n'
+    with open(newcppfilename, 'w', encoding='utf-8')as file:
+        file.write(newstr)
 
 def generate(filename, writedir):
     parsefile(filename)
-    genhfile(filename, writedir)
+    genheadfile(filename, writedir)
     gencppfile(filename, writedir)
 
-generate('1.proto', './src/service/')
+generate('1.proto', './src/service')
