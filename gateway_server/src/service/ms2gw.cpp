29c1c8619d936fbb9a85516c8cb1e6a5b02d5601
luhailong <lhlcpp6@163.com>
2022-02-20 14:18:47 +0800

    rpc gen

--- gateway_server/src/service/ms2gw.cpp
+++ gateway_server/src/service/ms2gw.cpp
@@ -15,17 +15,17 @@ using namespace  gateway;
 using namespace  common;
 ///<<< END WRITING YOUR CODE
 
-namespace ms2gw
-{
+namespace ms2gw{
 	///<<< BEGIN WRITING YOUR CODE
 	///<<< END WRITING YOUR CODE
+
     /// ///<<<rpc begin
-    void Ms2gwServiceImpl::StartGS(::google::protobuf::RpcController* controller,
-        const ::ms2gw::StartGSRequest* request, 
-        ::google::protobuf::Empty* response,
-        ::google::protobuf::Closure* done)
-    {
-        ///<<< BEGIN WRITING YOUR CODE
+void Ms2gwServiceImpl::StartGS(::google::protobuf::RpcController* controller,
+    const ms2gw::StartGSRequest* request,
+    ::google::protobuf::Empty* response,
+    ::google::protobuf::Closure* done)
+{
+///<<< BEGIN WRITING YOUR CODE StartGS
         AutoRecycleClosure cp(done);
         InetAddress gs_addr(request->ip(), request->port());
         for (auto e : reg.view<InetAddress>())
@@ -46,15 +46,15 @@ namespace ms2gw
         SessionReg::GetSingleton().emplace<InetAddress>(e, gs_addr);
         SessionReg::GetSingleton().emplace<uint32_t>(e, request->node_id());
         LOG_INFO << "connect to game server " << gs_addr.toIpPort() << " server id " << request->node_id();
-        ///<<< END WRITING YOUR CODE
-    }
+///<<< END WRITING YOUR CODE StartGS
+}
 
-    void Ms2gwServiceImpl::StopGS(::google::protobuf::RpcController* controller, 
-        const ::ms2gw::StopGSRequest* request, 
-        ::google::protobuf::Empty* response, 
-        ::google::protobuf::Closure* done)
-    {
-        ///<<< BEGIN WRITING YOUR CODE 
+void Ms2gwServiceImpl::StopGS(::google::protobuf::RpcController* controller,
+    const ms2gw::StopGSRequest* request,
+    ::google::protobuf::Empty* response,
+    ::google::protobuf::Closure* done)
+{
+///<<< BEGIN WRITING YOUR CODE StopGS
         AutoRecycleClosure cp(done);
         for (auto e : SessionReg::GetSingleton().view<InetAddress>())
         {
@@ -67,15 +67,15 @@ namespace ms2gw
             SessionReg::GetSingleton().destroy(e);
             break;
         }
-        ///<<< END WRITING YOUR CODE
-    }
+///<<< END WRITING YOUR CODE StopGS
+}
 
-    void Ms2gwServiceImpl::PlayerEnterGS(::google::protobuf::RpcController* controller, 
-        const ::ms2gw::PlayerEnterGSRequest* request, 
-        ::google::protobuf::Empty* response, 
-        ::google::protobuf::Closure* done)
-    {
-        ///<<< BEGIN WRITING YOUR CODE 
+void Ms2gwServiceImpl::PlayerEnterGS(::google::protobuf::RpcController* controller,
+    const ms2gw::PlayerEnterGSRequest* request,
+    ::google::protobuf::Empty* response,
+    ::google::protobuf::Closure* done)
+{
+///<<< BEGIN WRITING YOUR CODE PlayerEnterGS
         AutoRecycleClosure cp(done);
         auto it = g_client_sessions_->find(request->connection_id());
         if (it == g_client_sessions_->end())
@@ -83,11 +83,11 @@ namespace ms2gw
             return;
         }
         it->second.node_id_ = request->node_id();
-        ///<<< END WRITING YOUR CODE
-    }
+///<<< END WRITING YOUR CODE PlayerEnterGS
+}
+
 ///<<<rpc end
 ///<<< BEGIN WRITING YOUR CODE 
 ///<<< END WRITING YOUR CODE
-}
 
-
+}// namespace ms2gw
--- gateway_server/src/service/ms2gw.h
+++ gateway_server/src/service/ms2gw.h
@@ -1,6 +1,6 @@
-#ifndef GATEWAY_SERVER_MASTER_SERVICE_SERVICE_MASTER_H_
-#define GATEWAY_SERVER_MASTER_SERVICE_SERVICE_MASTER_H_
-
+#ifndef GATEWAY_SERVER_SRC_SERVICE_MS2GW_H_
+#define GATEWAY_SERVER_SRC_SERVICE_MS2GW_H_
+#include "ms2gw.pb.h"
 #include "ms2gw.pb.h"
 ///<<< BEGIN WRITING YOUR CODE
 namespace gateway
@@ -8,28 +8,28 @@ namespace gateway
     class GatewayServer;
 }//namespace gateway
 ///<<< END WRITING YOUR CODE
-namespace ms2gw
-{
+namespace ms2gw{
     ///<<< BEGIN WRITING YOUR CODE
     ///<<< END WRITING YOUR CODE
-    class Ms2gwServiceImpl : public Ms2gwService
-    {
-    public:
-        virtual void StartGS(::google::protobuf::RpcController* controller,
-            const ::ms2gw::StartGSRequest* request,
-            google::protobuf::Empty* response,
-            ::google::protobuf::Closure* done)override;
-        virtual void StopGS(::google::protobuf::RpcController* controller,
-            const ::ms2gw::StopGSRequest* request,
-            ::google::protobuf::Empty* response,
-            ::google::protobuf::Closure* done)override;
-        virtual void PlayerEnterGS(::google::protobuf::RpcController* controller,
-            const ::ms2gw::PlayerEnterGSRequest* request,
-            ::google::protobuf::Empty* response,
-            ::google::protobuf::Closure* done)override;
+class Ms2gwServiceImpl : public Ms2gwService{
+public:
+    void StartGS(::google::protobuf::RpcController* controller,
+        const ms2gw::StartGSRequest* request,
+        ::google::protobuf::Empty* response,
+        ::google::protobuf::Closure* done)override;
+
+    void StopGS(::google::protobuf::RpcController* controller,
+        const ms2gw::StopGSRequest* request,
+        ::google::protobuf::Empty* response,
+        ::google::protobuf::Closure* done)override;
+
+    void PlayerEnterGS(::google::protobuf::RpcController* controller,
+        const ms2gw::PlayerEnterGSRequest* request,
+        ::google::protobuf::Empty* response,
+        ::google::protobuf::Closure* done)override;
+
 		///<<< BEGIN WRITING YOUR CODE
 		///<<< END WRITING YOUR CODE
-    };
-}//namespace gw2ms
-
-#endif//GATEWAY_SERVER_MASTER_SERVICE_SERVICE_MASTER_H_
+};
+}// namespace ms2gw
+#endif//GATEWAY_SERVER_SRC_SERVICE_MS2GW_H_
