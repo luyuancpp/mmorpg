ba0321cd1af5676acdaffacd34626f1db14979d9
luhailong <lhlcpp6@163.com>
2022-02-20 12:24:39 +0800

    gen rpc code

--- deploy_server/src/service/service.cpp
+++ deploy_server/src/service/service.cpp
@@ -1,5 +1,6 @@
 #include "service.h"
 
+///<<< BEGIN WRITING YOUR CODE
 #include "muduo/base/Logging.h"
 
 #include "src/deploy_server.h"
@@ -9,82 +10,96 @@
 #include "src/return_code/error_code.h"
 
 using namespace common;
+///<<< END WRITING YOUR CODE
 
 namespace deploy
 {
-    void DeployServiceImpl::ServerInfo(::google::protobuf::RpcController* controller, 
-        const ::deploy::ServerInfoRequest* request, 
-        ::deploy::ServerInfoResponse* response, 
-        ::google::protobuf::Closure* done)
+///<<< BEGIN WRITING YOUR CODE
+///<<< END WRITING YOUR CODE
+///<<<rpc begin
+void DeployServiceImpl::ServerInfo(::google::protobuf::RpcController* controller, 
+    const ::deploy::ServerInfoRequest* request, 
+    ::deploy::ServerInfoResponse* response, 
+    ::google::protobuf::Closure* done)
+{
+    ///<<< BEGIN WRITING YOUR CODE
+    AutoRecycleClosure cp(done);
+    auto group_id = request->group();
+    auto& servers_deploy = *response->mutable_info();
+    if (group_id > 0)
     {
-        AutoRecycleClosure cp(done);
-        auto group_id = request->group();
-        auto& servers_deploy = *response->mutable_info();
-        if (group_id > 0)
-        {
-            std::string where_case = std::to_string(group_id) + " = id  ";
-            db_->LoadOne(*servers_deploy.mutable_database_info(), where_case);
-            db_->LoadOne(*servers_deploy.mutable_login_info(), where_case);
-            db_->LoadOne(*servers_deploy.mutable_master_info(), where_case);
-            db_->LoadOne(*servers_deploy.mutable_gateway_info(), where_case);
-            db_->LoadOne(*servers_deploy.mutable_redis_info(), where_case);
-        }
-        else
-        {
-            std::string where_case = std::to_string(request->region_id()) + " = region_id  ";
-            db_->LoadAll<::master_server_db>(*response->mutable_region_masters(), where_case);
-        }
-        LoadRegionDeploy(request->region_id(), servers_deploy.mutable_regin_info());
+        std::string where_case = std::to_string(group_id) + " = id  ";
+        db_->LoadOne(*servers_deploy.mutable_database_info(), where_case);
+        db_->LoadOne(*servers_deploy.mutable_login_info(), where_case);
+        db_->LoadOne(*servers_deploy.mutable_master_info(), where_case);
+        db_->LoadOne(*servers_deploy.mutable_gateway_info(), where_case);
+        db_->LoadOne(*servers_deploy.mutable_redis_info(), where_case);
     }
-
-    void DeployServiceImpl::StartGS(::google::protobuf::RpcController* controller, 
-        const ::deploy::StartGSRequest* request,
-        ::deploy::StartGSResponse* response,
-        ::google::protobuf::Closure* done)
+    else
     {
-        AutoRecycleClosure cp(done);
-        auto& server_deploy = *response->mutable_my_info();
-        auto& client_info = request->rpc_client();
-        muduo::net::InetAddress ip_port(client_info.ip(), client_info.port());
-        if (server_deploy.id() > 0)
-        {
-            g_deploy_server->reuse_game_id().Emplace(ip_port.toIpPort(), server_deploy.id());
-            return;
-        }
-        server_deploy.set_ip(request->my_info().ip());
-        uint32_t node_id = g_deploy_server->CreateGSId();
-        LOG_INFO << "new server id " << node_id;
-        server_deploy.set_id(node_id);
-        server_deploy.set_port(node_id + kGSBeginPort);
-
-        g_deploy_server->reuse_game_id().Emplace(ip_port.toIpPort(), node_id);
-        g_deploy_server->SaveGSDb();
-        //g_deploy_server->LogReuseInfo();
+        std::string where_case = std::to_string(request->region_id()) + " = region_id  ";
+        db_->LoadAll<::master_server_db>(*response->mutable_region_masters(), where_case);
     }
+    LoadRegionDeploy(request->region_id(), servers_deploy.mutable_regin_info());
+    ///<<< END WRITING YOUR CODE
+}
 
-    void DeployServiceImpl::StartRegionServer(::google::protobuf::RpcController* controller, 
-        const ::deploy::RegionInfoRequest* request,
-        ::deploy::RegionInfoResponse* response,
-        ::google::protobuf::Closure* done)
+void DeployServiceImpl::StartGS(::google::protobuf::RpcController* controller, 
+    const ::deploy::StartGSRequest* request,
+    ::deploy::StartGSResponse* response,
+    ::google::protobuf::Closure* done)
+{
+    ///<<< BEGIN WRITING YOUR CODE
+    AutoRecycleClosure cp(done);
+    auto& server_deploy = *response->mutable_my_info();
+    auto& client_info = request->rpc_client();
+    muduo::net::InetAddress ip_port(client_info.ip(), client_info.port());
+    if (server_deploy.id() > 0)
     {
-        AutoRecycleClosure cp(done);
-        LoadRegionDeploy(request->region_id(), response->mutable_info());
+        g_deploy_server->reuse_game_id().Emplace(ip_port.toIpPort(), server_deploy.id());
+        return;
     }
+    server_deploy.set_ip(request->my_info().ip());
+    uint32_t node_id = g_deploy_server->CreateGSId();
+    LOG_INFO << "new server id " << node_id;
+    server_deploy.set_id(node_id);
+    server_deploy.set_port(node_id + kGSBeginPort);
 
-    void DeployServiceImpl::LoadRegionDeploy(::google::protobuf::RpcController* controller,
-        const ::deploy::RegionInfoRequest* request, 
-        ::deploy::RegionInfoResponse* response, 
-        ::google::protobuf::Closure* done)
-    {
-        AutoRecycleClosure cp(done);
-        LoadRegionDeploy(request->region_id(), response->mutable_info());
-    }
+    g_deploy_server->reuse_game_id().Emplace(ip_port.toIpPort(), node_id);
+    g_deploy_server->SaveGSDb();
+    //g_deploy_server->LogReuseInfo();
+    ///<<< END WRITING YOUR CODE
+}
 
-    void DeployServiceImpl::LoadRegionDeploy(uint32_t region_id,
-        ::region_server_db* response)
-    {
-        std::string where_case = std::to_string(region_id) + " = id  ";
-        db_->LoadOne(*response, where_case);
-    }
+void DeployServiceImpl::StartRegionServer(::google::protobuf::RpcController* controller, 
+    const ::deploy::RegionInfoRequest* request,
+    ::deploy::RegionInfoResponse* response,
+    ::google::protobuf::Closure* done)
+{
+    ///<<< BEGIN WRITING YOUR CODE
+    AutoRecycleClosure cp(done);
+    LoadRegionDeploy(request->region_id(), response->mutable_info());
+    ///<<< END WRITING YOUR CODE
+}
 
+void DeployServiceImpl::LoadRegionDeploy(::google::protobuf::RpcController* controller,
+    const ::deploy::RegionInfoRequest* request, 
+    ::deploy::RegionInfoResponse* response, 
+    ::google::protobuf::Closure* done)
+{
+    ///<<< BEGIN WRITING YOUR CODE
+    AutoRecycleClosure cp(done);
+    LoadRegionDeploy(request->region_id(), response->mutable_info());
+    ///<<< END WRITING YOUR CODE
+}
+
+///<<<rpc end
+///<<< BEGIN WRITING YOUR CODE
+void DeployServiceImpl::LoadRegionDeploy(uint32_t region_id,
+::region_server_db* response)
+{
+std::string where_case = std::to_string(region_id) + " = id  ";
+db_->LoadOne(*response, where_case);
+}
+///<<< END WRITING YOUR CODE
 }//namespace deploy
--- deploy_server/src/service/service.h
+++ deploy_server/src/service/service.h
@@ -1,24 +1,18 @@
 #ifndef DEPLOY_SERVER_SRC_SERVICE_SERVICE_H_
 #define DEPLOY_SERVER_SRC_SERVICE_SERVICE_H_
-
+///<<< BEGIN WRITING YOUR CODE
 #include "src/mysql_database/mysql_database.h"
 
 #include "deploy.pb.h"
+///<<< END WRITING YOUR CODE
 
 namespace deploy
 {
+    ///<<< BEGIN WRITING YOUR CODE
+    ///<<< END WRITING YOUR CODE
     class DeployServiceImpl : public DeployService
     {
     public:
-        using ServerId = uint32_t;
-        using MysqlClientPtr = std::shared_ptr<common::MysqlDatabase>;
-        using GSMap = std::unordered_map<uint32_t, ::game_server_db>;
-
-        void set_player_mysql_client(MysqlClientPtr& ptr)
-        {
-            db_ = ptr;
-        }
-
         virtual void ServerInfo(::google::protobuf::RpcController* controller,
             const ::deploy::ServerInfoRequest* request,
             ::deploy::ServerInfoResponse* response,
@@ -38,13 +32,22 @@ namespace deploy
 			const ::deploy::RegionInfoRequest* request,
 			::deploy::RegionInfoResponse* response,
 			::google::protobuf::Closure* done)override;
+	///<<< BEGIN WRITING YOUR CODE
+		using ServerId = uint32_t;
+		using MysqlClientPtr = std::shared_ptr<common::MysqlDatabase>;
+		using GSMap = std::unordered_map<uint32_t, ::game_server_db>;
+
+		void set_player_mysql_client(MysqlClientPtr& ptr)
+		{
+			db_ = ptr;
+		}
 
     private:
         void LoadRegionDeploy(uint32_t region_id, ::region_server_db* response);
 
         MysqlClientPtr db_;
         GSMap logic_server_map_;
-        
+        ///<<< END WRITING YOUR CODE
     };
 }//namespace deploy
 
