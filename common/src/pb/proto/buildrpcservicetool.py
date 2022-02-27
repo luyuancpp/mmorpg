7b220d30aeb0cfb240e938d903cc28227f8fbbac
luhailong <lhlcpp6@163.com>
2022-02-27 15:04:12 +0800

    gen sol lua

--- common/src/pb/proto/buildrpcservicetool.py
+++ common/src/pb/proto/buildrpcservicetool.py
@@ -240,3 +240,12 @@ def generate(filename, writedir):
     md5copy(filename, writedir, '.h')
     md5copy(filename, writedir, '.cpp')
 
+generate('gw2l.proto', '../../../../login_server/src/service')
+generate('l2db.proto', '../../../../database_server/src/service')
+generate('ms2g.proto', '../../../../game_server/src/service')
+generate('rg2g.proto', '../../../../game_server/src/service')
+generate('node2deploy.proto', '../../../../deploy_server/src/service')
+generate('ms2gw.proto', '../../../../gateway_server/src/service')
+generate('gw2ms.proto', '../../../../master_server/src/service')
+generate('l2ms.proto', '../../../../master_server/src/service')
+generate('g2ms.proto', '../../../../master_server/src/service')
