c6429132d9773f8cef7a6f29d0f59b2703ebee26
luhailong <lhlcpp6@163.com>
2022-03-05 22:59:50 +0800

    gen rpc ap

--- common/src/pb/proto/buildrpcserviceid.py
+++ common/src/pb/proto/buildrpcserviceid.py
@@ -14,6 +14,9 @@ local.servicenames = []
 local.servicebeginid = []
 local.service = ''
 local.hfilename = ''
+local.rpcmsgnameid = []
+local.msgcount = 0
+local.cppfilename = 'msgmap.cpp'
 
 threads = []
 local.pkg = ''
@@ -21,7 +24,7 @@ tabstr = '    '
 cpkg = 'package'
 servicedir = './service/'
 writedir = '../pbc/'
-perserviceidcount = 200
+perserviceidcount = 150
 
 if not os.path.exists(servicedir):
     os.makedirs(servicedir)
@@ -31,54 +34,60 @@ def parsefile(filename, fileid):
     local.pkg = ''
     local.service = ''
     rpcbegin = 0 
-    
+    serviceid = fileid * perserviceidcount
     with open(filename,'r', encoding='utf-8') as file:
         for fileline in file:
             if fileline.find('rpc') >= 0 and rpcbegin == 1:
-                local.rpcarry.append(fileline)
+                s = fileline.strip(' ').split(' ')
+                idstr = local.pkg.upper() + '_' + s[1].upper()  + '_ID'
+                line = 'const uint32_t ' + idstr + ' = ' + str(serviceid)
+                line += ';\n'
+                rq = s[2].replace('(', '').replace(')', '')
+                pbfullname = local.pkg + '.' + s[2].replace('(', '').replace(')', '')
+                local.rpcmsgnameid.append([[s[1], rq, local.pkg, local.service],serviceid])
+                serviceid += 1
             elif fileline.find(cpkg) >= 0:
                 local.pkg = fileline.replace(cpkg, '').replace(';', '').replace(' ', '').strip('\n')
             elif fileline.find('service ') >= 0:
                 rpcbegin = 1
                 local.service = fileline.replace('service', '').replace('{', '').replace(' ', '').strip('\n')
 
-def genheadrpcfun(fileid):
-    servicestr = '\n'
-    local.servicenames = []
-    serviceid = fileid * perserviceidcount
-    for service in local.rpcarry:
-        s = service.strip(' ').split(' ')
-        line = 'const uint32_t ' + local.pkg.upper() + '_'
-        line +=  '_' + s[1].upper()  + '_ID = ' + str(serviceid)
-        line += ';\n'
-        serviceid += 1
-        servicestr += line
-    #print(servicestr)
-    return servicestr
-
-def genheadfile(filename, fileid):
-    hfullfilename = writedir + '/' + filename.replace('.proto', 'id.h')
-    folder_path, local.hfilename = os.path.split(hfullfilename)    
-    newheadfilename = servicedir + local.hfilename.replace('.proto', 'id.h')
-    headdefine = writedir.replace('/', '_').replace('.', '').upper().strip('_') + '_' + filename.replace('.proto', '').upper() + 'SERVICE_ID'
-    newstr = '#ifndef ' + headdefine + '_H_\n'
-    newstr += '#define ' + headdefine + '_H_\n'
-    newstr += genheadrpcfun(fileid) + '\n'
-    newstr += '#endif//' + headdefine + '_H_\n'
-    with open(newheadfilename, 'w', encoding='utf-8')as file:
+def genmsgidcpp(fullfilename):
+    newstr = '#include <array>\n'
+    newstr += '#include <string>\n'
+    newstr += '#include <unordered_map>\n\n'
+    newstr += 'std::unordered_map<std::string, uint32_t> g_msgid{\n'
+    #msg 2 id
+    for kv in local.rpcmsgnameid:
+        newstr += '{"' + kv[0][2] + '.' + kv[0][0] + '", ' + str(kv[1]) + '},\n'
+    newstr = newstr.strip('\n').strip(',')
+    newstr += '};\n'
+    newstr += '\nstd::array<std::string, ' + str(local.msgcount) + '> g_idservice;\n'
+    newstr += 'void InitMsgId2Servcie()\n{\n'
+    #id 2 service 
+    for kv in local.rpcmsgnameid:
+        newstr += tabstr + 'g_idservice[' + str(kv[1]) + '] = "' +  kv[0][2] + '.' + kv[0][3] +'";\n'
+    newstr += '}\n'
+    #id 2 service 
+    newstr += '\nstd::array<std::string, ' + str(local.msgcount) + '> g_idmsg;\n'
+    newstr += 'void InitMsgId2Msg()\n{\n'
+    for kv in local.rpcmsgnameid:
+        newstr += tabstr + 'g_idmsg[' + str(kv[1]) + '] = "' + kv[0][1] +'";\n'
+    newstr += '}\n'
+    with open(fullfilename, 'w', encoding='utf-8')as file:
         file.write(newstr)
 
-def md5copy(filename, fileextend):
-        gennewfilename = servicedir + filename.replace('.proto', fileextend)
+def md5copy(hfullfilename):
+        gennewfilename = servicedir + local.cppfilename
         filenamemd5 = gennewfilename + '.md5'
         error = None
-        emptymd5 = False
-        if  not os.path.exists(filenamemd5):
-            emptymd5 = True
+        copy = False
+        if  not os.path.exists(filenamemd5) or not os.path.exists(hfullfilename):
+            copy = True
         else:
-            error = md5tool.check_against_md5_file(gennewfilename, filenamemd5)              
-        hfullfilename = writedir + '/' + filename.replace('.proto', fileextend)
-        if error == None and os.path.exists(hfullfilename) and emptymd5 == False:
+            error = md5tool.check_against_md5_file(hfullfilename, filenamemd5)              
+        
+        if error == None and copy == False:
             return
         #print("copy %s ---> %s" % (gennewfilename, hfullfilename))
         md5tool.generate_md5_file_for(gennewfilename, filenamemd5)
@@ -86,34 +95,16 @@ def md5copy(filename, fileextend):
 
 def generate(filename, fileid):
     parsefile(filename, fileid)
-    genheadfile(filename, fileid)
-    md5copy(filename, 'id.h')
 
-genfile = protofilearray.genfile
-
-class myThread (threading.Thread):
-    def __init__(self, filename,  fileid):
-        threading.Thread.__init__(self)
-        self.filename = str(filename)
-        self.fileid = fileid
-    def run(self):
-        generate(self.filename, self.fileid)
+genfile = ['c2gs.proto']
 
 def main():
     filelen = len(genfile)
-    global threads
-    step = filelen / cpu_count() + 1
-    if cpu_count() > filelen:
-        for i in range(0, filelen):
-            t = myThread( genfile[i][0],  i )
-            t.start()
-            threads.append(t)
-    else :
-        for i in range(0, cpu_count()):
-            for j in range(i, i * step) :
-                t = myThread(genfile[j][0],  j)
-                t.start()
-                threads.append(t)
-    for t in threads :
-        t.join()
+    local.msgcount = filelen * perserviceidcount
+    for i in range(0, filelen):
+           generate(genfile[i], i)
 main()
+genfilename = servicedir + local.cppfilename
+fullfilename = writedir + local.cppfilename
+genmsgidcpp(genfilename)
+md5copy(fullfilename)
