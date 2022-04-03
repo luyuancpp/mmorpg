250cdade6e702a29fa11f9a4552b0464074a880d
luhailong <lhlcpp6@163.com>
2022-04-03 20:42:18 +0800

    clear code

--- gateway_server/src/service/open_service.cpp
+++ gateway_server/src/service/open_service.cpp
@@ -1,14 +1,10 @@
 #include <unordered_set>
-#include "logic_proto/team_ms.pb.h"
-#include "logic_proto/team_gs.pb.h"
-#include "logic_proto/scene_gs.pb.h"
-#include "logic_proto/scene_ms.pb.h"
+#include "logic_proto/scene_client_player.pb.h"
+#include "logic_proto/team_client_player.pb.h"
 namespace gateway
 {
 std::unordered_set<std::string> g_open_player_services;
 void OpenPlayerServcie()
 {
-    g_open_player_services.emplace("gsplayerservice.PlayerTeamService");
-    g_open_player_services.emplace("gsplayerservice.PlayerSceneService");
 }
 }//namespace gateway
