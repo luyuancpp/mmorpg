8709829d50c96f8924a9fec27f347bda31320364
luhailong <lhlcpp6@163.com>
2022-05-03 12:28:41 +0800

    clear code

--- master_server/src/service/logic/msscene_server_player.h
+++ master_server/src/service/logic/msscene_server_player.h
@@ -3,8 +3,6 @@
 #include "logic_proto/scene_server_player.pb.h"
 ///<<< BEGIN WRITING YOUR CODE
 ///<<< END WRITING YOUR CODE
-///<<< BEGIN WRITING YOUR CODE
-///<<< END WRITING YOUR CODE
 class ServerPlayerSceneServiceImpl : public PlayerService {
 public:
     using PlayerService::PlayerService;
