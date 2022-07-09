7ee0536936780ddae594f7b3ec62f67c8dc73302
luhailong <lhlcpp6@163.com>
2022-07-09 12:14:48 +0800

    test case

--- bin/script/client/service/c_scene_client_player.lua
+++ bin/script/client/service/c_scene_client_player.lua
@@ -1,23 +1,46 @@
 ---<<< BEGIN WRITING YOUR CODE
 ---<<< END WRITING YOUR CODE
 
+local  own_server_scene1 = 4294967298
+local  own_server_scene2 = 4294967297
+local  cross_server_scene1 = 1
+local  cross_server_scene2 = 2
+
 ---<<<rpc begin
 function EnterSceneC2SProcess(request, response)
 ---<<< BEGIN WRITING YOUR CODE 
 ---<<< END WRITING YOUR CODE 
 end
 
+function ChangeGsScene(response, form_scene, to_scene)
+	if (response:scene_info().scene_id == form_scene) then	
+		LogInfo(response:DebugString())
+		message = EnterSeceneC2SRequest.new()
+		message:mutable_scene_info().scene_id = to_scene
+		player:send(message)
+	end
+end
+
 function PushEnterSceneS2CProcess(request, response)
 ---<<< BEGIN WRITING YOUR CODE 
-	LogInfo(response:DebugString())
+	--连续切换有问题
+	ChangeGsScene(response, own_server_scene1, cross_server_scene1)
+	ChangeGsScene(response, own_server_scene1, cross_server_scene2)
+	ChangeGsScene(response, own_server_scene2, cross_server_scene1)
+	ChangeGsScene(response, own_server_scene2, cross_server_scene2)
+	ChangeGsScene(response, cross_server_scene1, own_server_scene1)
+	ChangeGsScene(response, cross_server_scene1, own_server_scene2)
+	ChangeGsScene(response, cross_server_scene2, own_server_scene1)
+	ChangeGsScene(response, cross_server_scene2, own_server_scene2)
+
 ---<<< END WRITING YOUR CODE 
 end
 
 function PushSceneInfoS2CProcess(request, response)
 ---<<< BEGIN WRITING YOUR CODE 
-	LogInfo(response:DebugString());
+	--LogInfo(response:DebugString());
 	message = EnterSeceneC2SRequest.new()
-	message:mutable_scene_info().scene_confid = 2
+	message:mutable_scene_info().scene_id = own_server_scene1
 	player:send(message)
 ---<<< END WRITING YOUR CODE 
 end
