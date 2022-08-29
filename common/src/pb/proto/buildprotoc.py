a2ebd772ad706e210f8e7ffe41cf0b8b8518122b
luyuancpp <lhlcpp6@163.com>
2022-08-29 17:47:12 +0800

    clear code

--- common/src/pb/proto/buildprotoc.py
+++ common/src/pb/proto/buildprotoc.py
@@ -52,6 +52,7 @@ gen_protoc('./logic_proto/', '../../../../third_party/protobuf/src/', '../pbc/')
 gen_protoc('./component_proto/', '../../../../third_party/protobuf/src/', '../pbc/')
 gen_protoc('./event_proto/', '../../../../third_party/protobuf/src/', '../pbc/')
 genmd5('./')
+genmd5('./component_proto/')
 genmd5('./logic_proto/')
 genmd5('./event_proto/')
 
