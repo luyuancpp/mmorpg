fbc38212364f6f4ae1b0e57db936bb80b05b9b42
luyuancpp <lhlcpp6@163.com>
2022-11-01 15:07:12 +0800

    clear code

--- common/src/pb/proto/buildopenplayerservice.py
+++ common/src/pb/proto/buildopenplayerservice.py
@@ -4,18 +4,17 @@ import md5tool
 import shutil
 
 tabstr = '    '
-servicedir = './md5/'
-playerservicedir = '../../../../gateway_server/src/service'
+md5dir = './md5/'
 openfilename = 'open_service.cpp'
 
-if not os.path.exists(servicedir):
-    os.makedirs(servicedir)
+if not os.path.exists(md5dir):
+    os.makedirs(md5dir)
 
 def gen(readfilename, filename):
-    fullfilename = servicedir + filename
+    destfilename = md5dir + filename
     newstr =  '#include <unordered_set>\n'
     newstr += 'std::unordered_set<uint32_t> g_open_player_msgids{\n'
-    with open(servicedir + readfilename,'r', encoding='utf-8') as file:
+    with open(md5dir + readfilename,'r', encoding='utf-8') as file:
         for fileline in file:
             if fileline.find('};') >= 0:
                 msgid = fileline.split(',')[1].replace('}', '').replace(';', '')
@@ -24,13 +23,13 @@ def gen(readfilename, filename):
             elif fileline.find('C2SRequest') >= 0:
                 msgid = fileline.split(',')[1].replace('}', '').replace('"', '')
                 newstr += tabstr  + msgid + ',\n'
-    with open(fullfilename, 'w', encoding='utf-8')as file:
+    with open(destfilename, 'w', encoding='utf-8')as file:
         file.write(newstr)
 
-def md5copy(filename, writedir):
+def md5copy(filename, destdir):
     if not filename.endswith("open_service.cpp"):
         return
-    gennewfilename = servicedir + filename
+    gennewfilename = md5dir + filename
     filenamemd5 = gennewfilename + '.md5'
     error = None
     emptymd5 = False
@@ -38,12 +37,12 @@ def md5copy(filename, writedir):
         emptymd5 = True
     else:
         error = md5tool.check_against_md5_file(gennewfilename, filenamemd5)              
-    fullfilename = writedir + '/' + filename
-    if error == None and os.path.exists(fullfilename) and emptymd5 == False:
+    destfilename = destdir + '/' + filename
+    if error == None and os.path.exists(destfilename) and emptymd5 == False:
         return
-    print("copy %s ---> %s" % (gennewfilename, fullfilename))
+    print("copy %s ---> %s" % (gennewfilename, destfilename))
     md5tool.generate_md5_file_for(gennewfilename, filenamemd5)
-    shutil.copy(gennewfilename, fullfilename)
+    shutil.copy(gennewfilename, destfilename)
 
 gen('logic_proto/msgmap.cpp', openfilename)
 md5copy(openfilename, '../../../../gateway_server/src/service')
