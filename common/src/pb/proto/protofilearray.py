72a23f6139d864d7e09cbeda103d135f164d110f
luyuancpp <lhlcpp6@163.com>
2022-11-01 17:42:00 +0800

    controller_server

--- common/src/pb/proto/protofilearray.py
+++ common/src/pb/proto/protofilearray.py
@@ -3,7 +3,7 @@ import os
 genfile = [['login_service.proto', '../../../../login_server/src/service/'],
 ['db_service.proto', '../../../../database_server/src/service/'],
 ['deploy_service.proto', '../../../../deploy_server/src/service/'],
-['ms_service.proto', '../../../../master_server/src/service/'],
+['ms_service.proto', '../../../../controller_server/src/service/'],
 ['gs_service.proto', '../../../../game_server/src/service/'],
 ['gw_service.proto', '../../../../gateway_server/src/service/']
 ]
