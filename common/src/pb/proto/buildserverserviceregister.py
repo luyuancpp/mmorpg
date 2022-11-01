a9aa8b6a682b12c278548426b884abc882283c4f
luyuancpp <lhlcpp6@163.com>
2022-11-01 19:16:37 +0800

    clear code

--- common/src/pb/proto/buildserverserviceregister.py
+++ common/src/pb/proto/buildserverserviceregister.py
@@ -65,9 +65,9 @@ def md5copy(writedfilename, destfilename):
 
 scanservice()
 genheadfile('./md5/server_service.h')
-gencppfile('./md5/ms_server_service.cpp', buildpublic.controller_file_prefix)
+gencppfile('./md5/controller_server_service.cpp', buildpublic.controller_file_prefix)
 gencppfile('./md5/gs_server_service.cpp', buildpublic.gs_file_prefix)
-md5copy('./md5/ms_server_service.cpp', '../../../../controller_server/src/service/logic/controller_server_service.cpp')
+md5copy('./md5/controller_server_service.cpp', '../../../../controller_server/src/service/logic/controller_server_service.cpp')
 md5copy('./md5/gs_server_service.cpp', '../../../../game_server/src/service/logic/gs_server_service.cpp')
 md5copy('./md5/server_service.h', '../../../../controller_server/src/service/logic/server_service.h')
 md5copy('./md5/server_service.h', '../../../../game_server/src/service/logic/server_service.h')
