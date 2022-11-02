a4b39cddb59459e6387c3a04904563dfaa6952fe
luyuancpp <lhlcpp6@163.com>
2022-11-02 11:27:40 +0800

    clear code

--- common/src/pb/proto/buildpublic.py
+++ common/src/pb/proto/buildpublic.py
@@ -2,13 +2,12 @@
 controller_file_prefix = 'controller_'
 gs_file_prefix = 'gs_'
 rg_file_prefix = 'rg_'
-rg_file_suffix = '_rg'
 
 def is_server_proto(filename):
     return (filename.find('client_player') < 0 and filename.find('server_player') < 0) or  filename.find(rg_file_prefix) >= 0 
 
 def is_gs_and_controller_server_proto(filename):
-    return filename.find('client_player') < 0 and filename.find('server_player') < 0 and  filename.find(rg_file_prefix) < 0  and  filename.find(rg_file_suffix) < 0 
+    return filename.find('client_player') < 0 and filename.find('server_player') < 0 and  filename.find(rg_file_prefix) < 0  
 
 def is_client_proto(filename):
     return filename.find('client_player') >= 0 
