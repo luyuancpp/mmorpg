fa18f7d9e6fc2ab3b3538d12d5eb5ac80ffcd372
luyuancpp <lhlcpp6@163.com>
2022-11-07 16:41:11 +0800

    rever code

--- game_server/src/service/logic_proto/scene_server_player.cpp
+++ game_server/src/service/logic_proto/scene_server_player.cpp
@@ -2,6 +2,8 @@
 #include "src/game_logic/game_registry.h"
 #include "src/network/message_system.h"
 ///<<< BEGIN WRITING YOUR CODE
+#include "src/system/player_common_system.h"
+#include "src/system/player_scene_system.h"
 ///<<< END WRITING YOUR CODE
 
 ///<<<rpc begin
@@ -18,6 +20,8 @@ void ServerPlayerSceneServiceImpl::EnterSceneController2Gs(entt::entity player,
     ::google::protobuf::Empty* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
+		//todo进入了gate 然后才可以开始可以给客户端发送信息了, gs消息顺序问题要注意，进入a, 再进入b gs到达客户端消息的顺序不一样
+	PlayerSceneSystem::EnterScene(player, request->scene_id());
 ///<<< END WRITING YOUR CODE
 }
 
@@ -34,6 +38,13 @@ void ServerPlayerSceneServiceImpl::LeaveSceneController2Gs(entt::entity player,
     ::google::protobuf::Empty* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
+	PlayerSceneSystem::LeaveScene(player);
+	if (request->change_gs())//存储完毕以后才能换场景，防止回档
+	{
+		//离开gs 清除session
+		PlayerCommonSystem::RemovePlayereSession(player);
+		PlayerCommonSystem::SavePlayer(player);
+	}
 ///<<< END WRITING YOUR CODE
 }
 
