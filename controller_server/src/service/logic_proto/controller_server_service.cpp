e3c9b7c62710e775a0d4b46e6b5acb790ffcea6b
luyuancpp <lhlcpp6@163.com>
2022-11-07 16:43:44 +0800

    clear code

--- controller_server/src/service/logic_proto/controller_server_service.cpp
+++
@@ -1,9 +0,0 @@
-#include <array>
-#include <memory>
-#include <google/protobuf/message.h>
-
-#include "controller_scene.h"
-#include "controller_team.h"
-std::array<std::unique_ptr<::google::protobuf::Service>, 2> g_server_nomal_service{
-std::unique_ptr<::google::protobuf::Service>(new ServerSceneServiceImpl),
-std::unique_ptr<::google::protobuf::Service>(new S2STeamServiceImpl)};
