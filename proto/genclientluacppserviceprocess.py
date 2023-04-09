bd191bcecc581deb72d3847d53c3c275bc02aa91
luyuan <lhlcpp6@163.com>
2023-04-09 17:11:36 +0800

    clear code

--- proto/genclientluacppserviceprocess.py
+++ proto/genclientluacppserviceprocess.py
@@ -2,6 +2,7 @@ import os
 from os import system
 import md5tool
 import shutil
+import genpublic
 
 luadir = '../bin/script/lua/service/'
 clientservicedir = '../client/src/service/logic_proto/'
@@ -40,20 +41,11 @@ def gencpp():
 	with open(md5dirfilename, 'w', encoding='utf-8')as file:
 		file.write(newstr)
 
-def md5copy(destfilename):
-    filenamemd5 = md5dirfilename + '.md5'
-    error = None
-    emptymd5 = False
-    if  not os.path.exists(filenamemd5):
-        emptymd5 = True
-    else:
-        error = md5tool.check_against_md5_file(md5dirfilename, filenamemd5) 
-    if error == None and os.path.exists(destfilename) and emptymd5 == False:
-        return
-    print("copy %s ---> %s" % (md5dirfilename, destfilename))
-    md5tool.generate_md5_file_for(md5dirfilename, filenamemd5)
-    shutil.copy(md5dirfilename, destfilename)
-
 scanprotofile()
 gencpp()
-md5copy(clientservicedir + cppfilename)
+
+cppmd5info = genpublic.md5fileinfo()
+cppmd5info.filename = cppfilename
+cppmd5info.destdir = clientservicedir
+cppmd5info.md5dir = md5dir
+genpublic.md5copy(cppmd5info)
