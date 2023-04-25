0d3205ff2e64bb08975a76ad6ad21884331246d8
lhlcpp6@163.com <lhlcpp6@163.com>
2023-04-26 02:52:01 +0800

    clear code

--- proto/gengo/src/util/file.go
+++ proto/gengo/src/util/file.go
@@ -1,6 +1,7 @@
 package util
 
 import (
+	"fmt"
 	"io"
 	"os"
 	"path/filepath"
@@ -22,14 +23,23 @@ func IsProtoFile(fd os.DirEntry) bool {
 }
 
 func Copy(dst string, src string) (written int64, err error) {
-	fileDst, err := os.Open(dst)
+	sourceFileStat, err := os.Stat(src)
 	if err != nil {
 		return 0, err
 	}
-	fileSrc, err := os.Open(src)
+	if !sourceFileStat.Mode().IsRegular() {
+		return 0, fmt.Errorf("%s is not a regular file", src)
+	}
+	source, err := os.Open(src)
 	if err != nil {
 		return 0, err
 	}
-	written, err = io.Copy(fileDst, fileSrc)
-	return written, err
+	defer source.Close()
+	destination, err := os.Create(dst)
+	if err != nil {
+		return 0, err
+	}
+	defer destination.Close()
+	nBytes, err := io.Copy(destination, source)
+	return nBytes, err
 }
