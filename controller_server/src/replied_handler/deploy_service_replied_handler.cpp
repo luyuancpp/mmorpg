2de0f5d9fde1c1e8a08f1ca6d0ba7f8e84f8d61a
lhlcpp6@163.com <lhlcpp6@163.com>
2023-05-06 21:28:39 +0800

    replied

--- controller_server/src/replied_handler/deploy_service_replied_handler.cpp
+++ controller_server/src/replied_handler/deploy_service_replied_handler.cpp
@@ -1,15 +1,20 @@
 #include "deploy_service_replied_handler.h"
 ///<<< BEGIN WRITING YOUR CODE
+#include "src/controller_server.h"
+
+void set_server_squence_node_id(uint32_t node_id);
 ///<<< END WRITING YOUR CODE
 void OnServerInfoRepliedHandler(const TcpConnectionPtr& conn, const std::shared_ptr<ServerInfoResponse>& replied, Timestamp timestamp)
 {
 ///<<< BEGIN WRITING YOUR CODE
+	g_controller_node->StartServer(replied->info());
 ///<<< END WRITING YOUR CODE
 }
 
 void OnStartGSRepliedHandler(const TcpConnectionPtr& conn, const std::shared_ptr<StartGSResponse>& replied, Timestamp timestamp)
 {
 ///<<< BEGIN WRITING YOUR CODE
+	
 ///<<< END WRITING YOUR CODE
 }
 
@@ -40,6 +45,7 @@ void OnLoginNodeInfoRepliedHandler(const TcpConnectionPtr& conn, const std::shar
 void OnSceneSqueueNodeIdRepliedHandler(const TcpConnectionPtr& conn, const std::shared_ptr<SceneSqueueResponese>& replied, Timestamp timestamp)
 {
 ///<<< BEGIN WRITING YOUR CODE
+	set_server_squence_node_id(replied->node_id());
 ///<<< END WRITING YOUR CODE
 }
 
