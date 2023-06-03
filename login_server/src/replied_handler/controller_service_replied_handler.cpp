f652681fbcb5bbda59de9dc016c4d43abbd95168
lhlcpp6@163.com <lhlcpp6@163.com>
2023-06-03 18:22:09 +0800

    clear code by rider

--- login_server/src/replied_handler/controller_service_replied_handler.cpp
+++ login_server/src/replied_handler/controller_service_replied_handler.cpp
@@ -2,18 +2,18 @@
 #include "src/network/codec/dispatcher.h"
 
 ///<<< BEGIN WRITING YOUR CODE
-#include "src/comp/account_player.h"
-#include "src/game_logic/thread_local/common_logic_thread_local_storage.h"
-#include "src/game_logic/tips_id.h"
 #include "src/login_server.h"
+#include "src/comp/account_player.h"
+#include "src/game_logic/tips_id.h"
+#include "src/game_logic/thread_local/common_logic_thread_local_storage.h"
 #include "src/network/node_info.h"
 #include "src/network/route_system.h"
+#include "src/pb/pbc/service.h"
 #include "src/thread_local/login_thread_local_storage.h"
 #include "src/util/defer.h"
-#include "src/pb/pbc/service.h"
 
-#include "src/pb/pbc/common_proto/database_service.pb.h"
 #include "controller_service_service.h"
+#include "database_service_service.h"
 #include "gate_service_service.h"
 
 using PlayerPtr = std::shared_ptr<AccountPlayer>;
@@ -112,7 +112,7 @@ void OnControllerServiceLsLoginAccountRepliedHandler(const TcpConnectionPtr& con
     rq.set_account(replied->account());
     rq.set_session_id(session_id);
 
-    Route2Node(kDatabaseNode, ControllerServiceLsLoginAccountMsgId, rq);
+    Route2Node(kDatabaseNode, DbServiceLoginMsgId, rq);
 ///<<< END WRITING YOUR CODE
 }
 
