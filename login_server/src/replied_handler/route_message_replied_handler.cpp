a9eb0db18ac65c518faf4a805acfafff4d95cb9e
lhlcpp6@163.com <lhlcpp6@163.com>
2023-06-07 15:40:05 +0800

    clear code

--- login_server/src/replied_handler/route_message_replied_handler.cpp
+++ login_server/src/replied_handler/route_message_replied_handler.cpp
@@ -23,7 +23,7 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 	defer(cl_tls.set_next_route_node_id(UINT32_MAX));
 	defer(cl_tls.set_current_session_id(kInvalidSessionId));
 
-    cl_tls.set_current_session_id(replied->session_id());
+	cl_tls.set_current_session_id(replied->session_id());
 
 	if (replied->route_data_list().empty())
 	{
@@ -31,12 +31,12 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 		return;
 	}
 	//todo find all service
-	if (replied->route_data_list().rbegin()->message_id() >= g_message_info.size() )
+	if (replied->route_data_list().rbegin()->message_id() >= g_message_info.size())
 	{
 		LOG_ERROR << "message_id not found " << replied->DebugString();
 		return;
 	}
-	auto& message_info = g_message_info[replied->route_data_list().rbegin()->message_id()];
+	const auto& message_info = g_message_info.at(replied->route_data_list().rbegin()->message_id());
 	if (nullptr == message_info.service_impl_instance_)
 	{
 		LOG_ERROR << "message_id not found " << replied->DebugString();
@@ -50,19 +50,15 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 		return;
 	}
 	//当前节点的请求信息
-	std::shared_ptr<google::protobuf::Message> current_node_response(service->GetResponsePrototype(method).New());
+	const MessagePtr current_node_response(service->GetResponsePrototype(method).New());
 	if (!current_node_response->ParseFromString(replied->body()))
 	{
 		LOG_ERROR << "invalid  body response" << replied->DebugString();
 		return;
 	}
-
-	auto mutable_replied = const_cast<::RouteMsgStringResponse*>(replied.get());
-
-
 	//当前节点的真正回复的消息
 	g_response_dispatcher.onProtobufMessage(conn, current_node_response, timestamp);
-
+	auto mutable_replied = const_cast<::RouteMsgStringResponse*>(replied.get());
 	//处理完以后要删除当前节点的信息
 	mutable_replied->mutable_route_data_list()->RemoveLast();
 
@@ -74,7 +70,7 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 		{
 			return;
 		}
-		auto& prev_route_data = *replied->route_data_list().rbegin();
+		const auto& prev_route_data = *replied->route_data_list().rbegin();
 		mutable_replied->set_body(current_node_response->SerializeAsString());
 		mutable_replied->set_session_id(cl_tls.session_id());
 		mutable_replied->set_id(mutable_replied->id());
@@ -83,7 +79,7 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 		case kGateNode:
 		{
 			//todo test 节点不存在了消息会不会存留
-			auto gate_it = login_tls.gate_nodes().find(prev_route_data.node_info().node_id());
+			const auto gate_it = login_tls.gate_nodes().find(prev_route_data.node_info().node_id());
 			if (gate_it == login_tls.gate_nodes().end())
 			{
 				LOG_ERROR << "gate not found node id " << prev_route_data.node_info().node_id() << " " << replied->DebugString();
@@ -93,53 +89,49 @@ void OnServiceRouteNodeStringMsgRepliedHandler(const TcpConnectionPtr& conn, con
 				replied->id(),
 				mutable_replied->SerializeAsString());
 		}
-		break;
+			break;
 
 		default:
-		{
-			LOG_INFO << "route to next node type error " << replied->DebugString() << "," << prev_route_data.node_info().node_type();
-		}
-		break;
+			{
+				LOG_INFO << "route to next node type error " << replied->DebugString() << "," << prev_route_data.node_info().node_type();
+			}
+			break;
 		}
 		return;
 	}
-	else
-	{
-		RouteMsgStringRequest request;
-		request.set_body(cl_tls.route_msg_body());
-		for (auto& request_data_it : mutable_replied->route_data_list())
-		{
-			*request.add_route_data_list() = request_data_it;
-		}
-		request.set_session_id(cl_tls.session_id());
-		request.set_id(mutable_replied->id());
 
-		auto send_route_data = request.add_route_data_list();
-		send_route_data->CopyFrom(cl_tls.route_data());
-		send_route_data->mutable_node_info()->CopyFrom(g_login_node->node_info());
-		switch (cl_tls.next_route_node_type())
-		{
-		case kControllerNode:
-		{
-			//发送到下个节点
-			g_login_node->controller_node()->Route2Node(ControllerServiceRouteNodeStringMsgMsgId, request);
-		}
+	RouteMsgStringRequest request;
+	request.set_body(cl_tls.route_msg_body());
+	for (const auto& request_data_it : mutable_replied->route_data_list())
+	{
+		*request.add_route_data_list() = request_data_it;
+	}
+	request.set_session_id(cl_tls.session_id());
+	request.set_id(mutable_replied->id());
+
+	const auto send_route_data = request.add_route_data_list();
+	send_route_data->CopyFrom(cl_tls.route_data());
+	send_route_data->mutable_node_info()->CopyFrom(g_login_node->node_info());
+	switch (cl_tls.next_route_node_type())
+	{
+	case kControllerNode:
+	{
+		//发送到下个节点
+		g_login_node->controller_node()->Route2Node(ControllerServiceRouteNodeStringMsgMsgId, request);
+	}
 		break;
-		case kDatabaseNode:
+	case kDatabaseNode:
 		{
 			//发送到下个节点
 			g_login_node->db_node()->Route2Node(DbServiceRouteNodeStringMsgMsgId, request);
 		}
 		break;
-		default:
+	default:
 		{
 			LOG_ERROR << "route to next node type error " << request.DebugString() << "," << cl_tls.next_route_node_type();
 		}
 		break;
-		}
 	}
-
-
 	///<<< END WRITING YOUR CODE
 }
 
