68f38507cbf70c5d000bf99646a8a4ed118a8462
lhlcpp6@163.com <lhlcpp6@163.com>
2023-06-09 10:45:48 +0800

    clear code

--- common/src/game_logic/thread_local/common_logic_thread_local_storage.h
+++ common/src/game_logic/thread_local/common_logic_thread_local_storage.h
@@ -9,17 +9,18 @@ public:
     RouteData& route_data() { return route_data_; };
     std::string& route_msg_body() { return route_msg_body_; }
     void set_next_route_node_type(uint32_t node_type) { next_route_node_type_ = node_type; }
-	uint32_t next_route_node_type()const { return next_route_node_type_; }
-	void set_next_route_node_id(uint32_t next_node_id) { next_route_node_id_ = next_node_id; }
-	uint32_t next_route_node_id()const { return next_route_node_id_; }
+    uint32_t next_route_node_type()const { return next_route_node_type_; }
+    void set_next_route_node_id(uint32_t next_node_id) { next_route_node_id_ = next_node_id; }
+    uint32_t next_route_node_id()const { return next_route_node_id_; }
     void set_current_session_id(uint64_t current_session_id) { current_session_id_ = current_session_id; }
     uint64_t session_id()const { return current_session_id_; }
 private:
     RouteData route_data_;
     std::string route_msg_body_;
-    uint32_t next_route_node_type_{ UINT32_MAX };
-    uint32_t next_route_node_id_{ UINT32_MAX };
-    uint64_t current_session_id_{ kInvalidSessionId };
+    uint32_t next_route_node_type_{UINT32_MAX};
+    uint32_t next_route_node_id_{UINT32_MAX};
+    uint64_t current_session_id_{kInvalidSessionId};
+    std::string prev_node_replied_;
 };
 
- extern thread_local CommonLogicThreadLocalStorage cl_tls;
+extern thread_local CommonLogicThreadLocalStorage cl_tls;
--- login_server/src/network/route_system.cpp
+++ login_server/src/network/route_system.cpp
@@ -7,6 +7,6 @@
 void Route2Node(uint32_t node_type, uint32_t message_id, const google::protobuf::Message& message)
 {
 	cl_tls.route_data().set_message_id(message_id);
-	cl_tls.route_msg_body() = std::move(message.SerializeAsString());
+	cl_tls.route_msg_body() = message.SerializeAsString();
 	cl_tls.set_next_route_node_type(node_type);
 }
