bed1b62c3d8d5164d8d1e4884da86e780fc9a487
lhlcpp6 <lhlcpp6@163.com>
2023-06-11 12:09:47 +0800

    enter gs

--- controller_server/src/network/message_system.cpp
+++ controller_server/src/network/message_system.cpp
@@ -51,11 +51,11 @@ void Send2GsPlayer(uint32_t message_id, const google::protobuf::Message& message
 		LOG_INFO << "gs not found ";
 		return;
 	}
-	NodeServiceMessageRequest msg;
-	msg.mutable_msg()->set_body(message.SerializeAsString());
-	msg.mutable_msg()->set_message_id(message_id);
-	msg.mutable_ex()->set_session_id(try_player_session->session_id());
-	gs->session_.Send(GameServiceSend2PlayerMsgId,  message);
+	NodeServiceMessageRequest message_wrapper;
+	message_wrapper.mutable_msg()->set_body(message.SerializeAsString());
+	message_wrapper.mutable_msg()->set_message_id(message_id);
+	message_wrapper.mutable_ex()->set_session_id(try_player_session->session_id());
+	gs->session_.Send(GameServiceSend2PlayerMsgId,  message_wrapper);
 }
 
 
@@ -87,11 +87,11 @@ void Send2PlayerViaGs(uint32_t message_id, const google::protobuf::Message& mess
         return;
     }
    
-    NodeServiceMessageRequest msg;
-	msg.mutable_msg()->set_message_id(message_id);
-    msg.mutable_msg()->set_body(message.SerializeAsString());
-	msg.mutable_ex()->set_session_id(try_player_session->session_id());
-	gs->session_.Send(message_id, message);
+    NodeServiceMessageRequest message_wrapper;
+	message_wrapper.mutable_msg()->set_message_id(message_id);
+    message_wrapper.mutable_msg()->set_body(message.SerializeAsString());
+	message_wrapper.mutable_ex()->set_session_id(try_player_session->session_id());
+	gs->session_.Send(message_id, message_wrapper);
 }
 
 void Send2Player(uint32_t message_id, const google::protobuf::Message& message, entt::entity player)
@@ -156,9 +156,9 @@ void CallGsPlayerMethod(uint32_t message_id, const google::protobuf::Message& me
     {
         return;
     }
-    NodeServiceMessageRequest msg;
-    msg.mutable_msg()->set_body(message.SerializeAsString());
-	msg.mutable_msg()->set_message_id(message_id);
-	msg.mutable_ex()->set_session_id(try_player_session->session_id());
-    tls.registry.get<GsNodePtr>(gs_it->second)->session_.CallMethod(message_id, msg);
+    NodeServiceMessageRequest message_wrapper;
+    message_wrapper.mutable_msg()->set_body(message.SerializeAsString());
+	message_wrapper.mutable_msg()->set_message_id(message_id);
+	message_wrapper.mutable_ex()->set_session_id(try_player_session->session_id());
+    tls.registry.get<GsNodePtr>(gs_it->second)->session_.CallMethod(message_id, message_wrapper);
 }
