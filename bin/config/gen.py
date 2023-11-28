6a91cbfda758e49b895f0d410fdd5b6184345d43
lhlcpp6@163.com <lhlcpp6@163.com>
2023-11-28 16:43:52 +0800

    add build

--- bin/config/gen.py
+++ bin/config/gen.py
@@ -11,14 +11,14 @@ if not os.path.exists("json"):
 	os.makedirs("json")
 if not os.path.exists("../../pbc/src/game_config/"):
 	os.makedirs("../../pbc/src/game_config/")
-if not os.path.exists("../../common/src/game_config/"):
-	os.makedirs("../../common/src/game_config/")
+if not os.path.exists("../../config/src/"):
+	os.makedirs("../../config/src/")
 
 print(system("python xlstojson.py"))
 print(system("python xlstopb.py"))
 print(system("cd proto && python buildproto.py"))
 print(system("python xlstocpp.py"))
 print(system("python xlstocppid.py"))
-print(system("python md5tool.py md5copy ./cpp/ ../../common/src/game_config/"))
+print(system("python md5tool.py md5copy ./cpp/ ../../config/src/"))
 print(system("python md5tool.py md5copy ./protocpp/ ../../pbc/src/game_config/ "))
 print(system("python genxlsmd5.py"))
