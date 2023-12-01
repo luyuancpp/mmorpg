99d9aafb849c1466f678dc49382b3a81ebdfd0bb
lhlcpp6@163.com <lhlcpp6@163.com>
2023-12-01 13:27:55 +0800

    tidy dirctory

--- bin/config/gen.py
+++ bin/config/gen.py
@@ -9,16 +9,16 @@ if not os.path.exists("protocpp"):
 	os.makedirs("protocpp")
 if not os.path.exists("json"):
 	os.makedirs("json")
-if not os.path.exists("../../pbc/src/game_config/"):
-	os.makedirs("../../pbc/src/game_config/")
-if not os.path.exists("../../config/src/"):
-	os.makedirs("../../config/src/")
+if not os.path.exists("../../pkg/pbc/src/game_config/"):
+	os.makedirs("../../pkg/pbc/src/game_config/")
+if not os.path.exists("../../pkg/config/src/"):
+	os.makedirs("../../pkg/config/src/")
 
 print(system("python xlstojson.py"))
 print(system("python xlstopb.py"))
 print(system("cd proto && python buildproto.py"))
 print(system("python xlstocpp.py"))
 print(system("python xlstocppid.py"))
-print(system("python md5tool.py md5copy ./cpp/ ../../config/src/"))
-print(system("python md5tool.py md5copy ./protocpp/ ../../pbc/src/game_config/ "))
+print(system("python md5tool.py md5copy ./cpp/ ../../pkg/config/src/"))
+print(system("python md5tool.py md5copy ./protocpp/ ../../pkg/pbc/src/game_config/ "))
 print(system("python genxlsmd5.py"))
