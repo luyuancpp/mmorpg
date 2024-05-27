dbb538361c655ad392c3b6f531d0e1218c3c5105
luyuan <luyuan@qq.com>
2024-05-27 13:59:01 +0800

    db server

--- server/login_server/src/replied_handler/register_replied_handler.cpp
+++ server/login_server/src/replied_handler/register_replied_handler.cpp
@@ -1,8 +1,5 @@
 void InitRepliedHandler()
 {
-	void InitAccountDBServiceLoad2RedisRepliedHandler();
-	InitAccountDBServiceLoad2RedisRepliedHandler();
-
 	void InitControllerServiceStartGsRepliedHandler();
 	InitControllerServiceStartGsRepliedHandler();
 
