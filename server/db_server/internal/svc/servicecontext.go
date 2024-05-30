a2ed83aca156484212eabac46e50fd5758217bc1
luyuan <luyuan@qq.com>
2024-05-30 12:09:35 +0800

    daba base server

--- server/db_server/internal/svc/servicecontext.go
+++ server/db_server/internal/svc/servicecontext.go
@@ -1,13 +1,20 @@
 package svc
 
-import "db_server/internal/config"
+import (
+	"db_server/internal/config"
+	"github.com/redis/go-redis/v9"
+)
 
 type ServiceContext struct {
 	Config config.Config
+	Rdb    *redis.Client
 }
 
 func NewServiceContext(c config.Config) *ServiceContext {
 	return &ServiceContext{
 		Config: c,
+		Rdb: redis.NewClient(&redis.Options{
+			Addr: "127.0.0.1:6379",
+		}),
 	}
 }
