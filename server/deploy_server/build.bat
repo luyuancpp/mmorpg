b2d8e55fd9e2ecad61ad9b59abb5dfb0d3e9c1e7
luhailong <lhlcpp@163.com>
2024-06-04 20:18:24 +0800

    db server

--- server/deploy_server/build.bat
+++
@@ -1,2 +0,0 @@
-goctl rpc protoc deploy_service.proto --go_out=. --go-grpc_out=. --zrpc_out=. -m
-pause
---
+++ server/go/deploy_server/build.bat
@@ -0,0 +1,2 @@
+goctl rpc protoc deploy_service.proto --go_out=. --go-grpc_out=. --zrpc_out=. -m
+pause
