c482460d76d9f846a4d2ccaa6d26dc61cb55ab18
luhailong <lhlcpp@163.com>
2024-06-06 13:31:33 +0800

    go lang connect to c++

--- server/go/deploy_server/internal/logic/deployservice/getnodeinfologic.go
+++ server/go/deploy_server/internal/logic/deployservice/getnodeinfologic.go
@@ -3,7 +3,6 @@ package deployservicelogic
 import (
 	"context"
 	"deploy_server/pkg"
-	"log"
 	"strconv"
 
 	"deploy_server/internal/svc"
@@ -38,8 +37,6 @@ func (l *GetNodeInfoLogic) GetNodeInfo(in *deploy.NodeInfoRequest) (*deploy.Node
 			LobbyInfo:      &deploy.LobbyServerDb{},
 		},
 	}
-	log.Println("zone id ", in.ZoneId)
-	return response, nil
 
 	zoneId := strconv.FormatUint(uint64(in.ZoneId), 10)
 	pkg.PbDb.LoadOneByKV(response.Info.GetDatabaseInfo(), "zone_id", zoneId)
@@ -47,6 +44,5 @@ func (l *GetNodeInfoLogic) GetNodeInfo(in *deploy.NodeInfoRequest) (*deploy.Node
 	pkg.PbDb.LoadOneByKV(response.Info.GetControllerInfo(), "zone_id", zoneId)
 	pkg.PbDb.LoadOneByKV(response.Info.GetGateInfo(), "zone_id", zoneId)
 	pkg.PbDb.LoadOneByKV(response.Info.GetLobbyInfo(), "zone_id", zoneId)
-
 	return response, nil
 }
