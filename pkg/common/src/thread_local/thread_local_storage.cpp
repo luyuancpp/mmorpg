783aa9a29ca244e156eb8c13b99322a05cb9ea5a
luhailong <lhlcpp@163.com>
2024-06-09 15:46:04 +0800

    clear code

--- pkg/common/src/thread_local/thread_local_storage.cpp
+++ pkg/common/src/thread_local/thread_local_storage.cpp
@@ -5,6 +5,8 @@ thread_local ThreadLocalStorage tls;
 void ThreadLocalStorage::ClearForTest()
 {
 	registry.clear();
+	session_registry.clear();
+	scene_registry.clear();
 	op_entity = entt::null;
 	error_entity = entt::null;
 	global_entity = entt::null;
