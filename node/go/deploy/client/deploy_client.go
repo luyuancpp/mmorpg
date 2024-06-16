078ccdd96f3f5f725853b96847826545bb323258
luhailong <lhlcpp@163.com>
2024-06-16 12:16:54 +0800

    clear client code

--- node/go/deploy/client/deploy_client.go
+++ node/go/deploy/client/deploy_client.go
@@ -1,11 +1,9 @@
 package main
 
 import (
-	"deploy_server/client/deployservice"
 	"flag"
 )
 import (
-	"context"
 	"github.com/zeromicro/go-zero/core/conf"
 	"github.com/zeromicro/go-zero/zrpc"
 )
@@ -17,16 +15,4 @@ func main() {
 
 	var c zrpc.RpcClientConf
 	conf.MustLoad(*configFile, &c)
-	client := zrpc.MustNewClient(c)
-	client1 := zrpc.MustNewClient(c)
-
-	for i := 0; i < 1000; i++ {
-		deploy := deployservice.NewDeployService(client)
-		deploy.StartGs(context.Background(), &deployservice.StartGsRequest{ZoneId: uint32(i)})
-		deploy.GetNodeInfo(context.Background(), &deployservice.NodeInfoRequest{ZoneId: uint32(i * 10)})
-
-		deploy1 := deployservice.NewDeployService(client1)
-		deploy1.StartGs(context.Background(), &deployservice.StartGsRequest{ZoneId: uint32(i * 100)})
-		deploy1.GetNodeInfo(context.Background(), &deployservice.NodeInfoRequest{ZoneId: uint32(i * 1000)})
-	}
 }
