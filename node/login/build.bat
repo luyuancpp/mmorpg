892ac5249bbc1ab1f0c98cf5d7aec12797cf3b2a
luhailong <lhlcpp@163.com>
2024-06-18 19:08:49 +0800

    login node

--- node/login/build.bat
+++ node/login/build.bat
@@ -4,6 +4,8 @@ protoc  ../../proto/common_proto/empty.proto --go_out=./  --proto_path=../../pro
 protoc  ../../proto/common_proto/c2gate.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
 protoc  ../../proto/common_proto/account_database.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
 protoc  ../../proto/common_proto/comp.proto --go_out=./pb/game  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
+protoc  ../../proto/common_proto/node_common.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
+protoc  ../../proto/common_proto/common.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
 protoc  ../../proto/common_proto/mysql_database_table.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
 protoc  ../../proto/component_proto/player_scene_comp.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
 protoc  ../../proto/common_proto/centre_service.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
