8270935ff78becd0229be0e9d53f973cee05e976
luhailong <lhlcpp@163.com>
2024-06-27 23:52:09 +0800

    change scene

--- client/client/logic/handler/sceneinfos2c.go
+++ client/client/logic/handler/sceneinfos2c.go
@@ -1,11 +1,15 @@
 package handler
 
 import (
+	"client/logic"
 	"client/pb/game"
 	"client/pkg"
-	"log"
+	"math/rand"
 )
 
 func SceneInfoS2CHandler(client *pkg.GameClient, response *game.SceneInfoS2C) {
-	log.Println(response)
+	req := &game.EnterSceneC2SRequest{}
+	randomIndex := rand.Intn(len(response.SceneInfo))
+	req.SceneInfo = response.SceneInfo[randomIndex]
+	logic.MainPlayer.Send(req, 16)
 }
