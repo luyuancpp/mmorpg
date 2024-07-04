05582fe486b53c0f53af9fbca58c596c24d5e873
luhailoing <lhlcpp6@163.com>
2024-07-04 23:00:23 +0800

    aoi

--- node/game/src/system/scene/aoi.cpp
+++ node/game/src/system/scene/aoi.cpp
@@ -7,30 +7,21 @@
 
 #include "thread_local/storage.h"
 
-Point kDefaultSize(10.0, 10.0);
+const Point kDefaultSize(10.0, 10.0);
+const Point kOrigin(10.0, 10.0);
+const auto KFlat = Layout(layout_flat, kDefaultSize, kOrigin);
 
 void AoiSystem::Update(entt::entity scene)
 {
     auto&& grid_list = tls.scene_registry.get<SceneGridList>(scene);
-    for (auto&&[e, transform, velocity] : tls.registry.view<Transform, Velocity>().each())
+    for (auto&&[e, transform] : tls.registry.view<Transform>().each())
     {
-        const auto flat = Layout(layout_flat, kDefaultSize, Point(35.0, 71.0));
-        const auto hex = pixel_to_hex(flat, Point(transform.localtion().x(), transform.localtion().y()));
+        const auto hex = pixel_to_hex(KFlat, Point(transform.localtion().x(), transform.localtion().y()));
         const uint64_t q = static_cast<uint64_t>(hex.q);
         const uint64_t r = static_cast<uint64_t>(hex.r);
         absl::uint128 key = q ;
         key = key << 64 & r;
-        if (grid_list.contains(key))
-        {
-            
-        }
-        else
-        {
-            auto it = grid_list.emplace(key, HexagonsGrid{});
-            if (it.second)
-            {
-                
-            }
-        }
+        //to test
+        grid_list[key].entity_list_.emplace(e);
     }
 }
