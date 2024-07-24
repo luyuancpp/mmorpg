24e0d3ef4c87cf6b580a89b643e85914ff4332b2
luhailong <lhpcpp6@163.com>
2024-07-24 16:32:53 +0800

    clear code

--- pkg/logic/src/comp/mission.cpp
+++ pkg/logic/src/comp/mission.cpp
@@ -24,28 +24,28 @@ MissionsComp::MissionsComp()
 std::size_t MissionsComp::CanGetRewardSize() const
 {
 	// Retrieve mission reward component
-	const auto* const mission_reward = tls.registry.try_get<MissionRewardPbComp>(event_owner());
-	if (nullptr == mission_reward)
+	const auto* const missionReward = tls.registry.try_get<MissionRewardPbComp>(EventOwner());
+	if (nullptr == missionReward)
 	{
 		return 0; // Return 0 if reward component is not found
 	}
-	return static_cast<std::size_t>(mission_reward->can_reward_mission_id_size()); // Return the number of missions eligible for reward
+	return static_cast<std::size_t>(missionReward->can_reward_mission_id_size()); // Return the number of missions eligible for reward
 }
 
-uint32_t MissionsComp::IsMissionUnaccepted(const uint32_t mission_id) const
+uint32_t MissionsComp::IsMissionUnaccepted(const uint32_t missionId) const
 {
 	// Check if the mission has been accepted
-	if (missionsComp.missions().find(mission_id) != missionsComp.missions().end())
+	if (missionsComp.missions().find(missionId) != missionsComp.missions().end())
 	{
 		return kMissionIdRepeated; // Return kMissionIdRepeated if mission is already accepted
 	}
 	return kOK; // Return kOK if mission is not accepted
 }
 
-uint32_t MissionsComp::IsMissionUncompleted(const uint32_t mission_id) const
+uint32_t MissionsComp::IsMissionUncompleted(const uint32_t missionId) const
 {
 	// Check if the mission is completed
-	if (missionsComp.complete_missions().count(mission_id) > 0)
+	if (missionsComp.complete_missions().count(missionId) > 0)
 	{
 		return kMissionAlreadyCompleted; // Return kMissionAlreadyCompleted if mission is already completed
 	}
