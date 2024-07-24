ec79f5f71cf624c59a0a5525301accc299a8dc02
luhailong <lhpcpp6@163.com>
2024-07-24 22:02:55 +0800

    clear code by chat gtp

--- pkg/logic/src/system/scene/node_scene_system.cpp
+++ pkg/logic/src/system/scene/node_scene_system.cpp
@@ -3,7 +3,7 @@
 #include "comp/node_scene.h"
 #include "thread_local/storage.h"
 #include "proto/logic/component/gs_node_comp.pb.h"
-
+#include "muduo/base/Logging.h"
 
 using GameNodePlayerInfoPtr = std::shared_ptr<GameNodeInfo>;
 
@@ -33,11 +33,18 @@ entt::entity FindSceneWithMinPlayerCountTemplate(const GetSceneParam& param, con
 	}
 
 	if (entt::null == bestNode) {
+		LOG_WARN << "FindSceneWithMinPlayerCountTemplate: No suitable node found for scene configuration ID: " << sceneConfigId;
 		return entt::null;
 	}
 
 	const auto& serverComps = tls.game_node_registry.get<ServerComp>(bestNode);
-	return serverComps.GetSceneWithMinPlayerCountByConfigId(sceneConfigId);
+	auto bestScene = serverComps.GetSceneWithMinPlayerCountByConfigId(sceneConfigId);
+
+	if (bestScene == entt::null) {
+		LOG_WARN << "FindSceneWithMinPlayerCountTemplate: No scene found with minimum player count for node: " << entt::to_integral(bestNode);
+	}
+
+	return bestScene;
 }
 
 template <typename ServerType>
@@ -64,6 +71,7 @@ entt::entity FindNotFullSceneTemplate(const GetSceneParam& param, const GetScene
 	}
 
 	if (entt::null == bestNode) {
+		LOG_WARN << "FindNotFullSceneTemplate: No suitable node found for scene configuration ID: " << sceneConfigId;
 		return entt::null;
 	}
 
@@ -81,6 +89,10 @@ entt::entity FindNotFullSceneTemplate(const GetSceneParam& param, const GetScene
 		break;
 	}
 
+	if (bestScene == entt::null) {
+		LOG_WARN << "FindNotFullSceneTemplate: No scene found that is not full for node: " << entt::to_integral(bestNode);
+	}
+
 	return bestScene;
 }
 
@@ -89,9 +101,11 @@ entt::entity NodeSceneSystem::FindSceneWithMinPlayerCount(const GetSceneParam& p
 
 	auto bestScene = FindSceneWithMinPlayerCountTemplate<MainSceneServer>(param, filterParam);
 	if (bestScene != entt::null) {
+		LOG_INFO << "FindSceneWithMinPlayerCount: Found scene with minimum player count, Scene ID: " << entt::to_integral(bestScene);
 		return bestScene;
 	}
 
+	LOG_WARN << "FindSceneWithMinPlayerCount: No scene found with minimum player count";
 	return FindSceneWithMinPlayerCountTemplate<MainSceneServer>(param, filterParam);
 }
 
@@ -100,9 +114,11 @@ entt::entity NodeSceneSystem::FindNotFullScene(const GetSceneParam& param) {
 
 	auto bestScene = FindNotFullSceneTemplate<MainSceneServer>(param, filterParam);
 	if (bestScene != entt::null) {
+		LOG_INFO << "FindNotFullScene: Found scene that is not full, Scene ID: " << entt::to_integral(bestScene);
 		return bestScene;
 	}
 
+	LOG_WARN << "FindNotFullScene: No scene found that is not full";
 	filterParam.nodePressureState = NodePressureState::kPressure;
 	return FindNotFullSceneTemplate<MainSceneServer>(param, filterParam);
 }
@@ -111,28 +127,34 @@ void NodeSceneSystem::SetNodePressure(entt::entity node) {
 	auto* const serverComp = tls.game_node_registry.try_get<ServerComp>(node);
 
 	if (nullptr == serverComp) {
+		LOG_ERROR << "SetNodePressure: ServerComp not found for node: " << entt::to_integral(node);
 		return;
 	}
 
 	serverComp->SetNodePressureState(NodePressureState::kPressure);
+	LOG_INFO << "SetNodePressure: Node entered pressure state, Node ID: " << entt::to_integral(node);
 }
 
 void NodeSceneSystem::ClearNodePressure(entt::entity node) {
 	auto* const serverComp = tls.game_node_registry.try_get<ServerComp>(node);
 
 	if (nullptr == serverComp) {
+		LOG_ERROR << "ClearNodePressure: ServerComp not found for node: " << entt::to_integral(node);
 		return;
 	}
 
 	serverComp->SetNodePressureState(NodePressureState::kNoPressure);
+	LOG_INFO << "ClearNodePressure: Node exited pressure state, Node ID: " << entt::to_integral(node);
 }
 
 void NodeSceneSystem::SetNodeState(entt::entity node, NodeState state) {
 	auto* const tryServerComp = tls.game_node_registry.try_get<ServerComp>(node);
 
 	if (nullptr == tryServerComp) {
+		LOG_ERROR << "SetNodeState: ServerComp not found for node: " << entt::to_integral(node);
 		return;
 	}
 
 	tryServerComp->SetState(state);
+	LOG_INFO << "SetNodeState: Node state set successfully, Node ID: " << entt::to_integral(node) << ", State: " << static_cast<int>(state);
 }
