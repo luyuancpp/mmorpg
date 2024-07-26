e92441ed816d6910d803c77cd44e8e3a8065235a
luhailong <lhpcpp6@163.com>
2024-07-26 10:27:09 +0800

    clear code by chat gtp

--- pkg/common/src/util/file2string.cpp
+++ pkg/common/src/util/file2string.cpp
@@ -1,32 +1,29 @@
 #include "file2string.h"
-
 #include <fstream>
-
 #include "muduo/base/Logging.h"
 
-//http://www.cplusplus.com/reference/istream/istream/read/
+// http://www.cplusplus.com/reference/istream/istream/read/
 std::string File2String(const std::string& filename)
 {
-    std::ifstream if_stream(filename, std::ifstream::binary);
-    std::string result;
-    if (if_stream)
-    {
-        // get length of file:
-        if_stream.seekg(0, std::ifstream::end);
-        const auto length = if_stream.tellg();
-        result.resize(static_cast<std::string::size_type>(length));
-        if_stream.seekg(0, std::ifstream::beg);
-        // read data as a block:
-        if_stream.read(result.data(), length);
-        if_stream.close();
-        // ...buffer contains the entire file...
-        //json_buffer.erase(remove_if(json_buffer.begin(), json_buffer.end(), iscntrl), json_buffer.end());
-        //json_buffer.erase(remove_if(json_buffer.begin(), json_buffer.end(), isspace), json_buffer.end());
-    }
-    else
-    {
-        LOG_FATAL << " No such file or directory " << filename;
-    }
-    return result;
+	std::ifstream ifstream(filename, std::ifstream::binary);
+	std::string result;
+	if (ifstream)
+	{
+		// Get length of file:
+		ifstream.seekg(0, std::ifstream::end);
+		const auto length = ifstream.tellg();
+		result.resize(static_cast<std::string::size_type>(length));
+		ifstream.seekg(0, std::ifstream::beg);
+		// Read data as a block:
+		ifstream.read(result.data(), length);
+		ifstream.close();
+		// Buffer contains the entire file...
+		// jsonBuffer.erase(remove_if(jsonBuffer.begin(), jsonBuffer.end(), iscntrl), jsonBuffer.end());
+		// jsonBuffer.erase(remove_if(jsonBuffer.begin(), jsonBuffer.end(), isspace), jsonBuffer.end());
+	}
+	else
+	{
+		LOG_FATAL << " No such file or directory " << filename;
+	}
+	return result;
 }
-
