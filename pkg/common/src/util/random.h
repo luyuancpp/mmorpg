dd764d39b22f4254b0406751dbfaef144eb8979b
luhailong <lhpcpp6@163.com>
2024-07-26 11:01:57 +0800

    clear code

--- pkg/common/src/util/random.h
+++ pkg/common/src/util/random.h
@@ -5,34 +5,33 @@
 class Random
 {
 public:
-    Random() { rand.seed(std::random_device{}()); }
+	Random() : rand(std::random_device{}()) {}
 
-    static Random& GetSingleton()
-    {
-        thread_local Random singleton;
-        return singleton;
-    }
+	static Random& GetSingleton()
+	{
+		thread_local Random singleton;
+		return singleton;
+	}
 
-    template <typename RandType>
-    inline decltype(auto) Rand(RandType min_num, RandType max_num)
-    {
-        std::uniform_int_distribution<RandType> dist(min_num, max_num);
-        return dist(rand);
-    }
+	template <typename RandType>
+	inline RandType Rand(RandType min_num, RandType max_num)
+	{
+		std::uniform_int_distribution<RandType> dist(min_num, max_num);
+		return dist(rand);
+	}
 
-    inline decltype(auto) Rand(double min_num, double max_num)
-    {
-        std::uniform_real_distribution<double> dist(min_num, max_num);
-        return dist(rand);
-    }
+	inline double Rand(double min_num, double max_num)
+	{
+		std::uniform_real_distribution<double> dist(min_num, max_num);
+		return dist(rand);
+	}
 
-    inline decltype(auto) Rand(float min_num, float max_num)
-    {
-        std::uniform_real_distribution<double> dist(min_num, max_num);
-        return dist(rand);
-    }
+	inline float Rand(float min_num, float max_num)
+	{
+		std::uniform_real_distribution<float> dist(min_num, max_num);
+		return dist(rand);
+	}
+
 private:
-    std::mt19937 rand;
+	std::mt19937 rand;
 };
-
-
