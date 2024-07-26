addf291b66565f10241228bdc05d35ac2d2d14cc
luhailong <lhpcpp6@163.com>
2024-07-26 10:21:38 +0800

    clear code

--- test/node_sequence_test/node_sequence_test.cpp
+++ test/node_sequence_test/node_sequence_test.cpp
@@ -1,5 +1,4 @@
 #include <gtest/gtest.h>
-
 #include <cassert>
 #include <functional>
 #include <iostream>
@@ -9,53 +8,52 @@
 
 #include "util/snow_flake.h"
 
-using guid_vetcor = std::vector<Guid>;
-using guid_set = std::unordered_set<Guid>;
+using GuidVector = std::vector<Guid>;
+using GuidSet = std::unordered_set<Guid>;
 
 using ServerSequence32 = NodeSequence<uint64_t, 32>;
 
 ServerSequence32 sf;
 
-
 static const std::size_t kTestSize = 0xffffff;
 
-void EmplaceToVector(guid_vetcor& v)
+void emplaceToVector(GuidVector& v)
 {
-    for (std::size_t i = 0; i < kTestSize; ++i)
-    {
-        v.emplace_back(sf.Generate());
-    }
+	for (std::size_t i = 0; i < kTestSize; ++i)
+	{
+		v.emplace_back(sf.Generate());
+	}
 }
 
-void PutVectorInToSet(guid_set& s, guid_vetcor& v)
+void putVectorIntoSet(GuidSet& s, GuidVector& v)
 {
-    for (auto& it : v)
-    {
-        s.emplace(it);
-    }
+	for (auto& it : v)
+	{
+		s.emplace(it);
+	}
 }
 
-TEST(TestSnowFlakeThreadSafe, JustGenerateTime)
+TEST(TestSnowFlakeThreadSafe, justGenerateTime)
 {
-    Guid id = sf.Generate();
+	Guid id = sf.Generate();
 }
 
-TEST(TestSnowFlakeThreadSafe, Generate)
+TEST(TestSnowFlakeThreadSafe, generate)
 {
-    guid_set guid_set;
-    guid_vetcor first_v;
-    EmplaceToVector(first_v);
-    PutVectorInToSet(guid_set, first_v);
+	GuidSet guidSet;
+	GuidVector firstVector;
+	emplaceToVector(firstVector);
+	putVectorIntoSet(guidSet, firstVector);
 
-    assert(guid_set.size() == first_v.size());
+	assert(guidSet.size() == firstVector.size());
 }
 
 int main(int argc, char** argv)
 {
-    testing::InitGoogleTest(&argc, argv);
-    while (true)
-    {
-        RUN_ALL_TESTS();
-    }
-    return RUN_ALL_TESTS();
+	testing::InitGoogleTest(&argc, argv);
+	while (true)
+	{
+		RUN_ALL_TESTS();
+	}
+	return RUN_ALL_TESTS();
 }
