688f3c6e1bd336dc4235dc29ebb1573c46fad215
luhailoing <lhlcpp6@163.com>
2024-07-27 21:47:30 +0800

    clear code

--- test/aoi_test/aoi_test.cpp
+++ test/aoi_test/aoi_test.cpp
@@ -17,13 +17,13 @@
 
 extern const Point kDefaultSize(20.0, 20.0);
 extern const Point kOrigin(0.0, 0.0);
-extern const auto KFlat = Layout(layout_flat, kDefaultSize, kOrigin);
+extern const auto kHexLayout = Layout(layout_flat, kDefaultSize, kOrigin);
 
 // Mock class for testing purposes
 class MockAoiSystem : public AoiSystem {
 public:
     using AoiSystem::GetGridId;
-    using AoiSystem::ScanNeighborGridId;
+    using AoiSystem::ScanNeighborGridIds;
     using AoiSystem::Update;
     using AoiSystem::BeforeLeaveSceneHandler;
     using AoiSystem::UpdateLogGridSize;
@@ -50,57 +50,57 @@ protected:
 
 // Test GetGridId for Location
 TEST_F(AoiSystemTest, TestGetGridIdForLocation) {
-    Location loc;
-    loc.set_x(10);
-    loc.set_y(20);
-    auto grid_id = aoi_system.GetGridId(loc);
+    Location location;
+    location.set_x(10);
+    location.set_y(20);
+    auto grid_id = aoi_system.GetGridId(location);
 
     // Expected value should be calculated based on your logic
-    absl::uint128 expected_grid_id = aoi_system.GetGridId(hex_round(pixel_to_hex(KFlat, Point(10, 20))));
+    absl::uint128 expected_grid_id = aoi_system.GetGridId(hex_round(pixel_to_hex(kHexLayout, Point(10, 20))));
     EXPECT_EQ(grid_id, expected_grid_id);
 }
 
-// Test ScanNeighborGridId
-TEST_F(AoiSystemTest, TestScanNeighborGridId) {
+// Test ScanNeighborGridIds
+TEST_F(AoiSystemTest, TestScanNeighborGridIds) {
     Hex hex{ 0, 0, 0 };
-    GridSet grid_set;
-    aoi_system.ScanNeighborGridId(hex, grid_set);
+    GridSet neighbor_grid_set;
+    aoi_system.ScanNeighborGridIds(hex, neighbor_grid_set);
 
     // Check the number of neighbors
-    EXPECT_EQ(grid_set.size(), 6);
+    EXPECT_EQ(neighbor_grid_set.size(), 6);
 
     // Check if specific neighbors are present
     for (int i = 0; i < 6; ++i) {
-        EXPECT_NE(grid_set.find(aoi_system.GetGridId(hex_neighbor(hex, i))), grid_set.end());
+        EXPECT_NE(neighbor_grid_set.find(aoi_system.GetGridId(hex_neighbor(hex, i))), neighbor_grid_set.end());
     }
 }
 
 // Test Update method for player movement
 TEST_F(AoiSystemTest, TestUpdatePlayerMovement) {
     // Mock data setup
-    auto sceneEntity = tls.sceneRegistry.create();
-    auto& sceneGridList = tls.sceneRegistry.emplace<SceneGridList>(sceneEntity);
+    auto scene_entity = tls.sceneRegistry.create();
+    auto& scene_grid_list = tls.sceneRegistry.emplace<SceneGridList>(scene_entity);
 
-    SceneEntityComp sceneEntityComp{ sceneEntity };
+    SceneEntityComp scene_entity_comp{ scene_entity };
 
-    std::unordered_map<absl::uint128, uint32_t, absl::Hash<absl::uint128>> expectedEntityCount;
+    std::unordered_map<absl::uint128, uint32_t, absl::Hash<absl::uint128>> expected_entity_count;
 
     for (uint32_t i = 0; i < 10; ++i) {
-        auto playerEntity = tls.registry.create();
+        auto player_entity = tls.registry.create();
 
-        Transform& transform = tls.registry.emplace<Transform>(playerEntity);
+        Transform& transform = tls.registry.emplace<Transform>(player_entity);
         transform.mutable_location()->set_x(tls_rand.RandDouble(0, 1000));
         transform.mutable_location()->set_y(tls_rand.RandDouble(0, 1000));
 
-        tls.registry.emplace<SceneEntityComp>(playerEntity, sceneEntityComp);
+        tls.registry.emplace<SceneEntityComp>(player_entity, scene_entity_comp);
 
         // Invoke Update method
         aoi_system.Update(0.1);
 
         auto grid_id = aoi_system.GetGridId(transform.location());
-        ++expectedEntityCount[grid_id];
-        EXPECT_TRUE(sceneGridList[grid_id].entity_list.contains(playerEntity));
-        EXPECT_EQ(sceneGridList[grid_id].entity_list.size(), expectedEntityCount[grid_id]);
+        ++expected_entity_count[grid_id];
+        EXPECT_TRUE(scene_grid_list[grid_id].entity_list.contains(player_entity));
+        EXPECT_EQ(scene_grid_list[grid_id].entity_list.size(), expected_entity_count[grid_id]);
     }
 
     for (auto&& [scene, grid_list] : tls.sceneRegistry.view<SceneGridList>().each()) {
@@ -115,59 +115,58 @@ TEST_F(AoiSystemTest, TestUpdatePlayerMovement) {
 // Test player movement across six neighboring hexes
 TEST_F(AoiSystemTest, TestPlayerMovementAcrossSixHexes) {
     // Mock data setup
-    auto sceneEntity = tls.sceneRegistry.create();
-    auto& sceneGridList = tls.sceneRegistry.emplace<SceneGridList>(sceneEntity);
+    auto scene_entity = tls.sceneRegistry.create();
+    auto& scene_grid_list = tls.sceneRegistry.emplace<SceneGridList>(scene_entity);
 
-    auto playerEntity = tls.registry.create();
+    auto player_entity = tls.registry.create();
 
-    Transform& transform = tls.registry.emplace<Transform>(playerEntity);
+    Transform& transform = tls.registry.emplace<Transform>(player_entity);
     transform.mutable_location()->set_x(0);
     transform.mutable_location()->set_y(0);
 
-    SceneEntityComp sceneEntityComp{ sceneEntity };
-    tls.registry.emplace<SceneEntityComp>(playerEntity, sceneEntityComp);
+    SceneEntityComp scene_entity_comp{ scene_entity };
+    tls.registry.emplace<SceneEntityComp>(player_entity, scene_entity_comp);
 
     // Initial position
     aoi_system.Update(0.1);
-    Hex initialHex = hex_round(pixel_to_hex(KFlat, Point(0, 0)));
-    auto initialGridId = aoi_system.GetGridId(initialHex);
-    EXPECT_TRUE(sceneGridList[initialGridId].entity_list.contains(playerEntity));
+    Hex initial_hex = hex_round(pixel_to_hex(kHexLayout, Point(0, 0)));
+    auto initial_grid_id = aoi_system.GetGridId(initial_hex);
+    EXPECT_TRUE(scene_grid_list[initial_grid_id].entity_list.contains(player_entity));
 
     // Move the player to each neighboring hex
     for (int i = 0; i < 6; ++i) {
-        Hex neighborHex = hex_neighbor(initialHex, i);
-        Point neighborPoint = hex_to_pixel(KFlat, neighborHex);
-        transform.mutable_location()->set_x(neighborPoint.x);
-        transform.mutable_location()->set_y(neighborPoint.y);
+        Hex neighbor_hex = hex_neighbor(initial_hex, i);
+        Point neighbor_point = hex_to_pixel(kHexLayout, neighbor_hex);
+        transform.mutable_location()->set_x(neighbor_point.x);
+        transform.mutable_location()->set_y(neighbor_point.y);
 
         // Update the system
         aoi_system.Update(0.1);
 
-        auto newGridId = aoi_system.GetGridId(neighborHex);
-        EXPECT_TRUE(sceneGridList[newGridId].entity_list.contains(playerEntity));
-        EXPECT_FALSE(sceneGridList[initialGridId].entity_list.contains(playerEntity));
+        auto new_grid_id = aoi_system.GetGridId(neighbor_hex);
+        EXPECT_TRUE(scene_grid_list[new_grid_id].entity_list.contains(player_entity));
+        EXPECT_FALSE(scene_grid_list[initial_grid_id].entity_list.contains(player_entity));
 
         // Move back to initial hex for next iteration
         transform.mutable_location()->set_x(0);
         transform.mutable_location()->set_y(0);
         aoi_system.Update(0.1);
-        EXPECT_TRUE(sceneGridList[initialGridId].entity_list.contains(playerEntity));
-        EXPECT_FALSE(sceneGridList[newGridId].entity_list.contains(playerEntity));
+        EXPECT_TRUE(scene_grid_list[initial_grid_id].entity_list.contains(player_entity));
+        EXPECT_FALSE(scene_grid_list[new_grid_id].entity_list.contains(player_entity));
     }
 
-    std::size_t expectedSize = 0;
+    std::size_t expected_size = 0;
     for (auto&& [scene, grid_list] : tls.sceneRegistry.view<SceneGridList>().each()) {
         for (const auto& [_, entity_list] : grid_list) {
-            if (entity_list.entity_list.empty())
-            {
+            if (entity_list.entity_list.empty()) {
                 continue;
             }
             EXPECT_EQ(entity_list.entity_list.size(), 1);
-            expectedSize += entity_list.entity_list.size();
+            expected_size += entity_list.entity_list.size();
         }
     }
 
-    EXPECT_EQ(expectedSize, 1);
+    EXPECT_EQ(expected_size, 1);
 }
 
 // Add more tests as needed
