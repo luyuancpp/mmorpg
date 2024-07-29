253787b20d1d3338166c03cee322678403735ea5
luhailong <lhpcpp6@163.com>
2024-07-29 15:43:33 +0800

    clear code

--- node/game/src/handler/service/game_scene_server_player_handler.cpp
+++ node/game/src/handler/service/game_scene_server_player_handler.cpp
@@ -15,7 +15,9 @@ void GamePlayerSceneServiceHandler::EnterScene(entt::entity player,
 	::google::protobuf::Empty* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
-//todo进入了gate 然后才可以开始可以给客户端发送信息了, gs消息顺序问题要注意，进入a, 再进入b gs到达客户端消息的顺序不一样
+	LOG_INFO << "Handling GsEnterSceneRequest for player: " << tls.registry.get<Guid>(player) << ", scene_id: " << request->scene_id();
+
+	// 进入了gate 然后才可以开始可以给客户端发送信息了, gs消息顺序问题要注意，进入a, 再进入b gs到达客户端消息的顺序不一样
 	PlayerSceneSystem::EnterScene(player, request->scene_id());
 ///<<< END WRITING YOUR CODE
 }
@@ -25,12 +27,15 @@ void GamePlayerSceneServiceHandler::LeaveScene(entt::entity player,
 	::google::protobuf::Empty* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
+	LOG_INFO << "Handling GsLeaveSceneRequest for player: " << tls.registry.get<Guid>(player);
+
 	PlayerSceneSystem::LeaveScene(player);
-	if (request->change_gs())//存储完毕以后才能换场景，防止回档
+	if (request->change_gs()) // 存储完毕以后才能换场景，防止回档
 	{
-		//离开gs 清除session
+		// 离开gs 清除session
 		PlayerNodeSystem::SavePlayer(player);
 		tls.registry.emplace<UnregisterPlayer>(player);
+		LOG_INFO << "Player " << tls.registry.get<Guid>(player) << " session cleared after leaving scene.";
 	}
 ///<<< END WRITING YOUR CODE
 }
@@ -40,14 +45,17 @@ void GamePlayerSceneServiceHandler::EnterSceneS2C(entt::entity player,
 	::EnterScenerS2CResponse* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
-	const auto scene_entity = tls.registry.try_get<SceneEntityComp>(player);
-	if (nullptr == scene_entity)
+	LOG_INFO << "Handling EnterSceneS2CRequest for player: " << tls.registry.get<Guid>(player);
+
+	const auto sceneEntity = tls.registry.try_get<SceneEntityComp>(player);
+	if (sceneEntity == nullptr)
 	{
-		LOG_ERROR << " player not enter scene ";
+		LOG_ERROR << "Player " << tls.registry.get<Guid>(player) << " has not entered any scene.";
 		return;
 	}
+
 	::EnterSceneS2C message;
-	message.mutable_scene_info()->CopyFrom(tls.registry.get<SceneInfo>(scene_entity->sceneEntity));
+	message.mutable_scene_info()->CopyFrom(tls.registry.get<SceneInfo>(sceneEntity->sceneEntity));
 	SendToPlayer(ClientPlayerSceneServicePushEnterSceneS2CMsgId, message, player);
 ///<<< END WRITING YOUR CODE
 }
