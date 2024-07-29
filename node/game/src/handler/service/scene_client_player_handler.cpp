1d921447a66d65f8238f722de2d25e923cd6ef8f
luhailong <lhpcpp6@163.com>
2024-07-29 17:59:52 +0800

    clear code

--- node/game/src/handler/service/scene_client_player_handler.cpp
+++ node/game/src/handler/service/scene_client_player_handler.cpp
@@ -16,35 +16,34 @@ void ClientPlayerSceneServiceHandler::EnterSceneC2S(entt::entity player,
 	::EnterSceneC2SResponse* response)
 {
 ///<<< BEGIN WRITING YOUR CODE
-        //如果是跨服副本服不能换场景
-    if ( auto game_node_type = gGameNode->GetNodeType() ;
-        eGameNodeType::kRoomNode == game_node_type ||
-        eGameNodeType::kRoomSceneCrossNode == game_node_type)
-    {
-        response->mutable_error()->set_id(kRetEnterSceneServerType);
-        return;
-    }
-    auto& scene_info = request->scene_info();
-    if (scene_info.scene_confid() <= 0 && scene_info.guid() <= 0)
-    {
-        response->mutable_error()->set_id(kRetEnterSceneParamError);
-        return;
-    }
-    //您当前就在这个场景，无需切换
-    if (const auto current_scene = tls.registry.try_get<SceneEntityComp>(player); nullptr != current_scene)
-    {
-        if (const auto my_scene_info = tls.registry.try_get<SceneInfo>(current_scene->sceneEntity);
-            nullptr != my_scene_info &&
-            my_scene_info->guid() == scene_info.guid()
-            && scene_info.guid() > 0)
-        {
-            response->mutable_error()->set_id(kRetEnterSceneYouInCurrentScene);
-            return;
-        }
-    }
-    CentreEnterSceneRequest rq;
-    rq.mutable_scene_info()->CopyFrom(request->scene_info());
-    Send2CentrePlayer(CentreScenePlayerServiceEnterSceneMsgId, rq, player);
+	auto game_node_type = gGameNode->GetNodeType();
+	if (game_node_type == eGameNodeType::kRoomNode ||
+		game_node_type == eGameNodeType::kRoomSceneCrossNode)
+	{
+		response->mutable_error()->set_id(kRetEnterSceneServerType);
+		return;
+	}
+
+	const auto& scene_info = request->scene_info();
+	if (scene_info.scene_confid() <= 0 && scene_info.guid() <= 0)
+	{
+		response->mutable_error()->set_id(kRetEnterSceneParamError);
+		return;
+	}
+
+	if (auto current_scene_comp = tls.registry.try_get<SceneEntityComp>(player))
+	{
+		const auto current_scene_info = tls.registry.try_get<SceneInfo>(current_scene_comp->sceneEntity);
+		if (current_scene_info && current_scene_info->guid() == scene_info.guid() && scene_info.guid() > 0)
+		{
+			response->mutable_error()->set_id(kRetEnterSceneYouInCurrentScene);
+			return;
+		}
+	}
+
+	CentreEnterSceneRequest rq;
+	rq.mutable_scene_info()->CopyFrom(scene_info);
+	Send2CentrePlayer(CentreScenePlayerServiceEnterSceneMsgId, rq, player);
 ///<<< END WRITING YOUR CODE
 }
 
