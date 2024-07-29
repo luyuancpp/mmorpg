1febc4e3fd64878129acc6ed36fcb1191add6d0c
luhailong <lhpcpp6@163.com>
2024-07-29 15:56:54 +0800

    clear code

--- node/game/src/handler/service/game_server_player_handler.cpp
+++ node/game/src/handler/service/game_server_player_handler.cpp
@@ -14,10 +14,13 @@ void GamePlayerServiceHandler::Centre2GsLogin(entt::entity player,
 	::google::protobuf::Empty* response)
 {
 	///<<< BEGIN WRITING YOUR CODE
-	if (request->enter_gs_type() == LOGIN_NONE)//登录，不是普通切换场景
+	LOG_INFO << "Handling Centre2GsLoginRequest for player: " << tls.registry.get<Guid>(player) << ", enter_gs_type: " << request->enter_gs_type();
+
+	if (request->enter_gs_type() == LOGIN_NONE) // 登录，不是普通切换场景
 	{
 		return;
 	}
+
 	PlayerNodeSystem::OnPlayerLogin(player, request->enter_gs_type());
 	///<<< END WRITING YOUR CODE
 }
