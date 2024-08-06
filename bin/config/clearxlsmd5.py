63f269b578bd1c53ec38c91eee9fd96d4506f7c0
luhailong <lhpcpp6@163.com>
2024-08-06 14:50:16 +0800

    clear code

--- bin/config/clearxlsmd5.py
+++ bin/config/clearxlsmd5.py
@@ -1,19 +1,21 @@
 #!/usr/bin/env python
 # coding=utf-8
-import xlrd
-import xlwt
-import json
-import md5tool
-import os.path
-from os import listdir
-from os.path import isfile, join
 
-xlsdir = "xlsx/"
+import os
+
+xls_dir = "xlsx/"
 
 def main():
-        for filename in listdir(xlsdir):
-                filename = xlsdir + filename
-                if filename.endswith('.xlsx') or filename.endswith('.xls'):
-                        os.remove(filename + ".md5")
-                     
-main()
+    for filename in os.listdir(xls_dir):
+        full_path = os.path.join(xls_dir, filename)
+        if os.path.isfile(full_path) and (filename.endswith('.xlsx') or filename.endswith('.xls')):
+            md5_file_path = full_path + ".md5"
+            try:
+                if os.path.exists(md5_file_path):
+                    os.remove(md5_file_path)
+                    print(f"Deleted: {md5_file_path}")
+            except Exception as e:
+                print(f"Error deleting {md5_file_path}: {e}")
+
+if __name__ == "__main__":
+    main()
