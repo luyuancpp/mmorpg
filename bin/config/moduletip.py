78e34c2b46a09d9c222cd9381300c04c44a4a16c
luhailong <lhpcpp6@163.com>
2024-08-06 13:35:39 +0800

    add tips

--- bin/config/moduletip.py
+++ bin/config/moduletip.py
@@ -12,6 +12,10 @@ sheet = workbook.sheet_by_index(0)
 # Number of rows in the sheet
 num_rows = sheet.nrows
 
+# Initialize variables for global unique IDs
+global_group_id = 1
+global_row_id = 1
+
 # Initialize list to store groups
 groups = []
 current_group = []
@@ -25,23 +29,25 @@ for row_idx in range(8, num_rows):
     if all(cell.value == '' for cell in row_cells):
         # Add current group to groups list if not empty
         if current_group:
-            groups.append(current_group)
+            groups.append((global_group_id, current_group))
             current_group = []
+            global_group_id += 1
     else:
-        # Add row to current group
-        current_group.append([cell.value for cell in row_cells])
+        # Add row to current group with unique row ID
+        current_group.append((global_row_id, [cell.value for cell in row_cells]))
+        global_row_id += 1
 
 # Add the last group if not empty
 if current_group:
-    groups.append(current_group)
+    groups.append((global_group_id, current_group))
 
 # Close the workbook
 workbook.release_resources()
 del workbook
 
-# Print groups
-for group_idx, group in enumerate(groups, start=1):
-    print(f"Group {group_idx}:")
-    for row in group:
-        print(row)
+# Print groups with global unique IDs
+for group_id, group in groups:
+    print(f"Group {group_id}:")
+    for row_id, row in group:
+        print(f"Row {row_id}: {row}")
     print()
