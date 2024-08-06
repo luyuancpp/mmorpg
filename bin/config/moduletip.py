31b88b9a39fefea02247abe17d6ca69df0a47685
luhailong <lhpcpp6@163.com>
2024-08-06 13:48:38 +0800

    add tips

--- bin/config/moduletip.py
+++ bin/config/moduletip.py
@@ -20,22 +20,28 @@ global_row_id = 1
 groups = []
 current_group = []
 
+# Flag to check if the current group has started
+group_started = False
+
 # Starting from row 8 (index 7), read the data
 for row_idx in range(8, num_rows):
     # Read each cell in the row
     row_cells = sheet.row(row_idx)
 
-    # Check if the row is empty (assuming all cells are empty)
-    if all(cell.value == '' for cell in row_cells):
-        # Add current group to groups list if not empty
+    # Check if the row starts with '//'
+    if row_cells[0].value.startswith('//'):
+        # If current group has started, add it to groups list
         if current_group:
             groups.append((global_group_id, current_group))
             current_group = []
             global_group_id += 1
+        # Set group started flag to True
+        group_started = True
     else:
-        # Add row to current group with unique row ID
-        current_group.append((global_row_id, [cell.value for cell in row_cells]))
-        global_row_id += 1
+        # If group has started, add row to current group with unique row ID
+        if group_started:
+            current_group.append((global_row_id, [cell.value for cell in row_cells]))
+            global_row_id += 1
 
 # Add the last group if not empty
 if current_group:
@@ -47,7 +53,13 @@ del workbook
 
 # Print groups with global unique IDs
 for group_id, group in groups:
-    print(f"Group {group_id}:")
+    print(f"// Group {group_id}:")
     for row_id, row in group:
-        print(f"Row {row_id}: {row}")
-    print()
+        print(f"// Row {row_id}: {row}")
+    print("//")
+
+# Clear all records and reset variables for a new process
+global_group_id = 1
+global_row_id = 1
+groups = []
+current_group = []
