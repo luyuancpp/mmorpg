b4e451347bab9773e280687d5b697d05c057686b
luhailong <lhpcpp6@163.com>
2024-08-08 20:50:20 +0800

    clear code

--- node/game/src/game_logic/scene/util/navigation_util.cpp
+++ node/game/src/game_logic/scene/util/navigation_util.cpp
@@ -5,24 +5,16 @@
 #include "muduo/base/Logging.h"
 
 #include "mainscene_config.h"
-#include "scene_config.h"
 
 #include "DetourCrowd/DetourCrowd.h"
 #include "game_node.h"
 #include "game_logic/network/message_util.h"
 #include "service/centre_scene_service.h"
-#include "game_logic/player/util/player_util.h"
 #include "game_logic/scene/util/recast_util.h"
 #include "scene/util/scene_util.h"
-#include "thread_local/storage.h"
 #include "thread_local/storage_game.h"
 #include "logic/event/scene_event.pb.h"
-#include "game_logic/scene/comp/grid_comp.h"
 #include "game_logic/scene/constants/nav_constants.h"
-#include "scene/comp/scene_comp.h"
-
-#include "proto/logic/component/player_comp.pb.h"
-#include "proto/logic/constants/node.pb.h"
 
 void NavigationUtil::LoadMainSceneNavBins() {
 	auto& configAll = GetMainSceneAllTable();
