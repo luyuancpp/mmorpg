54f242bcb174c42d964e3559ed50275110f6a0ab
luhailong <lhpcpp6@163.com>
2024-08-15 14:01:52 +0800

    abilit test

--- test/ability_test/ability_test.cpp
+++ test/ability_test/ability_test.cpp
@@ -13,6 +13,7 @@
 using ::testing::_;
 using ::testing::Return;
 
+
 // Mocked functions and data
 class MockAbilityTable {
 public:
@@ -36,15 +37,19 @@ protected:
 		// Optionally, you can use dependency injection or setup to replace real implementations
 	}
 
+	void TearDown() override {
+		tls.registry.clear();
+	}
+
 	std::unique_ptr<AbilityUtil> abilityUtil;
 	std::unique_ptr<MockAbilityTable> mockAbilityTable;
 	std::unique_ptr<MockCooldownTimeUtil> mockCooldownTimeUtil;
 };
 
 TEST_F(AbilityUtilTest, ValidateAbilityTable_InvalidId_ReturnsError) {
-	const uint32_t invalidAbilityId = 9999;
+	const uint32_t invalidAbilityId = 9999999;
 	EXPECT_CALL(*mockAbilityTable, GetAbilityTable(invalidAbilityId))
-		.WillOnce(Return(nullptr));
+		.WillRepeatedly(Return(nullptr));
 
 	auto [tableAbility, result] = abilityUtil->ValidateAbilityTable(invalidAbilityId);
 	EXPECT_EQ(result, kInvalidTableId);
@@ -56,27 +61,33 @@ TEST_F(AbilityUtilTest, ValidateTarget_InvalidTarget_ReturnsError) {
 	request.set_target_id(-1); // Invalid target ID
 
 	EXPECT_CALL(*mockAbilityTable, GetAbilityTable(request.ability_id()))
-		.WillOnce(Return(nullptr)); // Mock implementation
+		.WillRepeatedly(Return(nullptr)); // Mock implementation
 
 	uint32_t result = abilityUtil->ValidateTarget(&request);
 	EXPECT_EQ(result, kAbilityInvalidTargetId);
 }
 
 TEST_F(AbilityUtilTest, CheckCooldown_CooldownActive_ReturnsError) {
-	entt::entity caster{ 1 };
+	entt::entity caster = tls.registry.create();
+
 	auto tableAbility = std::make_shared<ability_row>();
 	tableAbility->set_cooldown_id(1);
 
 	CooldownTimeComp cooldownTimeComp;
 	EXPECT_CALL(*mockCooldownTimeUtil, IsInCooldown(_))
-		.WillOnce(Return(true));
+		.WillRepeatedly(Return(true));
+
+	auto& cooldownList = tls.registry.emplace<CooldownTimeListComp>(caster);
+	cooldownTimeComp.set_cooldown_table_id(1);
+	CoolDownTimeMillisecondUtil::Reset(cooldownTimeComp);
+	cooldownList.mutable_cooldown_list()->emplace(1, cooldownTimeComp);
 
 	uint32_t result = abilityUtil->CheckCooldown(caster, tableAbility.get());
 	EXPECT_EQ(result, kAbilityCooldownNotReady);
 }
 
 TEST_F(AbilityUtilTest, HandleCastingTimer_ImmediateAbility_ReturnsOk) {
-	entt::entity caster{ 1 };
+	entt::entity caster = tls.registry.create();
 	auto tableAbility = std::make_shared<ability_row>();
 	tableAbility->set_immediately(true);
 
@@ -85,21 +96,21 @@ TEST_F(AbilityUtilTest, HandleCastingTimer_ImmediateAbility_ReturnsOk) {
 	// Simulate timer behavior if necessary
 
 	EXPECT_CALL(*mockAbilityTable, GetAbilityTable(_))
-		.WillOnce(Return(tableAbility.get())); // Mock implementation
+		.WillRepeatedly(Return(tableAbility.get())); // Mock implementation
 
 	uint32_t result = abilityUtil->HandleCastingTimer(caster, tableAbility.get());
 	EXPECT_EQ(result, kOK);
 }
 
 TEST_F(AbilityUtilTest, BroadcastAbilityUsedMessage_CreatesMessage) {
-	entt::entity caster{ 1 };
+	entt::entity caster = tls.registry.create();
 	::UseAbilityRequest request;
 	request.set_ability_id(1);
 	request.set_target_id(2);
 	request.mutable_position()->set_x(10); // Mock position
 
 	EXPECT_CALL(*mockAbilityTable, GetAbilityTable(request.ability_id()))
-		.WillOnce(Return(nullptr)); // Mock implementation
+		.WillRepeatedly(Return(nullptr)); // Mock implementation
 
 	// Mock the ViewUtil::BroadcastMessageToVisiblePlayers function if possible
 	// If not possible, test if BroadcastAbilityUsedMessage behaves correctly
@@ -108,22 +119,29 @@ TEST_F(AbilityUtilTest, BroadcastAbilityUsedMessage_CreatesMessage) {
 }
 
 TEST_F(AbilityUtilTest, SetupCastingTimer_SetsTimer) {
-	entt::entity caster{ 1 };
+	entt::entity caster = tls.registry.create();
 	auto tableAbility = std::make_shared<ability_row>();
 	tableAbility->set_castpoint(1000); // Set cast point to 1000ms
 
 	EXPECT_CALL(*mockAbilityTable, GetAbilityTable(_))
-		.WillOnce(Return(tableAbility.get())); // Mock implementation
+		.WillRepeatedly(Return(tableAbility.get())); // Mock implementation
 
 	EXPECT_CALL(*mockAbilityTable, IsAbilityOfType(_, kGeneralAbility))
-		.WillOnce(Return(true));
+		.WillRepeatedly(Return(true));
 
 	EXPECT_NO_THROW(abilityUtil->SetupCastingTimer(caster, tableAbility.get(), 1));
 }
 
 int main(int argc, char** argv) {
+	
+
+	EventLoop loop;
+
 	CooldownConfigurationTable::GetSingleton().Load();
 	AbilityConfigurationTable::GetSingleton().Load();
+
 	::testing::InitGoogleTest(&argc, argv);
-	return RUN_ALL_TESTS();
+	auto ret = RUN_ALL_TESTS();
+	tls.registry.clear();
+	return ret;
 }
