89589d7c2aed01fd1929b1f77a89ffe5ce36e697
luhailoing <lhlcpp6@163.com>
2024-08-18 15:24:40 +0800

    buff system

---
+++ node/game/src/game_logic/combat/buff/constants/buff_constants.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+enum eBuffTag {
+    kNone = 0,
+    kMetal = 1 << 1,
+    kWood = 1 << 2,
+    kWater = 1 << 3,
+    kFire = 1 << 4,
+    kEarth = 1 << 5,
+    kControl = 1 << 6,
+    kStrongControl = 1 << 7,
+};
