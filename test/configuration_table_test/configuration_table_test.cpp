4f8ce1332d65280f971ff5d0ad52a760b523255f
luhailong <lhpcpp6@163.com>
2024-08-22 09:56:19 +0800

    table test

--- test/configuration_table_test/configuration_table_test.cpp
+++ test/configuration_table_test/configuration_table_test.cpp
@@ -39,9 +39,52 @@ TEST(LoadAllConfigTest, GetTestMultiKeyAllTable)
 	{
 		LOG_INFO << it.DebugString();
 	}
+}
 
+TEST(LoadAllConfigTest, GetTestMultiUint32ValueRangeTable)
+{
 	auto& data = TestMultiKeyConfigurationTable::GetSingleton().GetMuint32KeyData();
-	auto list_value = data.equal_range(17);
+	auto range17 = data.equal_range(17);
+	for (auto it = range17.first; it != range17.second; ++it) {
+		LOG_INFO << it->first << ' ' << it->second->DebugString();
+
+	}
+}
+
+TEST(LoadAllConfigTest, GetTestMultiInt32ValueRangeTable)
+{
+	auto& data = TestMultiKeyConfigurationTable::GetSingleton().GetMin32KeyData();
+	auto range10 = data.equal_range(10);
+	for (auto it = range10.first; it != range10.second; ++it) {
+		LOG_INFO << it->first << ' ' << it->second->DebugString();
+	}
+}
+
+TEST(LoadAllConfigTest, GetTestMultiStringValueRangeTable)
+{
+	auto& data = TestMultiKeyConfigurationTable::GetSingleton().GetMstringkeyData();
+	auto rangeaa = data.equal_range("aa");
+	for (auto it = rangeaa.first; it != rangeaa.second; ++it) {
+		LOG_INFO << it->first << ' ' << it->second->DebugString();
+	}
+}
+
+TEST(LoadAllConfigTest, GetTestUint32ValueFindTable)
+{
+	auto it = TestMultiKeyConfigurationTable::GetSingleton().GetByUint32Key(14);
+	EXPECT_EQ(it.first->id(), 1);
+}
+
+TEST(LoadAllConfigTest, GetTestInt32ValueAllTable)
+{
+	auto it = TestMultiKeyConfigurationTable::GetSingleton().GetByIn32Key(8);
+	EXPECT_EQ(it.first->id(), 1);
+}
+
+TEST(LoadAllConfigTest, GetTestStringValueAllTable)
+{
+	auto it = TestMultiKeyConfigurationTable::GetSingleton().GetByStringkey("aa");
+	EXPECT_EQ(it.first->id(), 1);
 }
 
 // Main function
