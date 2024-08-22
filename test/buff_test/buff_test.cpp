b837d8edbdc7524ca82678d1017f6a6c20a8ae25
luhailong <lhpcpp6@163.com>
2024-08-22 14:20:21 +0800

    add test

--- test/buff_test/buff_test.cpp
+++ test/buff_test/buff_test.cpp
@@ -1,31 +1,25 @@
-#include "gtest/gtest.h"
-#include "gmock/gmock.h"
-#include "game_logic/combat/ability/util/ability_util.h"
-#include "thread_local/storage.h"
-#include "ability_config.h"
+#include <gtest/gtest.h>
+#include <gmock/gmock.h>
+#include "game_logic/combat/buff/util/buff_util.h"
 #include "buff_config.h"
-#include "game_logic/combat/ability/comp/ability_comp.h"
-#include "game_logic/combat/ability/constants/ability_constants.h"
+#include "buff_error_tip.pb.h"
+#include "thread_local/storage.h"
+#include "proto/logic/event/buff_event.pb.h"
+#include "game_logic/combat/buff/comp/buff_comp.h"
 #include "time/comp/timer_task_comp.h"
-#include "time/util/cooldown_time_util.h"
-#include "pbc/ability_error_tip.pb.h"
+#include "pbc/buff_error_tip.pb.h"
 #include "pbc/common_error_tip.pb.h"
-
-using ::testing::_;
-using ::testing::Return;
-
-
 
 // Main function
 int main(int argc, char** argv) {
-    EventLoop loop;
-
     ::testing::InitGoogleTest(&argc, argv);
-    CooldownConfigurationTable::GetSingleton().Load();
-    AbilityConfigurationTable::GetSingleton().Load();
+
+    // Assuming BuffConfigurationTable::GetSingleton().Load() is necessary
     BuffConfigurationTable::GetSingleton().Load();
 
     int ret = RUN_ALL_TESTS();
-    tls.registry.clear(); // Clean up thread-local storage after all tests
+
+    // Clean up thread-local storage after all tests
+    tls.registry.clear();
     return ret;
 }
