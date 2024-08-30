cfe3b80ffd6fbff4310eb522970e812cf4b21d3b
luhailong <lhpcpp6@163.com>
2024-08-30 16:42:39 +0800

    clear code

--- test/ability_test/mockview.cpp
+++
@@ -1,7 +0,0 @@
-#include "game_logic/scene/util/view_util.h"
-
-void ViewUtil::BroadcastMessageToVisiblePlayers(entt::entity entity, const uint32_t message_id,
-	const google::protobuf::Message& message)
-{
-
-}
