b1a2c091ea67700b9c80a5b2a37b91046061998a
luhailong <lhpcpp6@163.com>
2024-09-19 14:18:59 +0800

    clear code

--- pkg/logic/src/time/util/time_util.cpp
+++ pkg/logic/src/time/util/time_util.cpp
@@ -5,41 +5,51 @@
 
 #include <boost/date_time/posix_time/posix_time.hpp>
 
+uint64_t PTimeToSeconds(const boost::posix_time::ptime& pt) {
+	// 1970年1月1日的时间点
+	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
+	return (pt - epoch).total_seconds();  // 返回自1970年以来的秒数
+}
+
+uint64_t PTimeToMilliseconds(const boost::posix_time::ptime& pt) {
+	// 1970年1月1日的时间点
+	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
+	return (pt - epoch).total_milliseconds();  // 返回自1970年以来的毫秒数
+}
+
+uint64_t PTimeToMicroseconds(const boost::posix_time::ptime& pt) {
+	// 1970年1月1日的时间点
+	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
+	return (pt - epoch).total_microseconds();  // 返回自1970年以来的微秒数
+}
+
 uint64_t TimeUtil::NowMicroseconds()
 {
 	// 获取当前 UTC 时间
 	boost::posix_time::ptime utc_now = boost::posix_time::second_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的微秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_microseconds();
+	return PTimeToMicroseconds(utc_now);
 }
 
 uint64_t TimeUtil::NowMilliseconds() {
 	// 获取当前 UTC 时间
 	boost::posix_time::ptime utc_now = boost::posix_time::microsec_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的毫秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_milliseconds();
+	return PTimeToMilliseconds(utc_now);
 }
 
 uint64_t TimeUtil::NowSeconds() {
 	// 获取当前 UTC 时间
-	boost::posix_time::ptime utc_now = boost::posix_time::second_clock::universal_time();
+	boost::posix_time::ptime utc_now = boost::posix_time::microsec_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_seconds();
+	return PTimeToMilliseconds(utc_now);
 }
 
 uint64_t TimeUtil::NowMillisecondsUTC() {
 	// 获取当前 UTC 时间
 	boost::posix_time::ptime utc_now = boost::posix_time::microsec_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的毫秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_milliseconds();
+	return PTimeToMilliseconds(utc_now);
 }
 
 uint64_t TimeUtil::NowMicrosecondsUTC()
@@ -47,16 +57,12 @@ uint64_t TimeUtil::NowMicrosecondsUTC()
 	// 获取当前 UTC 时间
 	boost::posix_time::ptime utc_now = boost::posix_time::second_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的微秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_microseconds();
+	return PTimeToMicroseconds(utc_now);
 }
 
 uint64_t TimeUtil::NowSecondsUTC() {
 	// 获取当前 UTC 时间
 	boost::posix_time::ptime utc_now = boost::posix_time::second_clock::universal_time();
 
-	// 将时间转换为自1970年1月1日以来的秒数
-	boost::posix_time::ptime epoch(boost::gregorian::date(1970, 1, 1), boost::posix_time::time_duration(0, 0, 0));
-	return (utc_now - epoch).total_seconds();
+	return PTimeToSeconds(utc_now);
 }
