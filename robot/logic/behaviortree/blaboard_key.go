a2a233a740c181ee8047a55c80ac7164f900a7e2
luhailong <lhpcpp6@163.com>
2024-09-20 16:29:31 +0800

    skill bt

--- robot/logic/behaviortree/blaboard_key.go
+++ robot/logic/behaviortree/blaboard_key.go
@@ -2,10 +2,12 @@ package behaviortree
 
 // 预定义的一些常量，用于标识行为树中的关键名称
 const (
-	ClientBoardKey           = "clientIdentifier" // 客户端标识
-	SceneInformationBoardKey = "sceneInformation" // 场景信息
-	PlayerListBoardKey       = "playerList"       // 玩家列表
-	TestCountBoardKey        = "testCount"        // 切换场景次数常量
-	ActorListBoardKey        = "actorList"        // 演员列表
-	HatredTargetBoardKey     = "hatredTarget"     // 仇恨目标
+	ClientBoardKey           = "ClientIdentifier" // 客户端标识
+	SceneInformationBoardKey = "SceneInformation" // 场景信息
+	PlayerListBoardKey       = "PlayerList"       // 玩家列表
+	TestCountBoardKey        = "TestCount"        // 切换场景次数常量
+	ActorListBoardKey        = "ActorList"        // 演员列表
+	HatredTargetBoardKey     = "HatredTarget"     // 仇恨目标
+	SkillListBoardKey        = "SkillList"        // 技能列表
+	SkillIDBoardKey          = "SkillID"          //当前的技能ID
 )
--- robot/logic/behaviortree/skill.go
+++ robot/logic/behaviortree/skill.go
@@ -2,6 +2,10 @@ package behaviortree
 
 import (
 	b3 "github.com/magicsea/behavior3go"
+	"go.uber.org/zap"
+	"robot/interfaces"
+	"robot/pb/game"
+
 	//. "github.com/magicsea/behavior3go/actions"
 	//. "github.com/magicsea/behavior3go/composites"
 	. "github.com/magicsea/behavior3go/config"
@@ -47,33 +51,58 @@ func (s *CheckSkillByDistance) OnTick(tick *Tick) b3.Status {
 type GetSkillID struct {
 	Action
 	SkillListBoard string
-	SkillIndex     uint32
+	SkillIndex     int
 	SkillIDBoard   string
 }
 
 func (s *GetSkillID) Initialize(setting *BTNodeCfg) {
 	s.Action.Initialize(setting)
 	s.SkillListBoard = setting.GetPropertyAsString("SkillListBoard")
-	s.SkillIndex = uint32(setting.GetProperty("SkillIndex"))
+	s.SkillIndex = setting.GetPropertyAsInt("SkillIndex")
 	s.SkillIDBoard = setting.GetPropertyAsString("SkillIDBoard")
 }
 
 func (s *GetSkillID) OnTick(tick *Tick) b3.Status {
+
+	playerSkillListPBComp, ok := tick.Blackboard.GetMem(SkillListBoardKey).([]*game.PlayerSkillListPBComp)
+	if !ok {
+		zap.L().Debug("Failed to retrieve skill list from blackboard", zap.String("Key", SkillListBoardKey), zap.Any("Value", tick.Blackboard.GetMem(SkillListBoardKey)))
+		return b3.FAILURE
+	}
+
+	if s.SkillIndex >= len(playerSkillListPBComp) {
+		zap.L().Debug("Invalid skill index", zap.Int("SkillIndex", s.SkillIndex), zap.Int("SkillListLength", len(playerSkillListPBComp)))
+		return b3.FAILURE
+	}
+
+	tick.Blackboard.Set(s.SkillIDBoard, playerSkillListPBComp[s.SkillIndex], "", "")
 
 	return b3.SUCCESS
 }
 
 type ReleaseSkill struct {
 	Action
-	skillIDBoard string
+	SkillIDBoard string
 }
 
 func (s *ReleaseSkill) Initialize(setting *BTNodeCfg) {
 	s.Action.Initialize(setting)
-	s.skillIDBoard = setting.GetPropertyAsString("skillIDBoard")
+	s.SkillIDBoard = setting.GetPropertyAsString("skillIDBoard")
 }
 
 func (s *ReleaseSkill) OnTick(tick *Tick) b3.Status {
+	// 从黑板中获取客户端
+	client, ok := tick.Blackboard.GetMem(ClientBoardKey).(interfaces.GameClientInterface)
+	if !ok {
+		zap.L().Error("Failed to cast client from blackboard", zap.String("Key", ClientBoardKey), zap.Any("Value", tick.Blackboard.GetMem(ClientBoardKey)))
+		return b3.FAILURE
+	}
+
+	rq := &game.ReleaseSkillSkillRequest{}
+	rq.SkillTableId = uint32(tick.Blackboard.GetInt32(s.SkillIDBoard, "", ""))
+
+	zap.L().Info("Sending skill release request", zap.Uint32("SkillTableId", rq.SkillTableId))
+	client.Send(rq, game.PlayerSkillServiceReleaseSkillMessageId)
 
 	return b3.SUCCESS
 }
