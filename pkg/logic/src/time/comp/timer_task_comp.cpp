bda9fe39a76094c3736bc202455858fb496c7cf1
luhailoing <lhlcpp6@163.com>
2024-09-25 14:28:01 +0800

    clear code

--- pkg/logic/src/time/comp/timer_task_comp.cpp
+++ pkg/logic/src/time/comp/timer_task_comp.cpp
@@ -1,108 +1,87 @@
 #include "timer_task_comp.h"
-
 #include "muduo/net/Timer.h"
 #include "muduo/net/TimerId.h"
 #include "time/util/time_util.h"
 
-#define  tlsEventLoop EventLoop::getEventLoopOfCurrentThread()
+#define tlsEventLoop EventLoop::getEventLoopOfCurrentThread()
 
-TimerTaskComp::~TimerTaskComp()
-{
+TimerTaskComp::~TimerTaskComp() {
     Cancel();
 }
 
-TimerTaskComp::TimerTaskComp(const TimerTaskComp& param)
-{
+TimerTaskComp::TimerTaskComp(const TimerTaskComp& param) {
     const_cast<TimerTaskComp&>(param).Cancel();
 }
 
-TimerTaskComp::TimerTaskComp( TimerTaskComp&& param) noexcept
-{
+TimerTaskComp::TimerTaskComp(TimerTaskComp&& param) noexcept {
     param.Cancel();
 }
 
-TimerTaskComp & TimerTaskComp::operator=( TimerTaskComp&& param)
-{
+TimerTaskComp& TimerTaskComp::operator=(TimerTaskComp&& param) {
     param.Cancel();
     return *this;
 }
 
-void TimerTaskComp::RunAt(const Timestamp& time, const TimerCallback& cb)
-{
-	Cancel();
+void TimerTaskComp::RunAt(const Timestamp& time, const TimerCallback& cb) {
+    Cancel();
     callback = cb;
-	timerId = tlsEventLoop->runAt(time, std::bind(&TimerTaskComp::OnTimer, this));
+    timerId = tlsEventLoop->runAt(time, std::bind(&TimerTaskComp::OnTimer, this));
     UpdateEndStamp();
 }
 
-void TimerTaskComp::RunAfter(double delay, const TimerCallback& cb)
-{
-	Cancel();
+void TimerTaskComp::RunAfter(double delay, const TimerCallback& cb) {
+    Cancel();
     callback = cb;
     timerId = tlsEventLoop->runAfter(delay, std::bind(&TimerTaskComp::OnTimer, this));
     UpdateEndStamp();
 }
 
-void TimerTaskComp::RunEvery(double interval, const TimerCallback& cb)
-{
-    Cancel();	
+void TimerTaskComp::RunEvery(double interval, const TimerCallback& cb) {
+    Cancel();
     callback = cb;
     timerId = tlsEventLoop->runEvery(interval, std::bind(&TimerTaskComp::OnTimer, this));
     UpdateEndStamp();
 }
 
-void TimerTaskComp::Run()
-{
-    if (!callback)
-    {
-        return;
+void TimerTaskComp::Run() {
+    if (callback) {
+        callback();
     }
-    callback();
 }
 
-void TimerTaskComp::Cancel()
-{
+void TimerTaskComp::Cancel() {
     tlsEventLoop->cancel(timerId);
     timerId = TimerId();
     endTime = Timestamp();
     assert(nullptr == timerId.GetTimer());
 }
 
-bool TimerTaskComp::IsActive() const
-{
+bool TimerTaskComp::IsActive() const {
     return !(endTime.invalid() == endTime);
 }
 
-uint64_t TimerTaskComp::GetEndTime()
-{
-    if (endTime < Timestamp(TimeUtil::NowSecondsUTC()))
-    {
+uint64_t TimerTaskComp::GetEndTime() {
+    if (endTime < Timestamp(TimeUtil::NowSecondsUTC())) {
         return 0;
     }
     return timerId.GetTimer()->expiration().secondsSinceEpoch();
 }
 
-void TimerTaskComp::UpdateEndStamp()
-{
-    if (nullptr == timerId.GetTimer())
-    {
+void TimerTaskComp::UpdateEndStamp() {
+    if (nullptr == timerId.GetTimer()) {
         return;
     }
     endTime = timerId.GetTimer()->expiration();
 }
 
-void TimerTaskComp::SetCallBack(const TimerCallback& cb)
-{
+void TimerTaskComp::SetCallBack(const TimerCallback& cb) {
     callback = cb;
 }
 
-void TimerTaskComp::OnTimer()
-{
-    if (!callback)
-    {
-        return;
+void TimerTaskComp::OnTimer() {
+    if (callback) {
+        TimerCallback copycb = callback;
+        copycb();
+        endTime = Timestamp();
     }
-    TimerCallback copycb = callback;
-    copycb();
-    endTime = Timestamp();
 }
