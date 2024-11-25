765d2e8295cb94e260faf7d914e0c1bb565ccc1f
luhailoing <lhlcpp6@163.com>
2024-11-25 14:13:50 +0800

    clear code

---
+++ robot/logic/handler/entitysyncservicesyncattribute30frames.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"robot/logic/gameobject"
+	"robot/pb/game"
+)
+
+func EntitySyncServiceSyncAttribute30FramesHandler(player *gameobject.Player, response *game.AttributeDelta30FramesS2C) {
+}
---
+++ robot/logic/handler/entitysyncservicesyncbaseattribute.go
@@ -0,0 +1,9 @@
+package handler
+
+import (
+	"robot/pb/game"
+	"robot/logic/gameobject"
+)
+
+func EntitySyncServiceSyncBaseAttributeHandler(player *gameobject.Player, response *game.BaseAttributeSyncDataS2C) {
+}
