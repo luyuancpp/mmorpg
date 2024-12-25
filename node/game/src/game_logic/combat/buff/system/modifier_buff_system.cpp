925fbe685e0402598065e047bfae68c75e258a6b
luhailoing <lhlcpp6@163.com>
2024-12-25 15:54:21 +0800

    clear code

--- node/game/src/game_logic/combat/buff/system/modifier_buff_system.cpp
+++ node/game/src/game_logic/combat/buff/system/modifier_buff_system.cpp
@@ -1,6 +1,8 @@
 ﻿#include "modifier_buff_system.h"
+
+#include "buff_config.h"
 #include "buff_config.pb.h"
-#include "modifier_buff_impl_system.h"
+#include "component/actor_attribute_state_comp.pb.h"
 #include "game_logic/actor/attribute/constants/actor_state_attribute_calculator_constants.h"
 #include "game_logic/actor/attribute/system/actor_attribute_calculator_system.h"
 #include "game_logic/combat/buff/comp/buff_comp.h"
@@ -51,14 +53,54 @@ bool ModifierBuffSystem::OnBuffDestroy(entt::entity parent, BuffComp& buff, cons
     return false;
 }
 
+static bool OnHealthRegenerationBasedOnLostHealth(entt::entity parent, BuffComp& buffComp, const BuffTable* buffTable)
+{
+    if (buffTable == nullptr) {
+        return false;
+    }
+
+    //todo 及时计算 max_health
+    auto& baseAttributesPbComponent = tls.registry.get<BaseAttributesPbComponent>(parent);
+    const auto& derivedAttributesPbComponent = tls.registry.get<DerivedAttributesPbComponent>(parent);
+    const auto& levelComponent = tls.registry.get<LevelPbComponent>(parent);
+
+    const auto lostHealth = derivedAttributesPbComponent.max_health() - baseAttributesPbComponent.health();  // 计算已损失生命值
+
+    BuffConfigurationTable::Instance().SetHealthregenerationParam(
+        { static_cast<double>(levelComponent.level()),  static_cast<double>(lostHealth) });
+
+    const auto healingAmount = BuffConfigurationTable::Instance().GetHealthregeneration(buffTable->id());
+    const auto currentHealth = std::min<uint64_t>(derivedAttributesPbComponent.max_health(),
+        static_cast<uint64_t>(static_cast<double>(baseAttributesPbComponent.health()) +
+            healingAmount));
+
+    baseAttributesPbComponent.set_health(currentHealth);
+
+    LOG_TRACE << "Healing applied, current health: " << currentHealth;
+
+    return  true;
+}
+
 bool ModifierBuffSystem::OnIntervalThink(entt::entity parent, BuffComp& buffComp, const BuffTable* buffTable)
 {
-    return ModifierBuffUtilImplSystem::OnIntervalThink(parent, buffComp, buffTable);
+
+    switch (buffTable->bufftype())
+    {
+    case kBuffTypeHealthRegenerationBasedOnLostHealth:
+    {
+        OnHealthRegenerationBasedOnLostHealth(parent, buffComp, buffTable);
+        return true;
+    }
+    break;
+    default:
+        return false;
+        break;
+    }
+    return false;
 }
 
 void ModifierBuffSystem::OnSkillHit(entt::entity caster, entt::entity target)
 {
-    ModifierBuffUtilImplSystem::OnSkillHit(caster, target);
 }
 
 bool ModifierBuffSystem::IsMovementSpeedBuff(const BuffTable* buffTable) {
