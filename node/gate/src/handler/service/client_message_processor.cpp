aa4d268de1dc927fddb1a810d86679a6387b5dc3
luhailoing <lhlcpp6@163.com>
2025-01-07 20:45:54 +0800

    clear code

--- node/gate/src/handler/service/client_message_processor.cpp
+++ node/gate/src/handler/service/client_message_processor.cpp
@@ -28,36 +28,32 @@ RpcClientSessionHandler::RpcClientSessionHandler(ProtobufCodec& codec,
 }
 
 //todo 考虑中间一个login服务关了，原来的login服务器处理到一半，新的login处理不了
-entt::entity FindLoginNodeForSession(uint64_t sessionId)
-{
+std::optional<entt::entity> FindLoginNodeForSession(uint64_t sessionId) {
     const auto sessionIt = tls_gate.sessions().find(sessionId);
-    if (sessionIt == tls_gate.sessions().end())
-    {
+    if (sessionIt == tls_gate.sessions().end()) {
         LOG_ERROR << "Session not found for session id: " << sessionId;
-        return entt::null;
+        return std::nullopt;
     }
 
     auto& session = sessionIt->second;
-    if (!session.HasLoginNodeId())
-    {
+    if (!session.HasLoginNodeId()) {
         const auto loginNodeIt = tls_gate.login_consistent_node().GetByHash(sessionId);
-        if (tls_gate.login_consistent_node().end() == loginNodeIt)
-        {
+        if (tls_gate.login_consistent_node().end() == loginNodeIt) {
             LOG_ERROR << "Login server not found for session id: " << sessionId;
-            return entt::null;
+            return std::nullopt;
         }
         session.loginNodeId = entt::to_integral(loginNodeIt->second);
     }
 
     const auto loginNodeIt = tls_gate.login_consistent_node().GetNodeValue(session.loginNodeId);
-    if (tls_gate.login_consistent_node().end() == loginNodeIt)
-    {
+    if (tls_gate.login_consistent_node().end() == loginNodeIt) {
         LOG_ERROR << "Login server crashed for session id: " << sessionId;
         session.loginNodeId = kInvalidNodeId;
-        return entt::null;
+        return std::nullopt;
     }
     return loginNodeIt->second;
 }
+
 
 void RpcClientSessionHandler::OnConnection(const muduo::net::TcpConnectionPtr& conn)
 {
@@ -94,13 +90,16 @@ void RpcClientSessionHandler::SendTipToClient(const muduo::net::TcpConnectionPtr
 {
     TipInfoMessage tipMessage;
     tipMessage.set_id(tipId);
+
     MessageContent message;
     message.set_serialized_message(tipMessage.SerializeAsString());
     message.set_message_id(PlayerClientCommonServiceSendTipToClientMessageId);
+
     g_gate_node->Codec().send(conn, message);
 
     LOG_ERROR << "Sent tip message to session id: " << GetSessionId(conn) << ", tip id: " << tipId;
 }
+
 
 void RpcClientSessionHandler::HandleConnectionEstablished(const muduo::net::TcpConnectionPtr& conn)
 {
@@ -134,7 +133,6 @@ void SetSessionAndParseBody(Message& message, const Request& request, const uint
     }
 }
 
-
 void RpcClientSessionHandler::HandleConnectionDisconnection(const muduo::net::TcpConnectionPtr& conn)
 {
     // 重要: 此消息一定要发，不能只通过centre 的gate disconnect去发
@@ -145,12 +143,12 @@ void RpcClientSessionHandler::HandleConnectionDisconnection(const muduo::net::Tc
 
     // 处理登录服务器和中心服务器的断开通知
     const auto& loginNode = FindLoginNodeForSession(sessionId);
-    if (entt::null != loginNode)
+    if (loginNode)
     {
         LoginNodeDisconnectRequest request;
         request.set_session_id(sessionId);
 
-        SendDisconnectC2LRequest(loginNode, request);
+        SendDisconnectC2LRequest(*loginNode, request);
     }
 
     // 通知中心服务器
@@ -218,7 +216,7 @@ void SendEnterGameRequestToLoginNode(entt::entity loginNode, Guid sessionId, con
 void HandleLoginNodeMessage(Guid sessionId, const RpcClientMessagePtr& request, const muduo::net::TcpConnectionPtr& conn)
 {
     auto loginNode = FindLoginNodeForSession(sessionId);
-    if (entt::null == loginNode)
+    if (!loginNode)
     {
         LOG_ERROR << "Login node not found for session id: " << sessionId << ", message id: " << request->message_id();
         // TODO: Handle connection closure logic here.
@@ -228,13 +226,13 @@ void HandleLoginNodeMessage(Guid sessionId, const RpcClientMessagePtr& request, 
     switch (request->message_id())
     {
     case LoginServiceLoginMessageId:
-        SendLoginRequestToLoginNode(loginNode, sessionId, request);
+        SendLoginRequestToLoginNode(*loginNode, sessionId, request);
         break;
     case LoginServiceCreatePlayerMessageId:
-        SendCreatePlayerRequestToLoginNode(loginNode, sessionId, request);
+        SendCreatePlayerRequestToLoginNode(*loginNode, sessionId, request);
         break;
     case LoginServiceEnterGameMessageId:
-        SendEnterGameRequestToLoginNode(loginNode, sessionId, request);
+        SendEnterGameRequestToLoginNode(*loginNode, sessionId, request);
         break;
     default:
         LOG_ERROR << "Unhandled message id: " << request->message_id() << " for session id: " << sessionId;
