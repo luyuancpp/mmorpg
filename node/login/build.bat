e34f6893537780a8ee2e06f04a454a6c9908f399
luhailong <lhpcpp6@163.com>
2025-03-10 19:32:57 +0800

    clear code

--- node/login/build.bat
+++ node/login/build.bat
@@ -1,3 +1,3 @@
-goctl rpc protoc ../../proto/common/login_service.proto --proto_path=../.. --proto_path=../../third_party/protobuf/src/ --go_out=./ --go-grpc_out=./ --zrpc_out=. -m
-goctl rpc protoc ../../proto/common/deploy_service.proto --proto_path=../.. --proto_path=../../third_party/protobuf/src/ --go_out=./ --go-grpc_out=./  --zrpc_out=. -m
+goctl rpc protoc ../../proto/common/login_service.proto --proto_path=../.. --proto_path=../../third_party/grpc/third_party/protobuf/src/ --go_out=./ --go-grpc_out=./ --zrpc_out=. -m
+goctl rpc protoc ../../proto/common/deploy_service.proto --proto_path=../.. --proto_path=../../third_party/grpc/third_party/protobuf/src/  --go_out=./ --go-grpc_out=./  --zrpc_out=. -m
 pause
