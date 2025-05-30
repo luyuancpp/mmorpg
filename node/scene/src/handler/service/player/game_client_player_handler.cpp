c5506595857aa418c0363bacee8349a4d1550a14
luhailong <lhpcpp6@163.com>
2025-05-30 16:38:18 +0800

    clear code

--- node/scene/src/handler/service/player/game_client_player_handler.cpp
+++ node/scene/src/handler/service/player/game_client_player_handler.cpp
@@ -5,11 +5,23 @@
 ///<<< END WRITING YOUR CODE
 
 
-
-
+void GameClientPlayerCommonServiceHandler::SendTipToClient(entt::entity player,const ::TipInfoMessage* request,
+	::Empty* response)
+{
 ///<<< BEGIN WRITING YOUR CODE
 ///<<< END WRITING YOUR CODE
 
+}
+
+
+
+void GameClientPlayerCommonServiceHandler::KickPlayer(entt::entity player,const ::GameKickPlayerRequest* request,
+	::Empty* response)
+{
+///<<< BEGIN WRITING YOUR CODE
+///<<< END WRITING YOUR CODE
+
+}
 
 
 
