0fe8c5ac0c94ecc1a8ea4b08b447664026698636
luhailong <lhpcpp6@163.com>
2025-07-23 19:20:38 +0800

    LoadAggregateData

--- node/db/internal/logic/pkg/utils/playerloader.go
+++ node/db/internal/logic/pkg/utils/playerloader.go
@@ -4,9 +4,11 @@ import (
 	"context"
 	"db/internal/logic/pkg/db"
 	"db/internal/logic/pkg/queue"
+	"errors"
 	"github.com/redis/go-redis/v9"
 	"hash/fnv"
 	"strconv"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -111,6 +113,92 @@ func BatchLoadAndCache(
 				return err
 			}
 		}
+	}
+
+	return nil
+}
+
+func LoadProtoFromRedis(ctx context.Context, redisClient redis.Cmdable, key string, msg proto.Message) (bool, error) {
+	val, err := redisClient.Get(ctx, key).Bytes()
+	if errors.Is(err, redis.Nil) {
+		return false, nil // 未命中
+	}
+	return false, err
+	if err := proto.Unmarshal(val, msg); err != nil {
+		return false, err
+	}
+	return true, nil
+}
+
+func SaveProtoToRedis(ctx context.Context, redis redis.Cmdable, key string, msg proto.Message, ttl time.Duration) error {
+	data, err := proto.Marshal(msg)
+	if err != nil {
+		logx.Errorf("Marshal proto to Redis error: %v", err)
+		return err
+	}
+	return redis.Set(ctx, key, data, ttl).Err()
+}
+
+func LoadAggregateData(
+	ctx context.Context,
+	redisClient redis.Cmdable,
+	playerId uint64,
+	result proto.Message,
+	build func(uint64) []proto.Message,
+	assign func([]proto.Message, proto.Message) error,
+	keyBuilder func(uint64) string,
+	ttl time.Duration,
+) error {
+	key := keyBuilder(playerId)
+
+	// 1. 尝试从 Redis 加载
+	found, err := LoadProtoFromRedis(ctx, redisClient, key, result)
+	if err != nil {
+		logx.Errorf("Redis get failed: %v", err)
+		return err
+	}
+	if found {
+		return nil
+	}
+
+	// 2. Redis 未命中，从 DB 加载子结构
+	playerIdStr := strconv.FormatUint(playerId, 10)
+	hash := fnv.New64a()
+	_, _ = hash.Write([]byte(playerIdStr))
+	hashKey := hash.Sum64()
+
+	// 构建子结构
+	subMsgs := build(playerId)
+	var chs []*queue.MsgChannel
+
+	for _, msg := range subMsgs {
+		ch := &queue.MsgChannel{
+			Key:       hashKey,
+			Body:      msg,
+			Chan:      make(chan bool),
+			WhereCase: "where player_id='" + playerIdStr + "'",
+		}
+		db.DB.MsgQueue.Put(*ch)
+		chs = append(chs, ch)
+	}
+
+	// 等待所有异步加载完成
+	for _, ch := range chs {
+		<-ch.Chan
+	}
+
+	// 聚合数据
+	err = assign(subMsgs, result)
+	if err != nil {
+		logx.Errorf("assign aggregate data failed: %v", err)
+		return err
+	}
+
+	// 存回 Redis
+	err = SaveProtoToRedis(ctx, redisClient, key, result, ttl)
+	if err != nil {
+		logx.Errorf("Save to Redis failed: %v", err)
+		return err
 	}
 
 	return nil
--- node/db/internal/logic/playerdbservice/load2redislogic.go
+++ node/db/internal/logic/playerdbservice/load2redislogic.go
@@ -5,6 +5,9 @@ import (
 	"db/internal/logic/pkg/utils"
 	"db/internal/svc"
 	"db/pb/game"
+	"fmt"
+	"strconv"
+	"time"
 
 	"github.com/golang/protobuf/proto"
 	"github.com/zeromicro/go-zero/core/logx"
@@ -28,20 +31,55 @@ func (l *Load2RedisLogic) Load2Redis(in *game.LoadPlayerRequest) (*game.LoadPlay
 	resp := &game.LoadPlayerResponse{PlayerId: in.PlayerId}
 
 	msgCentre := &game.PlayerCentreDatabase{PlayerId: in.PlayerId}
-	msgPlayer := &game.PlayerDatabase{PlayerId: in.PlayerId}
-	msgPlayer1 := &game.PlayerDatabase_1{PlayerId: in.PlayerId}
 
 	err := utils.BatchLoadAndCache(
 		l.ctx,
 		l.svcCtx.Redis,
 		in.PlayerId,
 		[]proto.Message{
-			msgPlayer,
 			msgCentre,
-			msgPlayer1,
 		},
 	)
 	if err != nil {
+		logx.Errorf("BatchLoadAndCache error: %v", err)
+		return nil, err
+	}
+
+	playerAll := &game.PlayerAllData{}
+
+	err = utils.LoadAggregateData(
+		l.ctx,
+		l.svcCtx.Redis,
+		in.PlayerId,
+		playerAll,
+		func(id uint64) []proto.Message {
+			return []proto.Message{
+				&game.PlayerDatabase{PlayerId: id},
+				&game.PlayerDatabase_1{PlayerId: id},
+			}
+		},
+		func(messages []proto.Message, target proto.Message) error {
+			allData := target.(*game.PlayerAllData)
+			for _, m := range messages {
+				switch msg := m.(type) {
+				case *game.PlayerDatabase:
+					allData.PlayerDatabaseData = msg
+				case *game.PlayerDatabase_1:
+					allData.PlayerDatabase_1Data = msg
+				default:
+					return fmt.Errorf("unexpected type %T", msg)
+				}
+			}
+			return nil
+		},
+		func(id uint64) string {
+			return "PlayerAllData:" + strconv.FormatUint(id, 10)
+		},
+		time.Hour,
+	)
+
+	if err != nil {
+		logx.Errorf("Load2Redis error: %v", err)
 		return nil, err
 	}
 
