6c5f80e4b724de33c4509abdf12b7a25ce273005
luhailong <lhpcpp6@163.com>
2025-08-20 16:52:29 +0800

    add gate node

---
+++ node_pkg/node_logic/gate_node/src/id/manager/session_manager.h
@@ -0,0 +1,21 @@
+#pragma once
+
+#include "comp/gate_session.h"
+#include "util/consistent_hash_node.h"
+#include "network/network_utils.h"
+
+using SessionList = std::unordered_map<Guid, Session>;
+
+class ThreadLocalStorageGate
+{
+public:
+	ThreadLocalStorageGate();
+	SessionList& sessions() { return session_list_; }
+	SessionIdGenerator& session_id_gen() { return session_id_gen_; }
+private:
+	SessionList session_list_;
+	SessionIdGenerator session_id_gen_;
+};
+
+extern thread_local ThreadLocalStorageGate tls_gate;
+
