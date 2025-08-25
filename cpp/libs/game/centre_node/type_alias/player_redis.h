e3df91791a9f6eebbe4fcf07d3395e068e8970b2
luhailong <lhpcpp6@163.com>
2025-08-25 15:09:03 +0800

    clear code

---
+++ cpp/libs/game/centre_node/type_alias/player_redis.h
@@ -0,0 +1,14 @@
+#pragma once
+
+#include "redis_client/redis_client.h"
+
+#include "proto/db/mysql_database_table.pb.h"
+#include "util/game_registry.h"
+
+#include <thread_local/registry_manager.h>
+using PlayerCentreDataRedis = std::unique_ptr<MessageAsyncClient<Guid, player_centre_database>>;
+
+inline PlayerCentreDataRedis& GetPlayerCentreDataRedis() {
+	return tlsRegistryManager.globalRegistry.get<PlayerCentreDataRedis>(GlobalEntity());
+}
+
