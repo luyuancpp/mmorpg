eca5c42918f26e4e97340c5025fb9c6519d0615d
luhailong <lhpcpp6@163.com>
2025-08-26 16:26:40 +0800

    clear code

---
+++ cpp/nodes/gate/gate_node.h
@@ -0,0 +1,52 @@
+#pragma once
+
+#include <stdio.h>
+
+#include "muduo/base/AsyncLogging.h"
+#include "muduo/base/Logging.h"
+#include "muduo/net/EventLoop.h"
+#include "muduo/net/TcpServer.h"
+
+#include "handler/rpc/client_message_processor.h"
+#include "handler/rpc/gate_service_handler.h"
+#include "network/rpc_connection_event.h"
+#include "network/codec/codec.h"
+#include "network/codec/dispatcher.h"
+#include "time/comp/timer_task_comp.h"
+#include "node/system/node.h"
+
+class GateNode : public  Node
+{
+public:
+    using TcpServerPtr = std::unique_ptr<TcpServer>;
+
+    explicit GateNode(EventLoop* loop);
+    ~GateNode() override {}
+    inline ProtobufCodec& Codec() { return codec_; }
+	::google::protobuf::Service* GetNodeReplyService() override{ return &nodeReplyService ; }
+
+    inline void SendMessageToClient(const muduo::net::TcpConnectionPtr& conn,
+                            const ::google::protobuf::Message& message) const { rpcClientHandler.SendMessageToClient(conn, message); }
+    void StartRpcServer()override;
+private:
+    void OnConnection(const TcpConnectionPtr& conn)
+    {
+        rpcClientHandler.OnConnection(conn);
+    }
+
+    void OnUnknownMessage(const TcpConnectionPtr& conn,
+                                 const MessagePtr& message,
+                                 Timestamp)
+    {
+        LOG_ERROR << "onUnknownMessage: " << message->GetTypeName().data();
+        conn->shutdown();
+    }
+
+private:
+    ProtobufDispatcher dispatcher_;
+    ProtobufCodec codec_;
+    RpcClientSessionHandler rpcClientHandler;
+    GateHandler nodeReplyService;
+};
+
+extern GateNode* gGateNode;
---
+++ cpp/nodes/scene/scene_node.h
@@ -0,0 +1,29 @@
+#pragma once
+
+#include "muduo/net/EventLoop.h"
+#include "handler/rpc/game_service_handler.h"
+#include "network/rpc_client.h"
+#include "network/rpc_connection_event.h"
+#include "node/system/node.h"
+#include "redis_client/redis_client.h"
+
+class OnConnect2CentrePbEvent;
+
+class SceneNode : public Node
+{
+public:
+    explicit
+        SceneNode(muduo::net::EventLoop* loop);
+	~SceneNode() override = default;
+    
+    ::google::protobuf::Service* GetNodeReplyService() override { return &nodeReplyService; }
+
+    void StartRpcServer() override;
+    
+private:
+    SceneHandler nodeReplyService;
+    TimerTaskComp worldTimer;
+};
+
+
+
