06e34ad77f2f8ffefb9ce04e29b9a95865445fd5
luhailong <lhpcpp6@163.com>
2025-08-27 19:41:36 +0800

    clear code

--- cpp/libs/base/core/node/system/node/node.cpp
+++ cpp/libs/base/core/node/system/node/node.cpp
@@ -27,7 +27,7 @@
 #include "threading/redis_manager.h"
 #include "time/system/time_system.h"
 #include "network/network_utils.h"
-#include "util/gen_util.h"
+#include "proto_util/proto_util.h"
 #include "util/stacktrace_system.h"
 #include "network/node_utils.h"
 #include <boost/algorithm/string.hpp>
