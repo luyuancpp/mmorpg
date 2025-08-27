d381b23bf486d87374f4eabc8ef0edaa901f2a5e
luhailong <lhpcpp6@163.com>
2025-08-27 18:52:28 +0800

    clear code

--- cpp/nodes/scene/handler/event/scene_event_handler.cpp
+++ cpp/nodes/scene/handler/event/scene_event_handler.cpp
@@ -4,9 +4,9 @@
 
 ///<<< BEGIN WRITING YOUR CODE
 #include "muduo/base/Logging.h"
-#include "scene/system/aoi_system.h"
-#include "scene/system/game_node_scene_system.h"
-#include "scene/system/scene_crowd_system.h"
+#include "scene/scene/system/aoi_system.h"
+#include "scene/scene/system/game_node_scene_system.h"
+#include "scene/scene/system/scene_crowd_system.h"
 ///<<< END WRITING YOUR CODE
 
 
