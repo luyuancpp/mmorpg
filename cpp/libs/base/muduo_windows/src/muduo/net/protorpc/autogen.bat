be302da8a4c79ef79029baca2c527145da59e9cd
luhailong <lhpcpp6@163.com>
2025-09-01 20:42:35 +0800

    clear code

--- cpp/libs/base/muduo_windows/src/muduo/net/protorpc/autogen.bat
+++
@@ -1,3 +0,0 @@
-for /f "delims=\" %%a in ('dir /b /a-d /o-d "*.proto"') do (
-	protoc  -I=./ --cpp_out=. %%a)
-pause
---
+++ cpp/libs/engine/modules/reward/comp/reward_comp.h
@@ -0,0 +1,12 @@
+﻿#pragma once
+
+#include <bitset>
+
+#include "table/code/bit_index/reward_table_id_bit_index.h"
+
+// 使用 bitset 来表示奖励的领取状态
+using RewardBitset = std::bitset<kRewardMaxBitIndex>;
+
+struct RewardComp {
+    RewardBitset rewards;
+};
