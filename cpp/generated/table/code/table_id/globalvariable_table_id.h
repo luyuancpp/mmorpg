cdd4832a2f5fa3de6410952faf633315e2091947
luhailong <lhpcpp6@163.com>
2025-09-12 14:27:50 +0800

    clear code

---
+++ cpp/generated/table/code/table_id/globalvariable_table_id.h
@@ -0,0 +1,20 @@
+#pragma once
+enum e_GlobalVariable_table_id : uint32_t
+{
+    GlobalVariable_table_id1,
+    GlobalVariable_table_id2,
+    GlobalVariable_table_id3,
+    GlobalVariable_table_id4,
+    GlobalVariable_table_id5,
+    GlobalVariable_table_id6,
+    GlobalVariable_table_id7,
+    GlobalVariable_table_id8,
+    GlobalVariable_table_id9,
+    GlobalVariable_table_id10,
+    GlobalVariable_table_id11,
+    GlobalVariable_table_id12,
+    GlobalVariable_table_id13,
+    GlobalVariable_table_id14,
+    GlobalVariable_table_id15,
+    GlobalVariable_table_id16,
+};
