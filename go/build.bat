79a3c69ab65af41e315527b1eefaf5b2aad1235b
luhailong <lhpcpp6@163.com>
2025-09-15 09:49:59 +0800

    clear code

--- go/build.bat
+++ go/build.bat
@@ -1,5 +1,5 @@
 goctl rpc protoc ../proto/instance/instance.proto --proto_path=../ --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./instance --go-grpc_out=./instance --zrpc_out=./instance -m
-goctl rpc protoc ../proto/db/db.proto --proto_path=../ --proto_path=../third_party/protobuf/src/ --go_out=./ --go-grpc_out=./ --zrpc_out=./db -m
+goctl rpc protoc ../proto/db/db.proto  --go_out=./ --go-grpc_out=./ --zrpc_out=./ -m
 goctl rpc protoc ../proto/login/login.proto --proto_path=../ --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
 goctl rpc protoc ../proto/player_locator/player_locator.proto --proto_path=../ --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./player_locator --go-grpc_out=./player_locator --zrpc_out=./player_locator -m
 pause
---
+++ go/internal/logic/db/testlogic.go
@@ -0,0 +1,30 @@
+package dblogic
+
+import (
+	"context"
+
+	"game/generated/pb/game"
+	"game/internal/svc"
+
+	"github.com/zeromicro/go-zero/core/logx"
+)
+
+type TestLogic struct {
+	ctx    context.Context
+	svcCtx *svc.ServiceContext
+	logx.Logger
+}
+
+func NewTestLogic(ctx context.Context, svcCtx *svc.ServiceContext) *TestLogic {
+	return &TestLogic{
+		ctx:    ctx,
+		svcCtx: svcCtx,
+		Logger: logx.WithContext(ctx),
+	}
+}
+
+func (l *TestLogic) Test(in *game.Empty) (*game.Empty, error) {
+	// todo: add your logic here and delete this line
+
+	return &game.Empty{}, nil
+}
