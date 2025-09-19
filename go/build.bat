51f6ddc79b0d34bdd5e3885e4f6fa61c042d83be
luhailong <lhpcpp6@163.com>
2025-09-19 14:32:28 +0800

    clear code

--- go/build.bat
+++ go/build.bat
@@ -1,3 +1,3 @@
 goctl rpc protoc ../generated/generated_proto/db/proto/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db/ --go_out=./db --go-grpc_out=./db --zrpc_out=./db -m
-goctl rpc protoc ../generated/generated_proto/login/proto/service/go/grpc/login/db.proto --proto_path=../generated/generated_proto/login/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
+goctl rpc protoc ../generated/generated_proto/login/proto/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
 pause
