b9928a7f87645f66720b6b68c1b032812a9b59a3
luhailong <lhpcpp6@163.com>
2025-09-19 14:28:08 +0800

    clear code

--- go/player_locator/internal/keys/redis_keys.go
+++
@@ -1,9 +0,0 @@
-package keys
-
-import "fmt"
-
-const PlayerLocationKeyPrefix = "player_location"
-
-func PlayerLocationKey(uid int64) string {
-	return fmt.Sprintf("%s:%d", PlayerLocationKeyPrefix, uid)
-}
