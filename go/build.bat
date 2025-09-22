4e53900473f116a4ea1025b38a52436de298c5fc
luhailong <lhpcpp6@163.com>
2025-09-22 11:51:07 +0800

    add go zero sql

--- go/build.bat
+++ go/build.bat
@@ -1,4 +1,4 @@
-goctl rpc protoc ../generated/generated_proto/db/go-zero_proto/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db --go_out=./db/dbproto --go-grpc_out=./db/dbproto --zrpc_out=./db -m
+goctl rpc protoc ../generated/generated_proto/db/go-zero_proto/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db --go_out=./dbproto --go-grpc_out=./dbproto --zrpc_out=./db -m
 rmdir /s /q %cd%\db\dbproto
 goctl rpc protoc ../generated/generated_proto/login/proto/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
 pause
