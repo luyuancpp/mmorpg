5a4c02a3cc0a446a58d7736cce0fb0f8309dcef5
luhailong <lhpcpp6@163.com>
2025-09-22 17:14:18 +0800

    add code gen

--- cpp/libs/engine/core/network/autogen.bat
+++
@@ -1,5 +0,0 @@
-for /f "delims=\" %%a in ('dir /b /a-d /o-d "*.proto"') do (
-	protoc.exe   --cpp_out=. %%a)
-for /f "delims=\" %%a in ('dir /b /a-d /o-d "*.proto"') do (
-	protoc.exe --go_out=../../../../../go/login/ %%a)
-pause
--- cpp/libs/engine/core/network/autogen.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-
-for file in *.proto; do
-	protoc  -I=./ -I=../../../../third_party/protobuf/src --cpp_out=./ $file
-done
