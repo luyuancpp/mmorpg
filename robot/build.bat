825ee79570a2bce7ce636ee3a049b99110ccf893
luhailong <lhpcpp6@163.com>
2025-09-25 11:49:56 +0800

    clear code

--- robot/build.bat
+++
@@ -1,5 +0,0 @@
-protoc  ../../proto/common_proto/c2gate.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
-protoc  ../../proto/common_proto/tip.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
-protoc  ../../proto/common_proto/account_database.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
-
-pause
---
+++ tools/robot/build.bat
@@ -0,0 +1,5 @@
+protoc  ../../proto/common_proto/c2gate.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
+protoc  ../../proto/common_proto/tip.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
+protoc  ../../proto/common_proto/account_database.proto --go_out=./  --proto_path=../../proto/ --proto_path=../../third_party/protobuf/src/
+
+pause
