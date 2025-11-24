39d2cf0a30b32ea49e9becd5a8101132e6466767
luhailong <lhpcpp6@163.com>
2025-11-24 14:33:49 +0800

    clear code

--- tools/proto_generator/pbgen/internal/generator_centre_node_util.go
+++ tools/proto_generator/pbgen/internal/generator_centre_node_util.go
@@ -4,7 +4,8 @@ import (
 	messageoption "github.com/luyuancpp/protooption"
 )
 
-// IsCentreHostedServiceHandler 判断是否是Centre服务的普通方法处理器
+// IsCentreHostedServiceHandler 判断是否是Centre节点对外提供的普通服务处理器
+// （Centre作为服务端，处理外部调用）
 func IsCentreHostedServiceHandler(methodList *RPCMethods) bool {
 	firstMethodInfo := (*methodList)[0]
 
@@ -19,7 +20,8 @@ func IsCentreHostedServiceHandler(methodList *RPCMethods) bool {
 	return true
 }
 
-// IsCentreHostedPlayerServiceHandler 判断是否是Centre的Player服务方法处理器
+// IsCentreHostedPlayerServiceHandler 判断是否是Centre节点对外提供的玩家服务处理器
+// （Centre作为服务端，处理外部调用的玩家相关接口）
 func IsCentreHostedPlayerServiceHandler(methodList *RPCMethods) bool {
 	firstMethodInfo := (*methodList)[0]
 
@@ -30,7 +32,8 @@ func IsCentreHostedPlayerServiceHandler(methodList *RPCMethods) bool {
 	return isPlayerService(firstMethodInfo.ServiceDescriptorProto)
 }
 
-// IsCentreReceivedServiceResponseHandler 判断是否是外部返回给Centre服务的响应处理器
+// IsCentreReceivedServiceResponseHandler 判断是否是Centre节点接收的普通服务响应处理器
+// （Centre作为客户端，处理外部服务返回的响应）
 func IsCentreReceivedServiceResponseHandler(methodList *RPCMethods) bool {
 	firstMethodInfo := (*methodList)[0]
 
@@ -53,7 +56,8 @@ func IsCentreReceivedServiceResponseHandler(methodList *RPCMethods) bool {
 	return true
 }
 
-// IsCentreReceivedPlayerServiceResponseHandler 判断是否是外部返回给Centre Player服务的响应处理器
+// IsCentreReceivedPlayerServiceResponseHandler 判断是否是Centre节点接收的玩家服务响应处理器
+// （Centre作为客户端，处理外部服务返回的玩家相关响应）
 func IsCentreReceivedPlayerServiceResponseHandler(methodList *RPCMethods) bool {
 	firstMethodInfo := (*methodList)[0]
 
