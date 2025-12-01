bce447fffe877f8177ec8d89b72ad1073ad7444e
luhailong <lhpcpp6@163.com>
2025-12-01 20:28:13 +0800

    clear code

--- go/build.bat
+++ go/build.bat
@@ -1,5 +1,5 @@
-goctl rpc protoc ../generated/generated_proto/db/go-zero_proto/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db --go_out=./db --go-grpc_out=./db --zrpc_out=./db -m
+goctl rpc protoc ../generated/generated_proto/db/go-zero_proto/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db  --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./db --go-grpc_out=./db --zrpc_out=./db -m
 rmdir /s /q %cd%\db\service
-goctl rpc protoc ../generated/generated_proto/login/go-zero_proto/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
+goctl rpc protoc ../generated/generated_proto/login/go-zero_proto/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
 rmdir /s /q %cd%\login\service
 pause
