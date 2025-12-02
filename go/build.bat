99d2144072e716bf97054ffea427ddc0baa61387
luhailong <lhpcpp6@163.com>
2025-12-02 17:18:33 +0800

    clear code

--- go/build.bat
+++ go/build.bat
@@ -1,5 +1,5 @@
-goctl rpc protoc ../generated/generated_proto/db/go-zero/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./db --go-grpc_out=./db --zrpc_out=./db -m
+goctl rpc protoc ../generated/generated_proto/db/go-zero/service/go/grpc/db/db.proto --proto_path=../generated/generated_proto/db --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./db/proto/ --go-grpc_out=./db/proto/ --zrpc_out=./db -m
 rmdir /s /q %cd%\db\service
-goctl rpc protoc ../generated/generated_proto/login/go-zero/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login  --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./login --go-grpc_out=./login --zrpc_out=./login -m
+goctl rpc protoc ../generated/generated_proto/login/go-zero/service/go/grpc/login/login.proto --proto_path=../generated/generated_proto/login  --proto_path=../third_party/grpc/third_party/protobuf/src/ --go_out=./login/proto/ --go-grpc_out=./login/proto/ --zrpc_out=./login -m
 rmdir /s /q %cd%\login\service
 pause
