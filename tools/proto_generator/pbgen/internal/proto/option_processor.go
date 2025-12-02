ee5b7a791c952e237663f979e92b7563c1d513e4
luhailong <lhpcpp6@163.com>
2025-12-02 20:30:54 +0800

    clear code

--- tools/proto_generator/pbgen/internal/proto/option_processor.go
+++ tools/proto_generator/pbgen/internal/proto/option_processor.go
@@ -1,201 +1,366 @@
 package proto
 
 import (
-	"encoding/json"
-	"github.com/luyuancpp/protooption"
-	"google.golang.org/protobuf/proto"
-	"os"
-	"path/filepath"
-	_config "pbgen/internal/config"
+	"errors"
+	"log"
 	"strings"
 	"sync"
 
+	messageoption "github.com/luyuancpp/protooption"
+	"google.golang.org/protobuf/proto"
+	"google.golang.org/protobuf/runtime/protoimpl"
 	"google.golang.org/protobuf/types/descriptorpb"
-	"pbgen/internal/utils"
 )
 
-// MessageListConfig 用于生成消息列表JSON的结构
-type MessageListConfig struct {
-	Messages []string `json:"messages"`
+//
+// ---------------- Option Type Definitions ----------------
+//
+
+type OptionType int
+
+const (
+	OptionTypeFile OptionType = iota
+	OptionTypeService
+	OptionTypeMethod
+	OptionTypeMessage
+	OptionTypeField
+)
+
+type OptionHandler func(option interface{}) error
+
+type OptionConfig struct {
+	Type     OptionType
+	Handlers []OptionHandler
 }
 
-// Processor 协议处理器，仅依赖传入的FileDescriptorSet
-type Processor struct {
-	fdSet        *descriptorpb.FileDescriptorSet // 仅使用传入的描述符集
-	maxGoroutine int
-	workerChan   chan struct{}
-}
+//
+// ---------------- Entry: Process All Options ----------------
+//
 
-// NewProcessor 创建处理器实例，接收外部传入的FileDescriptorSet
-func NewProcessor(fdSet *descriptorpb.FileDescriptorSet, maxGoroutine int) *Processor {
-	if maxGoroutine <= 0 {
-		maxGoroutine = 10 // 默认并发数
+func ProcessAllOptions(wg *sync.WaitGroup, protoFiles []*descriptorpb.FileDescriptorProto) {
+	configs := []OptionConfig{
+		{
+			Type: OptionTypeFile,
+			Handlers: []OptionHandler{
+				ProcessFileDefaultNode,
+				ProcessFileCustomOptions,
+			},
+		},
+		{
+			Type: OptionTypeService,
+			Handlers: []OptionHandler{
+				ProcessServicePlayerOption,
+				ProcessServiceClientOption,
+				ProcessServiceCustomOptions,
+			},
+		},
+		{
+			Type: OptionTypeMethod,
+			Handlers: []OptionHandler{
+				ProcessMethodOperateType,
+				ProcessMethodCustomOptions,
+			},
+		},
+		{
+			Type: OptionTypeMessage,
+			Handlers: []OptionHandler{
+				ProcessMessageTableOption,
+				ProcessMessageKeyOption,
+				ProcessMessageAttributeSync,
+				ProcessMessageCustomOptions,
+			},
+		},
+		{
+			Type: OptionTypeField,
+			Handlers: []OptionHandler{
+				ProcessFieldDBOptions,
+				ProcessFieldCustomOptions,
+			},
+		},
 	}
-	return &Processor{
-		fdSet:        fdSet,
-		maxGoroutine: maxGoroutine,
-		workerChan:   make(chan struct{}, maxGoroutine),
+
+	for _, file := range protoFiles {
+		file := file
+		wg.Add(1)
+		go func() {
+			defer wg.Done()
+			processFileOptions(wg, file, configs)
+		}()
 	}
 }
 
-// Process 处理传入的FileDescriptorSet，不依赖内部全局状态
-func (p *Processor) Process() error {
-	var wg sync.WaitGroup
-	errCh := make(chan error, len(p.fdSet.File))
+//
+// ---------------- File Processing ----------------
+//
 
-	// 收集所有消息名（仅基于当前fdSet）
-	var allMessages []string
-	msgMu := sync.Mutex{}
+func processFileOptions(wg *sync.WaitGroup, file *descriptorpb.FileDescriptorProto, configs []OptionConfig) {
+	processOptionType(wg, OptionTypeFile, file.Options, configs)
 
-	// 遍历传入的描述符集中的所有文件
-	for _, fileDesc := range p.fdSet.File {
-		filename := utils.GetBaseName(fileDesc.GetName())
-		if _config.Global.ShouldIgnoreFile(filename) {
-			continue
+	for _, svc := range file.Service {
+		svc := svc
+		processOptionType(wg, OptionTypeService, svc.Options, configs)
+
+		for _, method := range svc.Method {
+			method := method
+			processOptionType(wg, OptionTypeMethod, method.Options, configs)
 		}
+	}
+
+	for _, msg := range file.MessageType {
+		msg := msg
+		processOptionType(wg, OptionTypeMessage, msg.Options, configs)
+		processMessageFields(wg, msg, configs)
+		processNestedMessages(wg, msg, configs)
+	}
+}
+
+func processMessageFields(wg *sync.WaitGroup, msg *descriptorpb.DescriptorProto, configs []OptionConfig) {
+	for _, field := range msg.Field {
+		field := field
+		processOptionType(wg, OptionTypeField, field.Options, configs)
+	}
+}
+
+func processNestedMessages(wg *sync.WaitGroup, msg *descriptorpb.DescriptorProto, configs []OptionConfig) {
+	for _, n := range msg.NestedType {
+		n := n
+		processOptionType(wg, OptionTypeMessage, n.Options, configs)
+		processMessageFields(wg, n, configs)
+		processNestedMessages(wg, n, configs)
+	}
+}
+
+//
+// ---------------- Option Dispatch ----------------
+//
+
+func processOptionType(wg *sync.WaitGroup, optType OptionType, option interface{}, configs []OptionConfig) {
+	var target OptionConfig
+	for _, cfg := range configs {
+		if cfg.Type == optType {
+			target = cfg
+			break
+		}
+	}
+
+	for _, handler := range target.Handlers {
+		handler := handler
+		option := option
 
 		wg.Add(1)
-		p.workerChan <- struct{}{}
-
-		go func(fd *descriptorpb.FileDescriptorProto) {
+		go func() {
 			defer wg.Done()
-			defer func() { <-p.workerChan }()
-
-			// 提取文件信息（仅使用当前fd的数据）
-			pkgName := fd.GetPackage()
-			fileName := utils.GetBaseName(fd.GetName())
-			messages := getMessageNames(fd)
-			services := getServiceNames(fd)
-			options := parseFileOptions(fd)
-
-			// 收集消息名
-			msgMu.Lock()
-			allMessages = append(allMessages, messages...)
-			msgMu.Unlock()
-
-			// 生成DB模型
-			if err := p.generateDBModel(pkgName, fileName, messages, options); err != nil {
-				errCh <- err
-				return
+			if err := handler(option); err != nil {
+				log.Fatalf("Option handler failed: %v", err)
 			}
-
-			// 生成服务代码
-			if len(services) > 0 {
-				if err := p.generateServiceCode(pkgName, fileName, services, options); err != nil {
-					errCh <- err
-				}
-			}
-		}(fileDesc)
+		}()
 	}
-
-	// 等待所有任务完成
-	go func() {
-		wg.Wait()
-		close(errCh)
-	}()
-
-	// 处理错误
-	for err := range errCh {
-		if err != nil {
-			return err
-		}
-	}
-
-	// 生成全局消息列表
-	return p.writeMessageNamesToJSON(allMessages)
 }
 
-// 提取消息名称（仅基于当前FileDescriptorProto）
-func getMessageNames(fd *descriptorpb.FileDescriptorProto) []string {
-	var names []string
-	for _, msg := range fd.MessageType {
-		names = append(names, msg.GetName())
+//
+// ---------------- Option Handlers ----------------
+//
+
+// FileOptions: 默认节点
+func ProcessFileDefaultNode(option interface{}) error {
+	opts, ok := option.(*descriptorpb.FileOptions)
+	if !ok {
+		return errors.New("invalid FileOptions type")
 	}
-	return names
+
+	val := getExt(opts, messageoption.E_OptionFileDefaultNode)
+	node := val.(messageoption.NodeType)
+	log.Printf("File default node: %s", messageoption.NodeType_name[int32(node)])
+	return nil
 }
 
-// 提取服务名称（仅基于当前FileDescriptorProto）
-func getServiceNames(fd *descriptorpb.FileDescriptorProto) []string {
-	var names []string
-	for _, svc := range fd.Service {
-		names = append(names, svc.GetName())
+// Service: is player service
+func ProcessServicePlayerOption(option interface{}) error {
+	opts, ok := option.(*descriptorpb.ServiceOptions)
+	if !ok {
+		return errors.New("invalid ServiceOptions type")
 	}
-	return names
+
+	val := getExt(opts, messageoption.E_OptionIsPlayerService)
+	log.Printf("Service.player = %v", val.(bool))
+	return nil
 }
 
-// 解析文件选项（仅基于当前FileDescriptorProto）
-func parseFileOptions(fd *descriptorpb.FileDescriptorProto) map[string]interface{} {
-	options := make(map[string]interface{})
-	if fd.Options != nil {
-		// 解析自定义选项（示例）
-		if val, ok := proto.GetExtension(fd.GetOptions(), messageoption.E_OptionFileDefaultNode).(string); ok {
-			options["file_default_node"] = val
-		}
+// Service: client protocol
+func ProcessServiceClientOption(option interface{}) error {
+	opts, ok := option.(*descriptorpb.ServiceOptions)
+	if !ok {
+		return errors.New("invalid ServiceOptions type")
 	}
-	return options
+
+	val := getExt(opts, messageoption.E_OptionIsClientProtocolService)
+	log.Printf("Service.client = %v", val.(bool))
+	return nil
 }
 
-// generateDBModel 生成DB模型代码
-func (p *Processor) generateDBModel(pkgName, fileName string, messages []string, options map[string]interface{}) error {
-	outputDir := filepath.Join(_config.Global.Paths.OutputRoot, "db", pkgName)
-	if err := os.MkdirAll(outputDir, 0755); err != nil {
-		return err
+// Method: operate type
+func ProcessMethodOperateType(option interface{}) error {
+	opts, ok := option.(*descriptorpb.MethodOptions)
+	if !ok {
+		return errors.New("invalid MethodOptions type")
 	}
 
-	var code strings.Builder
-	code.WriteString("package " + pkgName + "\n\n")
-	code.WriteString("// 自动生成的DB模型代码，请勿手动修改\n")
-	for _, msg := range messages {
-		code.WriteString(generateModelStruct(msg))
-	}
-
-	outputPath := filepath.Join(outputDir, fileName+"_db.gen.go")
-	return os.WriteFile(outputPath, []byte(code.String()), 0644)
+	val := getExt(opts, messageoption.E_OptionOperateType)
+	t := val.(messageoption.OPERATE_TYPE)
+	log.Printf("Method operate: %s", messageoption.OPERATE_TYPE_name[int32(t)])
+	return nil
 }
 
-// generateServiceCode 生成服务代码
-func (p *Processor) generateServiceCode(pkgName, fileName string, services []string, options map[string]interface{}) error {
-	outputDir := filepath.Join(_config.Global.Paths.OutputRoot, "service", pkgName)
-	if err := os.MkdirAll(outputDir, 0755); err != nil {
-		return err
+// Message: table options
+func ProcessMessageTableOption(option interface{}) error {
+	opts, ok := option.(*descriptorpb.MessageOptions)
+	if !ok {
+		return errors.New("invalid MessageOptions type")
 	}
 
-	var code strings.Builder
-	code.WriteString("package " + pkgName + "\n\n")
-	code.WriteString("// 自动生成的服务代码，请勿手动修改\n")
-	for _, svc := range services {
-		code.WriteString(generateServiceInterface(svc))
+	if name, _ := getString(opts, messageoption.E_OptionTableName); name != "" {
+		log.Printf("Table name: %s", name)
+	}
+	if id, _ := getUint(opts, messageoption.E_OptionTableId); id > 0 {
+		log.Printf("Table id  : %d", id)
+	}
+	if backup, _ := getString(opts, messageoption.E_OptionBackupTableName); backup != "" {
+		log.Printf("Backup table: %s", backup)
 	}
 
-	outputPath := filepath.Join(outputDir, fileName+"_service.gen.go")
-	return os.WriteFile(outputPath, []byte(code.String()), 0644)
+	return nil
 }
 
-// writeMessageNamesToJSON 生成消息列表JSON
-func (p *Processor) writeMessageNamesToJSON(messages []string) error {
-	data, err := json.MarshalIndent(&MessageListConfig{Messages: messages}, "", "  ")
-	if err != nil {
-		return err
+// Message: keys
+func ProcessMessageKeyOption(option interface{}) error {
+	opts, ok := option.(*descriptorpb.MessageOptions)
+	if !ok {
+		return errors.New("invalid MessageOptions type")
 	}
 
-	outputPath := filepath.Join(
-		_config.Global.Paths.TableGeneratorDir,
-		_config.Global.Naming.DbTableListJson,
-	)
-	return os.WriteFile(outputPath, data, 0644)
+	if pk, _ := getString(opts, messageoption.E_OptionPrimaryKey); pk != "" {
+		log.Printf("Primary key: %s", pk)
+	}
+	if fk, _ := getString(opts, messageoption.E_OptionForeignKey); fk != "" {
+		ref, _ := getString(opts, messageoption.E_OptionForeignReferences)
+		log.Printf("Foreign key: %s => %s", fk, ref)
+	}
+	if auto, _ := getString(opts, messageoption.E_OptionAutoIncrementKey); auto != "" {
+		log.Printf("Auto key: %s", auto)
+	}
+
+	return nil
 }
 
-// 生成模型结构体
-func generateModelStruct(msgName string) string {
-	return "type " + msgName + "Model struct {\n" +
-		"    // 自动生成的模型字段\n" +
-		"    Id int64 `db:\"id\"`\n" +
-		"}\n\n"
+// Message: attribute sync
+func ProcessMessageAttributeSync(option interface{}) error {
+	opts, ok := option.(*descriptorpb.MessageOptions)
+	if !ok {
+		return errors.New("invalid MessageOptions type")
+	}
+
+	if base, _ := getBool(opts, messageoption.E_OptionBaseAttributeSync); base {
+		log.Printf("Attr sync: base")
+	}
+	if delta, _ := getBool(opts, messageoption.E_OptionDeltaAttributeSync); delta {
+		log.Printf("Attr sync: delta")
+	}
+
+	return nil
 }
 
-// 生成服务接口
-func generateServiceInterface(svcName string) string {
-	return "type " + svcName + "Server interface {\n" +
-		"    // 自动生成的服务方法\n" +
-		"}\n\n"
+// Field: DB options
+func ProcessFieldDBOptions(option interface{}) error {
+	opts, ok := option.(*descriptorpb.FieldOptions)
+	if !ok {
+		return errors.New("invalid FieldOptions type")
+	}
+
+	if r, _ := getBool(opts, messageoption.E_OptionDBReadOnly); r {
+		log.Printf("Field readonly")
+	}
+	if p, _ := getString(opts, messageoption.E_OptionDBNamePrefix); p != "" {
+		log.Printf("Field prefix: %s", p)
+	}
+	if lim, _ := getUint(opts, messageoption.E_OptionRepeatLimit); lim > 0 {
+		log.Printf("Field repeat limit: %d", lim)
+	}
+
+	return nil
+}
+
+//
+// ---------------- Custom Options (empty) ----------------
+//
+
+func ProcessFileCustomOptions(option interface{}) error    { return nil }
+func ProcessServiceCustomOptions(option interface{}) error { return nil }
+func ProcessMethodCustomOptions(option interface{}) error  { return nil }
+
+func ProcessMessageCustomOptions(option interface{}) error {
+	opts, ok := option.(*descriptorpb.MessageOptions)
+	if !ok {
+		return errors.New("invalid MessageOptions type")
+	}
+
+	if p, _ := getString(opts, messageoption.E_OptionProcedureName); p != "" {
+		res, _ := getString(opts, messageoption.E_OptionProcedureResult)
+		log.Printf("Procedure: %s => %s", p, res)
+	}
+	return nil
+}
+
+func ProcessFieldCustomOptions(option interface{}) error {
+	opts, ok := option.(*descriptorpb.FieldOptions)
+	if !ok {
+		return errors.New("invalid FieldOptions type")
+	}
+
+	if old, _ := getString(opts, messageoption.E_OptionOldName); old != "" {
+		log.Printf("Old name: %s", old)
+	}
+	return nil
+}
+
+//
+// ---------------- Utils ----------------
+//
+
+// 判断是否 “extension 不存在”
+func isMissingExt(err error) bool {
+	if err == nil {
+		return false
+	}
+	return strings.Contains(err.Error(), "no such extension")
+}
+
+func getExt(opts proto.Message, ext *protoimpl.ExtensionInfo) any {
+	return proto.GetExtension(opts, ext)
+}
+
+func getString(opts proto.Message, ext *protoimpl.ExtensionInfo) (string, error) {
+	val := getExt(opts, ext)
+	s, ok := val.(string)
+	if !ok {
+		return "", errors.New("not string")
+	}
+	return s, nil
+}
+
+func getUint(opts proto.Message, ext *protoimpl.ExtensionInfo) (uint32, error) {
+	val := getExt(opts, ext)
+	u, ok := val.(uint32)
+	if !ok {
+		return 0, errors.New("not uint32")
+	}
+	return u, nil
+}
+
+func getBool(opts proto.Message, ext *protoimpl.ExtensionInfo) (bool, error) {
+	val := getExt(opts, ext)
+	b, ok := val.(bool)
+	if !ok {
+		return false, errors.New("not bool")
+	}
+	return b, nil
 }
