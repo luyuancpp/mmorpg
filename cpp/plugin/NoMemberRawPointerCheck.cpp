496bb7a3ec80356922103f11adff27c9e2518570
luhailong <lhpcpp6@163.com>
2025-12-06 21:09:51 +0800

    提示乱吗

--- cpp/plugin/NoMemberRawPointerCheck.cpp
+++ cpp/plugin/NoMemberRawPointerCheck.cpp
@@ -8,10 +8,10 @@ using namespace clang::ast_matchers;
 
 void NoMemberRawPointerCheck::registerMatchers(MatchFinder* Finder) {
 
-	// 匹配任何成员字段（包含 static），如果其类型是裸指针，则禁止
+	// Match any member field (including static) if its type is a raw pointer
 	Finder->addMatcher(
 		fieldDecl(
-			hasType(pointerType())     // 任何裸指针类型
+			hasType(pointerType())     // any raw pointer type
 		).bind("ptrField"),
 		this
 	);
@@ -22,6 +22,6 @@ void NoMemberRawPointerCheck::check(const clang::ast_matchers::MatchFinder::Matc
 	if (!Field) return;
 
 	diag(Field->getLocation(),
-		"禁止在类/结构体内声明任何裸指针成员变量 '%0'（包括 static 成员）")
+		"Declaring raw pointer member variables in classes/structs is prohibited (including static members) '%0'")
 		<< Field->getName();
 }
