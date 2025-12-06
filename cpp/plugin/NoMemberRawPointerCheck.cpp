1d8ebb81b90d9839171b3349940ac01ac7ab8160
luhailong <lhpcpp6@163.com>
2025-12-06 20:44:13 +0800

    指针变量检查

--- cpp/plugin/NoMemberRawPointerCheck.cpp
+++ cpp/plugin/NoMemberRawPointerCheck.cpp
@@ -1,22 +1,27 @@
 ﻿#include "NoMemberRawPointerCheck.h"
+
 #include "clang/ASTMatchers/ASTMatchers.h"
 #include "clang/ASTMatchers/ASTMatchFinder.h"
 
+using namespace clang;
 using namespace clang::ast_matchers;
 
-void NoMemberRawPointerCheck::registerMatchers(MatchFinder *Finder) {
-    // 匹配类或结构体成员为裸指针
-    Finder->addMatcher(
-        fieldDecl(hasType(pointerType())).bind("ptrField"),
-        this
-    );
+void NoMemberRawPointerCheck::registerMatchers(MatchFinder* Finder) {
+
+	// 匹配任何成员字段（包含 static），如果其类型是裸指针，则禁止
+	Finder->addMatcher(
+		fieldDecl(
+			hasType(pointerType())     // 任何裸指针类型
+		).bind("ptrField"),
+		this
+	);
 }
 
-void NoMemberRawPointerCheck::check(const MatchFinder &Result) {
-    const auto *Field = Result.Nodes.getNodeAs<clang::FieldDecl>("ptrField");
-    if (!Field) return;
+void NoMemberRawPointerCheck::check(const clang::ast_matchers::MatchFinder::MatchResult& Result) {
+	const FieldDecl* Field = Result.Nodes.getNodeAs<FieldDecl>("ptrField");
+	if (!Field) return;
 
-    diag(Field->getLocation(),
-         "class/struct 成员变量不允许是裸指针类型 '%0'")
-        << Field->getType();
+	diag(Field->getLocation(),
+		"禁止在类/结构体内声明任何裸指针成员变量 '%0'（包括 static 成员）")
+		<< Field->getName();
 }
--- cpp/plugin/NoMemberRawPointerCheck.h
+++ cpp/plugin/NoMemberRawPointerCheck.h
@@ -1,12 +1,11 @@
-#pragma once
+﻿#pragma once
+
 #include "clang-tidy/ClangTidyCheck.h"
 
 class NoMemberRawPointerCheck : public clang::tidy::ClangTidyCheck {
 public:
-    NoMemberRawPointerCheck(llvm::StringRef Name,
-                            clang::tidy::ClangTidyContext* Context)
-        : ClangTidyCheck(Name, Context) {}
+	using ClangTidyCheck::ClangTidyCheck;
 
-    void registerMatchers(clang::ast_matchers::MatchFinder* Finder) override;
-    void check(const clang::ast_matchers::MatchFinder& Result) override;
+	void registerMatchers(clang::ast_matchers::MatchFinder* Finder) override;
+	void check(const clang::ast_matchers::MatchFinder::MatchResult& Result) override;
 };
