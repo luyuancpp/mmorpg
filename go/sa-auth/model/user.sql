8cb0ca800a28291cdf18ce4900ee4694e77b0c61
luhailong <lhpcpp6@163.com>
2025-12-11 19:29:55 +0800

    sa token 用java

--- go/sa-auth/model/user.sql
+++
@@ -1,9 +0,0 @@
-CREATE TABLE `user` (
-  `id` BIGINT UNSIGNED NOT NULL AUTO_INCREMENT,
-  `display_name` VARCHAR(64) DEFAULT '',
-  `is_guest` TINYINT(1) DEFAULT 0,
-  `create_time` BIGINT NOT NULL,
-  `last_login` BIGINT,
-  PRIMARY KEY (`id`),
-  INDEX `idx_last_login` (`last_login`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
--- go/sa-auth/model/user_phone.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE `user_phone` (
-  `user_id` BIGINT UNSIGNED NOT NULL,
-  `phone` VARCHAR(32) NOT NULL,
-  `create_time` BIGINT NOT NULL,
-  PRIMARY KEY (`user_id`),
-  UNIQUE KEY `uk_phone` (`phone`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
