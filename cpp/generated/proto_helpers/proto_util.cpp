27397933e7a401d6e46a279282404d904dbfae3e
luhailong <lhpcpp6@163.com>
2026-02-10 16:19:57 +0800

    领域驱动开发

--- cpp/generated/proto_helpers/proto_util.cpp
+++ cpp/generated/proto_helpers/proto_util.cpp
@@ -4,24 +4,6 @@
 
 bool IsTcpNodeType(uint32_t nodeType) {
 	static const std::unordered_set<uint32_t> validTypes = {
-		eNodeType::DeployNodeService,
-		eNodeType::CentreNodeService,
-		eNodeType::SceneNodeService,
-		eNodeType::GateNodeService,
-		eNodeType::RedisNodeService,
-		eNodeType::EtcdNodeService,
-		eNodeType::ActivityNodeService,
-		eNodeType::TradeNodeService,
-		eNodeType::RankNodeService,
-		eNodeType::TaskNodeService,
-		eNodeType::GuildNodeService,
-		eNodeType::MatchNodeService,
-		eNodeType::PaymentNodeService,
-		eNodeType::SecurityNodeService,
-		eNodeType::CrossServerNodeService,
-		eNodeType::AnalyticsNodeService,
-		eNodeType::GmNodeService,
-		eNodeType::PlayerLocatorNodeService,
 	};
 
 	return validTypes.contains(nodeType);
