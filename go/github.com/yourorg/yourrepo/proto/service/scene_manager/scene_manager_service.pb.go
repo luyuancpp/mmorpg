c2a26d747ce22c7206dbc4270ad8dbf28bbe6b8b
luhailong <lhpcpp6@163.com>
2026-02-10 19:26:04 +0800

    clear code

--- go/github.com/yourorg/yourrepo/proto/service/scene_manager/scene_manager_service.pb.go
+++ go/github.com/yourorg/yourrepo/proto/service/scene_manager/scene_manager_service.pb.go
@@ -7,7 +7,7 @@
 package scene_managerpb
 
 import (
-	_ "db/proto/common"
+	_ "chat/proto/common"
 	protoreflect "google.golang.org/protobuf/reflect/protoreflect"
 	protoimpl "google.golang.org/protobuf/runtime/protoimpl"
 	emptypb "google.golang.org/protobuf/types/known/emptypb"
