4c4a9ab38c43d7c3248781b049254e8540115c08
luhailoing <lhlcpp6@163.com>
2026-03-16 14:25:51 +0800

    clear code

--- cpp/nodes/scene/rpc_replies/centre_scene_response_handler.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-#include "muduo/net/TcpConnection.h"
-#include "proto/centre/centre_scene.pb.h"
-
-using namespace muduo;
-using namespace muduo::net;
-void OnCentreSceneRegisterSceneReply(const TcpConnectionPtr& conn, const std::shared_ptr<::RegisterSceneResponse>& replied, Timestamp timestamp);
-void OnCentreSceneUnRegisterSceneReply(const TcpConnectionPtr& conn, const std::shared_ptr<::Empty>& replied, Timestamp timestamp);
