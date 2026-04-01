d4c66b253811ad109ea39bd55b5ffd08c38acc96
luhailoing <lhlcpp6@163.com>
2026-04-02 00:25:47 +0800

    feat: add turnbased node types and proto contracts

--- cpp/libs/engine/core/node/system/node/node_util.cpp
+++ cpp/libs/engine/core/node/system/node/node_util.cpp
@@ -7,18 +7,22 @@
 
 // Static node-type-to-name map
 const std::unordered_map<eNodeType, std::string> nodeTypeNameMap = {
-	{ eNodeType::SceneNodeService,   eNodeType_Name(SceneNodeService) },
-	{ eNodeType::GateNodeService,    eNodeType_Name(GateNodeService) },
-	{ eNodeType::LoginNodeService,   eNodeType_Name(LoginNodeService) },
-	{ eNodeType::SceneManagerNodeService,eNodeType_Name(SceneManagerNodeService) },
-	{ eNodeType::DataServiceNodeService,eNodeType_Name(DataServiceNodeService) },
-	{ eNodeType::SlgMapNodeService,  eNodeType_Name(SlgMapNodeService) },
-	{ eNodeType::SlgBattleNodeService,eNodeType_Name(SlgBattleNodeService) }
-};
+	{eNodeType::SceneNodeService, eNodeType_Name(SceneNodeService)},
+	{eNodeType::GateNodeService, eNodeType_Name(GateNodeService)},
+	{eNodeType::LoginNodeService, eNodeType_Name(LoginNodeService)},
+	{eNodeType::SceneManagerNodeService, eNodeType_Name(SceneManagerNodeService)},
+	{eNodeType::DataServiceNodeService, eNodeType_Name(DataServiceNodeService)},
+	{eNodeType::SlgMapNodeService, eNodeType_Name(SlgMapNodeService)},
+	{eNodeType::SlgBattleNodeService, eNodeType_Name(SlgBattleNodeService)},
+	{eNodeType::TurnBasedBattleNodeService, eNodeType_Name(TurnBasedBattleNodeService)},
+	{eNodeType::TurnBasedDungeonNodeService, eNodeType_Name(TurnBasedDungeonNodeService)}};
 
-eNodeType NodeUtils::GetServiceTypeFromPrefix(const std::string& prefix) {
-	for (const auto& [type, name] : nodeTypeNameMap) {
-		if (prefix.find(name) != std::string::npos) {
+eNodeType NodeUtils::GetServiceTypeFromPrefix(const std::string &prefix)
+{
+	for (const auto &[type, name] : nodeTypeNameMap)
+	{
+		if (prefix.find(name) != std::string::npos)
+		{
 			return type;
 		}
 	}
@@ -27,21 +31,27 @@ eNodeType NodeUtils::GetServiceTypeFromPrefix(const std::string& prefix) {
 	return eNodeType(std::numeric_limits<::int32_t>::max());
 }
 
-entt::registry& NodeUtils::GetRegistryForNodeType(uint32_t nodeType) {
+entt::registry &NodeUtils::GetRegistryForNodeType(uint32_t nodeType)
+{
 	return tlsNodeContextManager.GetRegistry(nodeType);
 }
 
-std::string NodeUtils::GetRegistryName(const entt::registry& registry) {
+std::string NodeUtils::GetRegistryName(const entt::registry &registry)
+{
 	const auto type = GetRegistryType(registry);
-	if (type == eNodeType(std::numeric_limits<::int32_t>::max())) {
+	if (type == eNodeType(std::numeric_limits<::int32_t>::max()))
+	{
 		return "UnknownRegistry";
 	}
 	return eNodeType_Name(static_cast<uint32_t>(type));
 }
 
-eNodeType NodeUtils::GetRegistryType(const entt::registry& registry){
-	for (uint32_t i = 0; i < tlsNodeContextManager.GetAllRegistries().size(); ++i){
-		if (&tlsNodeContextManager.GetRegistry(i) == &registry) {
+eNodeType NodeUtils::GetRegistryType(const entt::registry &registry)
+{
+	for (uint32_t i = 0; i < tlsNodeContextManager.GetAllRegistries().size(); ++i)
+	{
+		if (&tlsNodeContextManager.GetRegistry(i) == &registry)
+		{
 			return eNodeType(i);
 		}
 	}
@@ -49,20 +59,24 @@ eNodeType NodeUtils::GetRegistryType(const entt::registry& registry){
 	return eNodeType(std::numeric_limits<::int32_t>::max());
 }
 
-bool NodeUtils::IsSameNode(const std::string& uuid1, const std::string& uuid2)
+bool NodeUtils::IsSameNode(const std::string &uuid1, const std::string &uuid2)
 {
 	return uuid1 == uuid2;
 }
 
-bool NodeUtils::IsNodeConnected(uint32_t nodeType, const NodeInfo& info)  {
-	switch (info.protocol_type()) {
+bool NodeUtils::IsNodeConnected(uint32_t nodeType, const NodeInfo &info)
+{
+	switch (info.protocol_type())
+	{
 	case PROTOCOL_TCP:
 	{
-		entt::registry& registry = tlsNodeContextManager.GetRegistry(nodeType);
-		for (const auto& [entity, client, nodeInfo] : registry.view<RpcClientPtr, NodeInfo>().each()) {
-			if (NodeUtils::IsSameNode(info.node_uuid(), nodeInfo.node_uuid())) {
+		entt::registry &registry = tlsNodeContextManager.GetRegistry(nodeType);
+		for (const auto &[entity, client, nodeInfo] : registry.view<RpcClientPtr, NodeInfo>().each())
+		{
+			if (NodeUtils::IsSameNode(info.node_uuid(), nodeInfo.node_uuid()))
+			{
 				LOG_INFO << "Node already registered, IP: " << nodeInfo.endpoint().ip()
-					<< ", Port: " << nodeInfo.endpoint().port();
+						 << ", Port: " << nodeInfo.endpoint().port();
 				return true;
 			}
 		}
@@ -70,9 +84,11 @@ bool NodeUtils::IsNodeConnected(uint32_t nodeType, const NodeInfo& info)  {
 	break;
 	case PROTOCOL_GRPC:
 	{
-		entt::registry& registry = tlsNodeContextManager.GetRegistry(nodeType);
-		for (const auto& [entity, nodeInfo] : registry.view<NodeInfo>().each()) {
-			if (NodeUtils::IsSameNode(info.node_uuid(), nodeInfo.node_uuid())) {
+		entt::registry &registry = tlsNodeContextManager.GetRegistry(nodeType);
+		for (const auto &[entity, nodeInfo] : registry.view<NodeInfo>().each())
+		{
+			if (NodeUtils::IsSameNode(info.node_uuid(), nodeInfo.node_uuid()))
+			{
 				LOG_TRACE << "GRPC node already tracked, uuid=" << nodeInfo.node_uuid();
 				return true;
 			}
@@ -85,4 +101,3 @@ bool NodeUtils::IsNodeConnected(uint32_t nodeType, const NodeInfo& info)  {
 
 	return false;
 }
-
--- cpp/libs/engine/core/node/system/node/node_util.h
+++ cpp/libs/engine/core/node/system/node/node_util.h
@@ -5,64 +5,65 @@
 #include <cstdint>
 
 // Re-export common::base types at global scope for backward compatibility.
+using common::base::eNodeProtocolType;
 using common::base::eNodeType;
 using common::base::eSceneNodeType;
-using common::base::eNodeProtocolType;
 
 using common::base::eNodeType_ARRAYSIZE;
-using common::base::eNodeType_Name;
 using common::base::eNodeType_IsValid;
+using common::base::eNodeType_Name;
 using common::base::eSceneNodeType_ARRAYSIZE;
 using common::base::eSceneNodeType_Name;
 
 // Re-export eNodeType enumerator values.
-using common::base::UnknownNodeService;
-using common::base::SceneNodeService;
-using common::base::GateNodeService;
-using common::base::LoginNodeService;
-using common::base::RedisNodeService;
-using common::base::EtcdNodeService;
-using common::base::MailNodeService;
-using common::base::ChatNodeService;
-using common::base::TeamNodeService;
 using common::base::ActivityNodeService;
-using common::base::TradeNodeService;
-using common::base::RankNodeService;
-using common::base::TaskNodeService;
-using common::base::GuildNodeService;
-using common::base::MatchNodeService;
 using common::base::AiNodeService;
-using common::base::LogNodeService;
-using common::base::PaymentNodeService;
-using common::base::SecurityNodeService;
-using common::base::CrossServerNodeService;
 using common::base::AnalyticsNodeService;
-using common::base::GmNodeService;
-using common::base::PlayerLocatorNodeService;
-using common::base::SceneManagerNodeService;
+using common::base::ChatNodeService;
+using common::base::CrossServerNodeService;
 using common::base::DataServiceNodeService;
+using common::base::EtcdNodeService;
 using common::base::FriendNodeService;
-using common::base::SlgMapNodeService;
+using common::base::GateNodeService;
+using common::base::GmNodeService;
+using common::base::GuildNodeService;
+using common::base::LoginNodeService;
+using common::base::LogNodeService;
+using common::base::MailNodeService;
+using common::base::MatchNodeService;
+using common::base::PaymentNodeService;
+using common::base::PlayerLocatorNodeService;
+using common::base::RankNodeService;
+using common::base::RedisNodeService;
+using common::base::SceneManagerNodeService;
+using common::base::SceneNodeService;
+using common::base::SecurityNodeService;
 using common::base::SlgBattleNodeService;
+using common::base::SlgMapNodeService;
+using common::base::TaskNodeService;
+using common::base::TeamNodeService;
+using common::base::TradeNodeService;
+using common::base::TurnBasedBattleNodeService;
+using common::base::TurnBasedDungeonNodeService;
+using common::base::UnknownNodeService;
 
 // Re-export eSceneNodeType enumerator values.
+using common::base::kMainSceneCrossNode;
 using common::base::kMainSceneNode;
 using common::base::kSceneNode;
-using common::base::kMainSceneCrossNode;
 using common::base::kSceneSceneCrossNode;
 
 // Re-export eNodeProtocolType enumerator values.
-using common::base::PROTOCOL_TCP;
 using common::base::PROTOCOL_GRPC;
 using common::base::PROTOCOL_HTTP;
+using common::base::PROTOCOL_TCP;
 
 namespace NodeUtils
 {
-	eNodeType GetServiceTypeFromPrefix(const std::string& prefix);
-	entt::registry& GetRegistryForNodeType(uint32_t nodeType);
-	std::string GetRegistryName(const entt::registry& registry);
-	eNodeType GetRegistryType(const entt::registry& registry);
-	bool IsSameNode(const std::string& uuid1, const std::string& uuid2);
-	bool IsNodeConnected(uint32_t nodeType, const NodeInfo& info);
+	eNodeType GetServiceTypeFromPrefix(const std::string &prefix);
+	entt::registry &GetRegistryForNodeType(uint32_t nodeType);
+	std::string GetRegistryName(const entt::registry &registry);
+	eNodeType GetRegistryType(const entt::registry &registry);
+	bool IsSameNode(const std::string &uuid1, const std::string &uuid2);
+	bool IsNodeConnected(uint32_t nodeType, const NodeInfo &info);
 };
-
